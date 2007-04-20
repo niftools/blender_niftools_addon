f0ec5af9c76e9bdec5d41a3a31968ba92972ede6
Brandano <brandano@users.sourceforge.net>
2007-04-20 21:09:26 +0000

    Abstracted event ID's from GUI handling for easier editing

--- contrib/scripts/bpymodules/nifImEx/Config.py
+++ contrib/scripts/bpymodules/nifImEx/Config.py
@@ -2,6 +2,7 @@ import Blender, os, sys
 import Read, Write, Defaults
 from Blender import Draw, BGL, Registry
 
+# clears the console window
 if sys.platform in ('linux-i386','linux2'):
     os.system("clear")
 elif sys.platform in ('win32','dos','ms-dos'):
@@ -11,48 +12,72 @@ elif sys.platform in ('win32','dos','ms-dos'):
 _GUI_ELEMENTS = {}
 _WINDOW_SIZE = Blender.Window.GetAreaSize()
 
+# To avoid confusion with event ID handling I register them all in a list
+_GUI_EVENTS = []
+
 # Configuration
 _CONFIG = {}
+# Old configuration for cancel button
+_CONFIG_BACK = _CONFIG
 _CONFIG_NAME = "nifscripts"
 
 # Back target for exit
 _BACK_TARGET = None
 
+# Texture search path item index
+_IDX_TEXPATH = 0
+
 def __init__():
     global _GUI_ELEMENTS, _WINDOW_SIZE, _CONFIG
-    reload(Defaults)
     _GUI_ELEMENTS = {}
     _WINDOW_SIZE = Blender.Window.GetAreaSize()
     _CONFIG = {}
-    clean()
+    load()
+
+def addEvent(evName = "NO_NAME"):
+    global _GUI_EVENTS
+    eventId = len(_GUI_EVENTS)
+    if eventId >= 16383:
+        raise "Maximum number of events exceeded"
+        return None
+    #if evName in _GUI_EVENTS:
+    #    evName = "%s_%05d" % (evName, eventId)
+    _GUI_EVENTS.append(evName)
+    #print "id assigned: %05d" % eventId
+    return eventId
 
 def gui():
-    global _GUI_ELEMENTS, _WINDOW_SIZE, _CONFIG
+    global _GUI_ELEMENTS, _WINDOW_SIZE, _CONFIG, _IDX_TEXPATH, _GUI_EVENTS
+    _GUI_EVENTS = []
     # These are to save me some typing
     #W = _WINDOW_SIZE[0]
     H = _WINDOW_SIZE[1]
+    # dictionary of GUI elements
     E = {}
+    # dictionary of GUI events
+    
     # IMPORTANT: Don't start dictionary keys with an underscore, the Registry module doesn't like that, apparently
     # Draw.String(name, event, x, y, width, height, initial, length, tooltip=None) 
-    E["NIF_IMPORT_PATH"]        = Draw.String("",          150,  50, H- 75, 350, 20, _CONFIG["NIF_IMPORT_PATH"],        350, "export path")
-    E["BROWSE_IMPORT_PATH"]     = Draw.PushButton('browse',155, 410, H- 75, 100, 20)
-    E["NIF_EXPORT_PATH"]        = Draw.String("",          160,  50, H-100, 350, 20, _CONFIG["NIF_EXPORT_PATH"],        350, "import path")
-    E["BROWSE_EXPORT_PATH"]     = Draw.PushButton('browse',165, 410, H-100, 100, 20)
-    E["BASE_TEXTURE_FOLDER"]    = Draw.String("",          170,  50, H-125, 350, 20, _CONFIG["BASE_TEXTURE_FOLDER"],    350, "import path")
-    E["BROWSE_TEXBASE"]         = Draw.PushButton('browse',175, 410, H-125, 100, 20)
-    E["TEXTURE_SEARCH_PATH"]    = Draw.String("",          180,  50, H-150, 350, 20, _CONFIG["TEXTURE_SEARCH_PATH"], 350, "texture search path")
-    E["TEXPATH_ITEM"]           = Draw.String("",          190,  50, H-170, 350, 20, "" , 290)
-    E["TEXPATH_PREV"]           = Draw.PushButton('<',     200, 340, H-170,  20, 20)
-    E["TEXPATH_NEXT"]           = Draw.PushButton('>',     210, 360, H-170,  20, 20)
-    E["TEXPATH_REMOVE"]         = Draw.PushButton('X',     230, 380, H-170,  20, 20)
-    E["BROWSE_TEXPATH"]         = Draw.PushButton('browse',235, 410, H-170, 100, 20)
-    E["REALIGN_BONES"]          = Draw.Toggle(" ",         240,  50, H-200,  20, 20, _CONFIG["REALIGN_BONES"])
+    E["NIF_IMPORT_PATH"]        = Draw.String("",       addEvent("NIF_IMPORT_PATH"),     50, H- 75, 390, 20, _CONFIG["NIF_IMPORT_PATH"],       390, "export path")
+    E["BROWSE_IMPORT_PATH"]     = Draw.PushButton('...',addEvent("BROWSE_IMPORT_PATH"), 440, H- 75,  30, 20)
+    E["NIF_EXPORT_PATH"]        = Draw.String("",       addEvent("NIF_EXPORT_PATH"),     50, H-100, 390, 20, _CONFIG["NIF_EXPORT_PATH"],       390, "import path")
+    E["BROWSE_EXPORT_PATH"]     = Draw.PushButton('...',addEvent("BROWSE_EXPORT_PATH"), 440, H-100,  30, 20)
+    E["BASE_TEXTURE_FOLDER"]    = Draw.String("",       addEvent("BASE_TEXTURE_FOLDER"), 50, H-125, 390, 20, _CONFIG["BASE_TEXTURE_FOLDER"],   390, "import path")
+    E["BROWSE_TEXBASE"]         = Draw.PushButton('...',addEvent("BROWSE_TEXBASE"),     440, H-125,  30, 20)
+    E["TEXTURE_SEARCH_PATH"]    = Draw.String("",       addEvent("TEXTURE_SEARCH_PATH"), 50, H-150, 390, 20, "\n".join(_CONFIG["TEXTURE_SEARCH_PATH"]), 390, "texture search path")
+    E["TEXPATH_ITEM"]           = Draw.String("",       addEvent("TEXPATH_ITEM"),        50, H-170, 350, 20, "" , 290)
+    E["TEXPATH_PREV"]           = Draw.PushButton('<',  addEvent("TEXPATH_PREV"),       400, H-170,  20, 20)
+    E["TEXPATH_NEXT"]           = Draw.PushButton('>',  addEvent("TEXPATH_NEXT"),       420, H-170,  20, 20)
+    E["TEXPATH_REMOVE"]         = Draw.PushButton('X',  addEvent("TEXPATH_REMOVE"),     440, H-170,  20, 20)
+    E["BROWSE_TEXPATH"]         = Draw.PushButton('...',addEvent("BROWSE_TEXPATH"),     460, H-170,  30, 20)
+    E["REALIGN_BONES"]          = Draw.Toggle(" ",      addEvent("REALIGN_BONES"),       50, H-200,  20, 20, _CONFIG["REALIGN_BONES"])
     # To draw text on the screen I have to position its start point first
     BGL.glRasterPos2i( 75, H-195)
     Draw.Text("try to realign bones")
     
-    E["CANCEL"]                 = Draw.PushButton('cancel',250,  50, H-225, 100, 20)
-    E["OK"]                     = Draw.PushButton('ok',    260,  50, H-250, 100, 20)
+    E["CANCEL"]                 = Draw.PushButton('cancel',addEvent("CANCEL"),  50, H-225, 100, 20)
+    E["OK"]                     = Draw.PushButton('ok',    addEvent("OK"),  50, H-250, 100, 20)
+    # Sets the GUI elements to a global var to avoid them going out of scope (cases segfaults)
     _GUI_ELEMENTS = E
     Draw.Redraw(1)
 
@@ -60,13 +85,24 @@ def buttonEvent(evt):
     """
     Event handler for buttons
     """
-    if evt == 260:
+    global _GUI_EVENTS, _CONFIG, _CONFIG_BACK
+    evName = _GUI_EVENTS[evt]
+    if evName == "OK":
         save()
         exit()
-    elif  evt == 250:
+    elif evName == "CANCEL":
+        _CONFIG = _CONFIG_BACK
         exit()
-    elif  evt == 240:
+    elif evName == "REALIGN_BONES":
         _CONFIG["REALIGN_BONES"] = not _CONFIG["REALIGN_BONES"]
+    elif evName == "BROWSE_IMPORT_PATH":
+        # browse import path
+        Blender.Window.FileSelector(setImportPath, "set import path", _CONFIG["NIF_IMPORT_PATH"])
+        Draw.Redraw(1)
+    elif evName == "BROWSE_EXPORT_PATH":
+        # browse import path
+        Blender.Window.FileSelector(setExportPath, "set export path", _CONFIG["NIF_EXPORT_PATH"])
+        Draw.Redraw(1)
     else:
         None
     Draw.Redraw(1)
@@ -75,6 +111,7 @@ def event(evt, val):
     """
     Event handler for GUI elements
     """
+    global _GUI_EVENTS
     #print  "event(%i,%i)"%(arg1,arg2)
     if evt == Draw.ESCKEY:
         exit()
@@ -86,7 +123,7 @@ def open(back_target=None):
     # defines what script called the config screen
     global _BACK_TARGET
     _BACK_TARGET = back_target
-    clean()
+    load()
     Draw.Register(gui, event, buttonEvent)
     
 def exit():
@@ -105,28 +142,21 @@ def exit():
         reload(Main)
         Main.open()
 
-
 def save():
     """
     Saves the current configuration
     """
-    global _CONFIG, _CONFIG_NAME
+    global _CONFIG, _CONFIG_BACK, _CONFIG_NAME
     print "datadir", Blender.Get('datadir'), "\n\n"
     print "--",_CONFIG_NAME, _CONFIG, "\n\n"
     Registry.SetKey(_CONFIG_NAME, _CONFIG, True)
+    _CONFIG_BACK = _CONFIG
 
 def load():
     """
-    Loads the stored configuration
+    Loads the stored configuration and checks saved configuration for incompatible values
     """
-    global _CONFIG
-    #_CONFIG = Blender.Registry.GetKey(_CONFIG_NAME, True)
-    clean()
-
-def clean():
-    """
-    Checks saved configuration for incompatible values
-    """
+    # both loads and cleans up the configuration from the registry
     global _CONFIG, _CONFIG_NAME
     reload(Defaults)
     _CONFIG = {
@@ -153,5 +183,17 @@ def clean():
             newConfig[key] = _CONFIG[key]
     #print "newConfig", newConfig, "\n\n"
     _CONFIG = newConfig
+    _CONFIG_BACK = newConfig
     Blender.Registry.SetKey(_CONFIG_NAME, _CONFIG, True)
 
+def setImportPath(nifImportPath):
+    if nifImportPath == '' or  sys.exists(nifImportPath) != 1:
+        Draw.PupMenu('No path selected or path does not exist%t|Ok')
+    else:
+        _CONFIG["NIF_IMPORT_PATH"] = sys.dirname(nifImportPath)
+
+def setExportPath(nifExportPath):
+    if nifFilePath == '' or  sys.exists(nifExportPath) != 1:
+        Draw.PupMenu('No path selected or path does not exist%t|Ok')
+    else:
+        _CONFIG["NIF_EXPORT_PATH"] = sys.dirname(nifExportPath)
