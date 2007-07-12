9fdf8bbb67e5ccd51d199fbe7b3affa6f089e87a
Brandano <brandano@users.sourceforge.net>
2007-07-12 08:58:54 +0000

    Generalized for Windows languages other than English

--- makezip.bat
+++ makezip.bat
@@ -1,3 +1,3 @@
 rm blender_nif_scripts-2.0.zip
-"\Program Files\7-Zip\7z.exe" a -tzip blender_nif_scripts-2.0.zip scripts\nif_import.py scripts\nif_export.py scripts\bpymodules\nifImEx\__init__.py scripts\bpymodules\nifImEx\Config.py  scripts\bpymodules\nifImEx\Defaults.py scripts\bpymodules\nifImEx\Read.py scripts\bpymodules\nifImEx\Write.py scripts\bpymodules\nifImEx\niftools_logo.png ChangeLog README.html
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip blender_nif_scripts-2.0.zip scripts\nif_import.py scripts\nif_export.py scripts\bpymodules\nifImEx\__init__.py scripts\bpymodules\nifImEx\Config.py  scripts\bpymodules\nifImEx\Defaults.py scripts\bpymodules\nifImEx\Read.py scripts\bpymodules\nifImEx\Write.py scripts\bpymodules\nifImEx\niftools_logo.png ChangeLog README.html
 pause
