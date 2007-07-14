a15b3a023cadb11134f12470a6c93a75e24f7a49
Amorilia <amorilia@users.sourceforge.net>
2007-07-14 19:02:30 +0000

    linux zipper eol update (would not zip readme otherwise)

--- makezip.sh
+++ makezip.sh
@@ -1,2 +1,2 @@
-rm blender_nif_scripts-2.0.zip
+rm -f blender_nif_scripts-2.0.zip
 zip blender_nif_scripts-2.0.zip scripts/nif_import.py scripts/nif_export.py scripts/bpymodules/nifImEx/__init__.py scripts/bpymodules/nifImEx/Config.py  scripts/bpymodules/nifImEx/Defaults.py scripts/bpymodules/nifImEx/Read.py scripts/bpymodules/nifImEx/Write.py scripts/bpymodules/nifImEx/niftools_logo.png ChangeLog README.html
