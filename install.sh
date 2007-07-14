bea100b832ba612a42d1191f12c6eb39e3b69208
Amorilia <amorilia@users.sourceforge.net>
2007-07-14 19:11:08 +0000

    scripts: linux quick install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# quick and dirty script to install the scripts in a user's local blender dir
+
+cp scripts/nif_import.py scripts/nif_export.py ~/.blender/scripts
+mkdir -p ~/.blender/scripts/bpymodules/nifImEx
+cp scripts/bpymodules/nifImEx/__init__.py scripts/bpymodules/nifImEx/Config.py  scripts/bpymodules/nifImEx/Defaults.py scripts/bpymodules/nifImEx/Read.py scripts/bpymodules/nifImEx/Write.py scripts/bpymodules/nifImEx/niftools_logo.png ~/.blender/scripts/bpymodules/nifImEx
