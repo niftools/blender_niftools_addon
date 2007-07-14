335cde195252f6eb310670c369c241670dd9895a
Amorilia <amorilia@users.sourceforge.net>
2007-07-14 20:21:52 +0000

    scripts: * updated version number

--- makezip.sh
+++ makezip.sh
@@ -1,2 +1,2 @@
-rm -f blender_nif_scripts-2.0.zip
-zip blender_nif_scripts-2.0.zip scripts/nif_import.py scripts/nif_export.py scripts/bpymodules/nifImEx/__init__.py scripts/bpymodules/nifImEx/Config.py  scripts/bpymodules/nifImEx/Defaults.py scripts/bpymodules/nifImEx/Read.py scripts/bpymodules/nifImEx/Write.py scripts/bpymodules/nifImEx/niftools_logo.png ChangeLog README.html install.sh
+rm -f blender_nif_scripts-2.0.1.zip
+zip blender_nif_scripts-2.0.1.zip scripts/nif_import.py scripts/nif_export.py scripts/bpymodules/nifImEx/__init__.py scripts/bpymodules/nifImEx/Config.py  scripts/bpymodules/nifImEx/Defaults.py scripts/bpymodules/nifImEx/Read.py scripts/bpymodules/nifImEx/Write.py scripts/bpymodules/nifImEx/niftools_logo.png ChangeLog README.html install.sh
