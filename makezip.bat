6b9ba53b731aef83480674e7ec67082301aa0b5a
Amorilia <amorilia@users.sourceforge.net>
2007-07-30 14:28:41 +0000

    windows makezip update

--- makezip.bat
+++ makezip.bat
@@ -1,3 +1,13 @@
-del blender_nif_scripts-2.0.5.zip
-"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip blender_nif_scripts-2.0.5.zip scripts\nif_import.py scripts\nif_export.py scripts\bpymodules\nifImEx\__init__.py scripts\bpymodules\nifImEx\Config.py  scripts\bpymodules\nifImEx\Defaults.py scripts\bpymodules\nifImEx\Read.py scripts\bpymodules\nifImEx\Write.py scripts\bpymodules\nifImEx\niftools_logo.png ChangeLog README.html install.sh
+set NAME=blender_nif_scripts
+set VERSION=2.0.6
+set FILES=scripts\nif_import.py scripts\nif_export.py scripts\bpymodules\nifImEx\__init__.py scripts\bpymodules\nifImEx\Config.py  scripts\bpymodules\nifImEx\Defaults.py scripts\bpymodules\nifImEx\Read.py scripts\bpymodules\nifImEx\Write.py scripts\bpymodules\nifImEx\niftools_logo.png ChangeLog README.html install.sh
+
+del %NAME%-%VERSION%.*
+
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip %NAME%-%VERSION%.zip %FILES%
+"%PROGRAMFILES%\7-Zip\7z.exe" a -ttar %NAME%-%VERSION%.tar %FILES%
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tbzip2 %NAME%-%VERSION%.tar.bz2 %NAME%-%VERSION%.tar
+
+del %NAME%-%VERSION%.tar
+
 pause
