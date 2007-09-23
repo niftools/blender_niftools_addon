6369ddbd0118cb392a0bbf44034bd4bee23e7986
Amorilia <amorilia@users.sourceforge.net>
2007-09-23 15:24:16 +0000

    finishing last commit

--- install.sh
+++ install.sh
@@ -5,4 +5,3 @@
 cp scripts/nif_import.py scripts/nif_export.py scripts/mesh_weightsquash.py scripts/mesh_hull.py ~/.blender/scripts
 mkdir -p ~/.blender/scripts/bpymodules/nifImEx
 cp scripts/bpymodules/nifImEx/__init__.py scripts/bpymodules/nifImEx/Config.py  scripts/bpymodules/nifImEx/Defaults.py scripts/bpymodules/nifImEx/Read.py scripts/bpymodules/nifImEx/Write.py scripts/bpymodules/nifImEx/niftools_logo.png ~/.blender/scripts/bpymodules/nifImEx
-cp scripts/bpymodules/nifImEx/_Write.py ~/.blender/scripts/bpymodules/nifImEx/Write.py
