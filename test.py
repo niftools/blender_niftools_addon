76633276c82fa84023a20146b73acd94e1892a2b
Amorilia <amorilia@users.sourceforge.net>
2007-11-27 15:31:00 +0000

    updating list of test files

--- test.py
+++ test.py
@@ -5,9 +5,6 @@ from nif_import import NifImport
 from nif_export import NifExport
 from nif_common import NifConfig
 
-SCENE = Blender.Scene.GetCurrent() # current scene
-LAYER = 1
-
 # some tests to import and export nif files
 # as list of (filename, config dictionary, list of objects to be selected)
 # if the config has a EXPORT_VERSION key then the test is an export test
@@ -38,40 +35,56 @@ TEST_FILES = [
      ['Bip01']),
 ]
 
-for filename, filecfg, selection in TEST_FILES:
-    # select objects
-    SCENE.objects.selected = [
-        ob for ob in SCENE.objects if ob.name in selection]
+class Test:
+    """A class for running import and export tests with Blender."""
+    def __init__(self):
+        """Initialize the tester."""
+        self.scene = Blender.Scene.GetCurrent() # current scene
+        self.layer = 1
 
-    # set script configuration
-    config = dict(**NifConfig.DEFAULTS)
-    config["VERBOSITY"] = 99
-    for key, value in filecfg.items():
-        config[key] = value
+    def run(self, test_files):
+        """Test the specified files.
 
-    # run test
-    if 'EXPORT_VERSION' in filecfg:
-        # export the imported files
-        print "*** exporting %s ***" % filename
+        @param test_files: A list of all files to test. Each entry in the list
+            is a tuple containing the filename, a dictionary of config options
+            that differ from their default values, and a list of names of
+            objects to select before running the import. If the dictionary
+            has the key EXPORT_VERSION then the selection is exported,
+            otherwise a file is imported."""
+        for filename, filecfg, selection in test_files:
+            # select objects
+            self.scene.objects.selected = [
+                ob for ob in self.scene.objects if ob.name in selection]
 
-        config["EXPORT_FILE"] = "test/nif/%s" % filename
-        NifExport(**config)
+            # set script configuration
+            config = dict(**NifConfig.DEFAULTS)
+            config["VERBOSITY"] = 99
+            for key, value in filecfg.items():
+                config[key] = value
 
-        # increment active layer for next import
-        # different tests are put into different blender layers,
-        # so the results can be easily visually inspected
-        LAYER += 1
-        SCENE.setLayers([LAYER])
-    else:
-        print "*** importing %s ***" % filename
+            # run test
+            if 'EXPORT_VERSION' in filecfg:
+                # export the imported files
+                print("*** exporting %s ***" % filename)
 
-        config["IMPORT_FILE"] = "test/nif/%s" % filename
+                config["EXPORT_FILE"] = "test/nif/%s" % filename
+                NifExport(**config)
 
-        # import <filename>
-        print "import..."
-        NifImport(**config)
+                # increment active layer for next import
+                # different tests are put into different blender layers,
+                # so the results can be easily visually inspected
+                self.layer += 1
+                self.scene.setLayers([self.layer])
+            else:
+                print("*** importing %s ***" % filename)
 
-# deselect everything
-SCENE.objects.selected = []
-# reset active layer
-SCENE.setLayers([1])
+                config["IMPORT_FILE"] = "test/nif/%s" % filename
+
+                # import <filename>
+                print "import..."
+                NifImport(**config)
+
+        # deselect everything
+        self.scene.objects.selected = []
+        # reset active layer
+        self.scene.setLayers([1])
--- test/nif/README.txt
+++ test/nif/README.txt
@@ -8,3 +8,6 @@ characters/_male/lowerbody.nif
 characters/_male/hand.nif
 characters/_male/foot.nif
 
+architecture/anvil/anvilcirclebench01.nif (bhk packed tristrips)
+weapons/blackaxe/battleaxe.nif (bhk box + capsule)
+clutter/magesguild/crystalball02.nif (bhk box + sphere)
