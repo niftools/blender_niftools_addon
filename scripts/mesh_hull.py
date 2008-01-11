6a8d9a1b41b31e5725d7d2a6caf67377321dd535
Amorilia <amorilia@users.sourceforge.net>
2008-01-11 11:04:47 +0000

    updated year in copyright notice

--- scripts/mesh_hull.py
+++ scripts/mesh_hull.py
@@ -18,7 +18,7 @@ Tooltip: 'Hull Selected Objects'
 # 
 # BSD License
 # 
-# Copyright (c) 2007, NIF File Format Library and Tools
+# Copyright (c) 2007-2008, NIF File Format Library and Tools
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
--- scripts/nif_export.py
+++ scripts/nif_export.py
@@ -24,7 +24,7 @@ from nif_common import __version__
 # 
 # BSD License
 # 
-# Copyright (c) 2007, NIF File Format Library and Tools
+# Copyright (c) 2007-2008, NIF File Format Library and Tools
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
