4bd1366974af126ea72c62500c84369b001a5fe7
Amorilia <amorilia@users.sourceforge.net>
2008-02-18 12:15:06 +0000

    version bump + changelog update

--- scripts/bpymodules/nif_common.py
+++ scripts/bpymodules/nif_common.py
@@ -1,6 +1,6 @@
 """Common functions for the Blender nif import and export scripts."""
 
-__version__ = "2.2.8"
+__version__ = "2.2.9"
 __requiredpyffiversion__ = "0.9.0"
 __requiredblenderversion__ = "245"
 
