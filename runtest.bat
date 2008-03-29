af4f93fe2db9d3aaa82195053e95fdbd85e63894
Amorilia <amorilia@users.sourceforge.net>
2008-03-29 10:59:10 +0000

    added test script for windows

---
+++ runtest.bat
@@ -0,0 +1,33 @@
+@rem quick script to run Blender tests
+@echo off
+
+rem find Blender install path
+
+set BLENDERHOME=
+if [%1]==[] (
+for /f "tokens=2* delims=	 " %%A in ('reg.exe query "HKLM\SOFTWARE\BlenderFoundation" /v Install_Dir') DO SET BLENDERHOME="%%B"
+)
+
+if not defined BLENDERHOME (
+echo Blender not found! Cannot run tests.
+goto end
+)
+
+echo .
+echo Running tests...
+echo .
+
+%BLENDERHOME%\blender.exe -P runtest_skinning.py
+%BLENDERHOME%\blender.exe -P runtest_havok.py
+
+echo.
+echo Done!
+echo.
+
+:end
+
+rem undefine variables
+
+set BLENDERHOME=
+
+pause
