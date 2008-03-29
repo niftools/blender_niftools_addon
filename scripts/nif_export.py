42d73a4cc6d0c2bc1cf499db914e26571085c51f
Amorilia <amorilia@users.sourceforge.net>
2008-03-29 15:10:56 +0000

    exporter formatting

--- scripts/nif_export.py
+++ scripts/nif_export.py
@@ -507,7 +507,10 @@ and turn off envelopes."""%ob.getName()
             NIF_USER_VERSION = 0 if self.version != 0x14000005 else 11
             f = open(self.filename, "wb")
             try:
-                NifFormat.write(f, version = self.version, user_version = NIF_USER_VERSION, roots = [root_block])
+                NifFormat.write(
+                    f,
+                    version = self.version, user_version = NIF_USER_VERSION,
+                    roots = [root_block])
             finally:
                 f.close()
 
