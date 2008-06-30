6875806eea4466b8569c15e804149aed151c5a43
Amorilia <amorilia@users.sourceforge.net>
2008-06-30 12:04:53 +0000

    more version syncs

--- scripts/bpymodules/nif_common.py
+++ scripts/bpymodules/nif_common.py
@@ -1,7 +1,7 @@
 """Common functions for the Blender nif import and export scripts."""
 
-__version__ = "2.3.3"
-__requiredpyffiversion__ = "0.10.9"
+__version__ = "2.3.4"
+__requiredpyffiversion__ = "1.0.0"
 __requiredblenderversion__ = "245"
 
 # ***** BEGIN LICENSE BLOCK *****
