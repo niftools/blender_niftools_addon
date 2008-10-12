95a5d2e04af29ac1ddce5a840ab691d8ffe235ca
Amorilia <amorilia@users.sourceforge.net>
2008-10-12 19:58:15 +0000

    script for generating epydoc documentation

---
+++ runepydoc.py
@@ -0,0 +1,7 @@
+args = ["", "-v", "--output=docs", "--name='Blender NIF Scripts'", "--url='http://niftools.sourceforge.net/'", """--navlink='&nbsp;&nbsp;&nbsp;<a class="navbar" target="_top" href="http://niftools.sourceforge.net/wiki/Blender">Blender NIF Scripts</a>&nbsp;&nbsp;&nbsp;</th><th class="navbar" align="center">&nbsp;&nbsp;&nbsp;<a class="navbar" target="_top" href="http://sourceforge.net"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=199269" width="88" height="31" border="0" alt="SourceForge.net Logo" /></a>&nbsp;&nbsp;&nbsp;'""", "--top=nif_common", "nif_common", "nif_import", "nif_export", "nif_test"]
+
+import epydoc.cli
+import sys
+sys.argv = args
+epydoc.cli.cli()
+
