ba2d90a4df47fce85049a06b54b472a962ebc091
Amorilia <amorilia@users.sourceforge.net>
2008-11-23 21:20:41 +0000

    quick script to create correct bind pose nif for fallout 3 (still experimental)

---
+++ test/nif/fo3/createbodybindpose.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+"""Create bind pose nif for Fallout 3."""
+
+from __future__ import with_statement
+from contextlib import closing
+
+from PyFFI.Formats.NIF import NifFormat
+
+# read skeleton
+skeleton = NifFormat.Data()
+with closing(open("skeleton.nif", "rb")) as stream:
+    skeleton.read(stream)
+# merge all body parts
+for bodypartnif in ("femaleupperbody.nif",
+                    "femalerighthand.nif",
+                    "femalelefthand.nif",
+                    "headfemale.nif"):
+    bodypart = NifFormat.Data()
+    with closing(open(bodypartnif, "rb")) as stream:
+        bodypart.read(stream)
+        skeleton.roots[0].mergeExternalSkeletonRoot(bodypart.roots[0])
+# send all bones to their bind position
+for block in skeleton.roots[0].tree():
+    if isinstance(block, NifFormat.NiGeometry):
+        block.sendBonesToBindPosition()
+# remove non-ninode children
+for block in skeleton.roots[0].tree():
+    block.setChildren([child
+                       for child in block.getChildren()
+                       if isinstance(child, NifFormat.NiNode)])
+    block.setExtraDatas([])
+    block.setProperties([])
+    block.controller = None
+    block.collisionObject = None
+
+# write result
+with closing(open("fo3_bodybindpos.nif", "wb")) as stream:
+    skeleton.write(stream)
