eae0c06b27ca192d5f7a6429f53408a77ff2f048
Amorilia <amorilia@users.sourceforge.net>
2009-01-07 15:18:29 +0000

    improved bind pose nif creation for fo3

--- test/nif/fo3/createbodybindpose.py
+++ test/nif/fo3/createbodybindpose.py
@@ -4,27 +4,50 @@
 
 from __future__ import with_statement
 from contextlib import closing
+import logging
 
 from PyFFI.Formats.NIF import NifFormat
 
+# set up logger
+niftoolslogger = logging.getLogger("niftools")
+niftoolslogger.setLevel(logging.DEBUG)
+pyffilogger = logging.getLogger("pyffi")
+pyffilogger.setLevel(logging.INFO)
+loghandler = logging.StreamHandler()
+loghandler.setLevel(logging.DEBUG)
+logformatter = logging.Formatter("%(name)s:%(levelname)s:%(message)s")
+loghandler.setFormatter(logformatter)
+niftoolslogger.addHandler(loghandler)
+pyffilogger.addHandler(loghandler)
+
+logger = logging.getLogger("niftools.blender.test")
+
 # read skeleton
+logger.info("Reading skeleton.nif")
 skeleton = NifFormat.Data()
 with closing(open("skeleton.nif", "rb")) as stream:
     skeleton.read(stream)
+
 # merge all body parts
 for bodypartnif in ("femaleupperbody.nif",
                     "femalerighthand.nif",
                     "femalelefthand.nif",
                     "headfemale.nif"):
+    logger.info("Merging body part %s" % bodypartnif)
     bodypart = NifFormat.Data()
     with closing(open(bodypartnif, "rb")) as stream:
         bodypart.read(stream)
         skeleton.roots[0].mergeExternalSkeletonRoot(bodypart.roots[0])
+# send geometries to their bind position
+logger.info("Sending geometries to bind position")
+skeleton.roots[0].sendGeometriesToBindPosition()
 # send all bones to their bind position
+logger.info("Sending bones to bind position")
 for block in skeleton.roots[0].tree():
     if isinstance(block, NifFormat.NiGeometry):
         block.sendBonesToBindPosition()
 # remove non-ninode children
+logger.info("Removing non-NiNode children")
 for block in skeleton.roots[0].tree():
     block.setChildren([child
                        for child in block.getChildren()
@@ -35,5 +58,6 @@ for block in skeleton.roots[0].tree():
     block.collisionObject = None
 
 # write result
+logger.info("Writing fo3_bodybindpose.nif")
 with closing(open("fo3_bodybindpose.nif", "wb")) as stream:
     skeleton.write(stream)
