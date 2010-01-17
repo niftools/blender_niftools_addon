faa3e5990dddf6caa1fee2a86f5770f543f2ce18
Amorilia <amorilia@users.sourceforge.net>
2010-01-17 11:44:55 +0000

    Moved Fallout 3 full body code to nif_test (so it can be used by other scripts too).

--- runtest_fo3_animation.py
+++ runtest_fo3_animation.py
@@ -1,4 +1,4 @@
-"""Create bind pose nif for Fallout 3, and check animation."""
+"""Import and export full body (bind pose) nif for Fallout 3."""
 
 from __future__ import with_statement
 import logging
@@ -8,7 +8,7 @@ from pyffi.formats.nif import NifFormat
 
 from nif_test import TestSuite
 
-class AnimationTestSuite(TestSuite):
+class Fallout3FullBodyTestSuite(TestSuite):
     def run(self):
         self.make_fo3_fullbody()
         self.test(
@@ -21,55 +21,5 @@ class AnimationTestSuite(TestSuite):
             config=dict(EXPORT_VERSION='Fallout 3'),
             selection=['Scene Root'])
 
-    def make_fo3_fullbody(self):
-        if os.path.exists("test/nif/fo3/_fullbody.nif"):
-            # to save time, only create the full body nif if it does
-            # not yet exist
-            return
-        # fo3 body path
-        fo3_male = os.path.join(
-            self.config.get("path", "fallout3"),
-            "meshes", "characters", "_male")
-        # read skeleton
-        self.logger.info("Reading skeleton.nif")
-        skeleton = NifFormat.Data()
-        with open(os.path.join(fo3_male, "skeleton.nif"), "rb") as stream:
-            skeleton.read(stream)
-
-        # merge all body parts
-        for bodypartnif in ("femaleupperbody.nif",
-                            "femalerighthand.nif",
-                            "femalelefthand.nif",
-                            "../head/headfemale.nif"):
-            self.logger.info("Merging body part %s" % bodypartnif)
-            bodypart = NifFormat.Data()
-            with open(os.path.join(fo3_male, bodypartnif), "rb") as stream:
-                bodypart.read(stream)
-                skeleton.roots[0].merge_external_skeleton_root(bodypart.roots[0])
-        # send geometries to their bind position
-        self.logger.info("Sending geometries to bind position")
-        skeleton.roots[0].send_geometries_to_bind_position()
-        # send all bones to their bind position
-        self.logger.info("Sending bones to bind position")
-        skeleton.roots[0].send_bones_to_bind_position()
-        #for block in skeleton.roots[0].tree():
-        #    if isinstance(block, NifFormat.NiGeometry):
-        #        block.send_bones_to_bind_position()
-        # remove non-ninode children
-        #logger.info("Removing non-NiNode children")
-        #for block in skeleton.roots[0].tree():
-        #    block.set_children([child
-        #                       for child in block.get_children()
-        #                       if isinstance(child, NifFormat.NiNode)])
-        #    block.set_extra_datas([])
-        #    block.set_properties([])
-        #    block.controller = None
-        #    block.collision_object = None
-
-        # write result
-        self.logger.info("Writing fo3_bodybindpose.nif")
-        with open("test/nif/fo3/_fullbody.nif", "wb") as stream:
-            skeleton.write(stream)
-
-suite = AnimationTestSuite("fo3_animation")
+suite = Fallout3FullBodyTestSuite("fo3_animation")
 suite.run()
--- scripts/bpymodules/nif_test.py
+++ scripts/bpymodules/nif_test.py
@@ -34,11 +34,13 @@
 
 import ConfigParser
 import logging
+import os.path
 
 import Blender
 from import_nif import NifImport
 from export_nif import NifExport
 from nif_common import NifConfig
+from nif_common import NifFormat
 
 class TestSuite:
     """A test suite class.
@@ -135,6 +137,56 @@ class TestSuite:
                 self.scene.setLayers([self.layer])
 
             return result
+
+    def make_fo3_fullbody(self):
+        if os.path.exists("test/nif/fo3/_fullbody.nif"):
+            # to save time, only create the full body nif if it does
+            # not yet exist
+            return
+        # fo3 body path
+        fo3_male = os.path.join(
+            self.config.get("path", "fallout3"),
+            "meshes", "characters", "_male")
+        # read skeleton
+        self.logger.info("Reading skeleton.nif")
+        skeleton = NifFormat.Data()
+        with open(os.path.join(fo3_male, "skeleton.nif"), "rb") as stream:
+            skeleton.read(stream)
+
+        # merge all body parts
+        for bodypartnif in ("femaleupperbody.nif",
+                            "femalerighthand.nif",
+                            "femalelefthand.nif",
+                            "../head/headfemale.nif"):
+            self.logger.info("Merging body part %s" % bodypartnif)
+            bodypart = NifFormat.Data()
+            with open(os.path.join(fo3_male, bodypartnif), "rb") as stream:
+                bodypart.read(stream)
+                skeleton.roots[0].merge_external_skeleton_root(bodypart.roots[0])
+        # send geometries to their bind position
+        self.logger.info("Sending geometries to bind position")
+        skeleton.roots[0].send_geometries_to_bind_position()
+        # send all bones to their bind position
+        self.logger.info("Sending bones to bind position")
+        skeleton.roots[0].send_bones_to_bind_position()
+        #for block in skeleton.roots[0].tree():
+        #    if isinstance(block, NifFormat.NiGeometry):
+        #        block.send_bones_to_bind_position()
+        # remove non-ninode children
+        #logger.info("Removing non-NiNode children")
+        #for block in skeleton.roots[0].tree():
+        #    block.set_children([child
+        #                       for child in block.get_children()
+        #                       if isinstance(child, NifFormat.NiNode)])
+        #    block.set_extra_datas([])
+        #    block.set_properties([])
+        #    block.controller = None
+        #    block.collision_object = None
+
+        # write result
+        self.logger.info("Writing fullbody.nif")
+        with open("test/nif/fo3/_fullbody.nif", "wb") as stream:
+            skeleton.write(stream)
 
     def run(self):
         """Run the test suite. Override."""
