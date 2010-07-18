90a8d2fc0a2ee0f4e54c9060c7c8b5ce11abefb0
Amorilia <amorilia@users.sourceforge.net>
2010-07-18 12:41:46 +0100

    Releases now contain revision number.

--- makezip.sh
+++ makezip.sh
@@ -2,6 +2,7 @@ git clean -x -f -d
 ./install.sh
 
 VERSION="2.5.5"
+wcrev=`git log -1 --pretty=format:%h`
 NAME="blender_nif_scripts"
 FILES="scripts/import/import_nif.py scripts/export/export_nif.py scripts/bpymodules/nif_common.py scripts/bpymodules/nif_test.py scripts/mesh/mesh_niftools_weightsquash.py scripts/mesh/mesh_niftools_hull.py scripts/object/object_niftools_set_bone_priority.py scripts/object/object_niftools_save_bone_pose.py scripts/object/object_niftools_load_bone_pose.py scripts/mesh/mesh_niftools_morphcopy.py ChangeLog README.html install.sh install.bat docs/*.*"
 
@@ -10,9 +11,10 @@ rm -rf docs
 blender -P runepydoc.py
 
 rm -f "${NAME}-${VERSION}".*
-zip -9 "${NAME}-${VERSION}.zip" ${FILES}
-tar cfvj "${NAME}-${VERSION}.tar.bz2" ${FILES}
+zip -9 "${NAME}-${VERSION}.${wcrev}.zip" ${FILES}
+tar cfvj "${NAME}-${VERSION}.${wcrev}.tar.bz2" ${FILES}
 
 # create windows installer
 rm -f "win-install/${NAME}-${VERSION}-windows.exe"
 makensis -V3 win-install/${NAME}.nsi
+mv "win-install/${NAME}-${VERSION}-windows.exe" "win-install/${NAME}-${VERSION}.${wcrev}-windows.exe"
