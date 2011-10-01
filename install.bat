4f3be7e888855b8a292b40c5cab74df658253a82
Amorilia <amorilia@users.sourceforge.net>
2011-10-01 20:38:40 +0100

    Delegated blender detection to buildenv script, simplified install script accordingly.

--- install.bat
+++ install.bat
@@ -1,38 +1,23 @@
 @rem quick and dirty linux shell script to install the scripts in a user's local blender dir
 @echo off
 
-if "%BLENDERHOME%" == "" goto pleasesetblenderhome
+if "%BLENDERADDONS%" == "" goto pleasesetblenderhome
 
-echo.Installing in %BLENDERHOME%
-
-if not exist "%BLENDERHOME%\blender.exe" (
-echo.%BLENDERHOME%\blender.exe not found!
-echo.
-goto pleasesetblenderhome
+echo.Installing in
+echo.%BLENDERADDONS%\io_scene_nif
+rem remove old files
+rmdir /s /q "%BLENDERADDONS%\io_scene_nif"
+rem copy files from repository to blender addons folder
+for %%A in (io_scene_nif, ) do mkdir "%BLENDERADDONS%\%%A"
+for %%A in (__init__.py, import_export_nif.py, export_nif.py, import_nif.py) do (
+  copy "scripts\addons\io_scene_nif\%%A" "%BLENDERADDONS%\io_scene_nif"
 )
-
-set BLENDERADDONS=%BLENDERHOME%\2.59\scripts\addons
-
-echo on
-
-@echo Removing clutter
-@for %%A in ("%BLENDERADDONS%\io_scene_nif\*.*") do if exist "%%A" del "%%A"
-
-@echo Installing files
-@for %%A in (io_scene_nif, ) do mkdir "%BLENDERADDONS%\%%A"
-@for %%A in (__init__.py, import_export_nif.py, export_nif.py, import_nif.py) do copy "scripts\addons\io_scene_nif\%%A" "%BLENDERADDONS%\io_scene_nif"
-
-@echo off
-
-echo.
-echo.Done!
-echo.
 goto end
 
 :pleasesetblenderhome
-echo.Please set BLENDERHOME to the folder where blender.exe resides, such as:
+echo.Please set BLENDERADDONS to the folder where blender.exe resides, such as:
 echo.
-echo.  set BLENDERHOME="C:\Program Files\Blender Foundation\Blender"
+echo.  set BLENDERADDONS=C:\Program Files\Blender Foundation\Blender\2.59\scripts\addons
 echo.
 pause
 goto end
