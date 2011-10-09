7278284e8367515e1160eff50d78e3ad31e6be63
Amorilia <amorilia@users.sourceforge.net>
2011-10-09 16:29:00 +0100

    Remove startup objects when setting up the test module.

--- test/__init__.py
+++ test/__init__.py
@@ -5,6 +5,10 @@ import bpy
 def setup(self):
     """Enables the nif scripts addon, so all tests can use it."""
     bpy.ops.wm.addon_enable(module="io_scene_nif")
+    # remove default objects
+    for obj in bpy.data.objects[:]:
+        bpy.context.scene.objects.unlink(obj)
+        bpy.data.objects.remove(obj)
 
 def teardown(self):
     """Disables the nif scripts addon."""
