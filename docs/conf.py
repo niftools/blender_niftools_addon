5230e1dd0441a77d470a8c027ad643ef5c91c19e
Amorilia <amorilia@users.sourceforge.net>
2011-10-09 14:24:07 +0100

    The documentation now links back to the official blender docs. :-)

--- docs/conf.py
+++ docs/conf.py
@@ -244,7 +244,10 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {
+    'http://docs.python.org/': None,
+    'http://www.blender.org/documentation/blender_python_api_2_59_0': None,
+    }
 
 # Include all todos
 todo_include_todos = True
