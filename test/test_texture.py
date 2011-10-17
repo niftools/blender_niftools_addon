a749d048a3b53b1f77ddf575f9e06a735d60ce69
Amorilia <amorilia@users.sourceforge.net>
2011-10-17 20:23:03 +0100

    Refactored tests to use a base class for removing test data; started initial uv texture test.

---
+++ test/test_texture.py
@@ -0,0 +1,28 @@
+"""Export and import textured meshes."""
+
+import bpy
+import nose.tools
+
+import io_scene_nif.export_nif
+from test import Test
+
+class TestBaseUVTextureImport(Test):
+    def test_export(self):
+        bpy.ops.import_scene.nif(
+            filepath="test/import/base_uv_texture.nif",
+            log_level='DEBUG',
+            )
+
+class TestBaseUVTextureExport(Test):
+    def setup(self):
+        # create a cube
+        bpy.ops.mesh.primitive_cube_add()
+        self.obj = bpy.data.objects["Cube"]
+        self.mesh = self.obj.data
+        # TODO add a texture to it
+
+    def test_export(self):
+        bpy.ops.export_scene.nif(
+            filepath="test/export/base_uv_texture.nif",
+            log_level='DEBUG',
+            )
