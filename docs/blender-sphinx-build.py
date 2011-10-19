266ea0439a3a2ae1a43b5eb27484070e72b556bf
Amorilia <amorilia@users.sourceforge.net>
2011-10-19 19:15:48 +0100

    ... and the same for the sphinx builder.

--- docs/blender-sphinx-build.py
+++ docs/blender-sphinx-build.py
@@ -1,6 +1,12 @@
-# get site-packages into sys.path
-import site
+import os
+import site # get site-packages into sys.path
 import sys
+
+# add local addons folder to sys.path so blender finds it
+sys.path = (
+    [os.path.join(os.path.dirname(__file__), '..', 'scripts', 'addons')]
+    + sys.path
+    )
 
 # run sphinx builder
 # this assumes that the builder is called as
