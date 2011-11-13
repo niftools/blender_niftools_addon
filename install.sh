3779323f05af04d5dce91464c5b5c81881bb0be9
Amorilia <amorilia@users.sourceforge.net>
2011-11-13 19:53:19 +0000

    Install script update for latest blender release.

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # quick and dirty script to install the blender nif scripts
 
-for BLENDERVERSION in 2.59 2.58 2.57 2.56 2.55 2.54 2.53 2.52 2.51 2.50
+for BLENDERVERSION in 2.61 2.60 2.59 2.58 2.57 2.56 2.55 2.54 2.53 2.52 2.51 2.50
 do
   BLENDERADDONS=~/.blender/$BLENDERVERSION/scripts/addons
-  if [ -e $BLENDERADDONS ]
+  if [ -e ~/.blender/$BLENDERVERSION/ ]
   then
     break
   fi
 done
-if [ ! -e $BLENDERADDONS ]
+if [ ! -e ~/.blender/$BLENDERVERSION/ ]
 then
   echo Blender addons folder not found.
-  echo Start blender at least once, and try again.
+  echo Start blender at least once, save user preferences, and try again.
   exit 1
 fi
 
