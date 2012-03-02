c4189e90b5cd5203d65e64956074b11a9663f0a8
neomonkeus <abominalmonkey@gmail.com>
2012-03-02 23:29:06 +0000

    Fixed testing inheritance calls. Edited nifs which were using TriStrips instead of TriShape.

--- test/test_property.py
+++ test/test_property.py
@@ -150,9 +150,9 @@ class TestEmissiveMaterial(TestMaterialProperty):
                                 (0.5,0.0,0.0))
         
     def n_check_data(self, n_data):
-        TestMaterialProperty.n_check_data(self, n_data)
         n_geom = n_data.roots[0].children[0]
         self.n_check_material_emissive_property(n_geom.properties[0])
+        self.n_check_material_property(n_geom.properties[0])
 
     def n_check_material_emissive_property(self, n_mat_prop):
         #TODO - Refer to header
@@ -180,9 +180,9 @@ class TestGlossProperty(TestMaterialProperty):
         nose.tools.assert_equal(b_mat.specular_hardness, 100)
 
     def n_check_data(self, n_data):
-        TestMaterialProperty.n_check_data(self, n_data)
         n_geom = n_data.roots[0].children[0]
         self.n_check_material_gloss_property(n_geom.properties[0])
+        self.n_check_material_property(n_geom.properties[0])
 
     def n_check_material_gloss_property(self, n_mat_prop):
         #n_gloss = 4/b_gloss
@@ -223,7 +223,7 @@ class TestSpecularProperty(TestMaterialProperty):
     def b_create_object(self):
         b_obj = TestMaterialProperty.b_create_object(self)
         b_mat = b_obj.data.materials[0]
-        b_mat.specular_color = (1.0,0.0,0.0)
+        b_mat.specular_color = (0.5,0.0,0.0)
         b_mat.specular_intensity = 1.0
         
         #bpy.ops.wm.save_mainfile(filepath="test/autoblend/" + self.n_name)
@@ -239,18 +239,24 @@ class TestSpecularProperty(TestMaterialProperty):
         nose.tools.assert_equal((b_mat.specular_color.r,
                                  b_mat.specular_color.g,
                                  b_mat.specular_color.b),
-                                 (1.0,0.0,0.0))
+                                 (0.5,0.0,0.0))
         nose.tools.assert_equal(b_mat.specular_intensity, 1.0)
     
     def n_check_data(self, n_data):
-        TestMaterialProperty.n_check_data(self, n_data)
         n_geom = n_data.roots[0].children[0]    
         nose.tools.assert_equal(n_geom.num_properties, 2)
         self.n_check_specular_property(n_geom.properties[0])
-        self.n_check_material_property(n_geom.properties[1])
+        self.n_check_material_specular_color(n_geom.properties[1])
     
     def n_check_specular_property(self, n_mat_prop):
         nose.tools.assert_is_instance(n_mat_prop, NifFormat.NiSpecularProperty)
+        
+    def n_check_material_specular_color(self, n_mat_prop):
+        nose.tools.assert_equal((n_mat_prop.specular_color.r,
+                                 n_mat_prop.specular_color.g,
+                                 n_mat_prop.specular_color.b),
+                                 (0.5,0.0,0.0))
+        
         #TODO Check Prop settings
         
 class TestAlphaProperty(TestMaterialProperty):
@@ -273,9 +279,9 @@ class TestAlphaProperty(TestMaterialProperty):
         self.b_check_alpha_property(b_mat)
     
     def b_check_alpha_property(self, b_mat):
-         nose.tools.assert_equal(b_mat.use_transparency, True)
-         nose.tools.assert_equal(b_mat.alpha, 0.5)
-         nose.tools.assert_equal(b_mat.transparency_method, 'Z_TRANSPARENCY')
+        nose.tools.assert_equal(b_mat.use_transparency, True)
+        nose.tools.assert_equal(b_mat.alpha, 0.5)
+        nose.tools.assert_equal(b_mat.transparency_method, 'Z_TRANSPARENCY')
         
     def n_check_data(self, n_data):
         n_geom = n_data.roots[0].children[0]    
