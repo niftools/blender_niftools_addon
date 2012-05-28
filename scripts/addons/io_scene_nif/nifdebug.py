233835aecaae63b919dc080f2ed4c4f7664b302e
neomonkeus <abominalmonkey@gmail.com>
2012-05-28 15:38:29 +0100

    Added license to script files. Added some more .ignore files. Fixed nifdebug import.

--- scripts/addons/io_scene_nif/nifdebug.py
+++ scripts/addons/io_scene_nif/nifdebug.py
@@ -1,3 +1,42 @@
+'''Debugging module used by pyDev'''
+
+# ***** BEGIN LICENSE BLOCK *****
+# 
+# Copyright © 2005-2012, NIF File Format Library and Tools contributors.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+# 
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+# 
+#    * Neither the name of the NIF File Format Library and Tools
+#      project nor the names of its contributors may be used to endorse
+#      or promote products derived from this software without specific
+#      prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# ***** END LICENSE BLOCK *****
+
 import sys
 
 DEBUGGING = False
@@ -5,6 +44,7 @@ DEBUGGING = False
 PYDEV_SOURCE_DIR = 'C:\DevTools\eclipse\plugins\org.python.pydev_2.5.0.2012040618\PySrc'
 
 def startdebug():
+    '''Used as the insertion point for the Eclipse debug server to connect to Blender debug server'''
     
     if DEBUGGING == True:
         # test if PYDEV_SOURCE_DIR already in sys.path, otherwise append it
--- scripts/addons/io_scene_nif/properties.py
+++ scripts/addons/io_scene_nif/properties.py
@@ -1,5 +1,41 @@
-'''Nif Properties'''
-'''Make custom nif properties accessible via Blender object types''' 
+'''Nif Properties, adds custom properties via Blender types''' 
+
+# ***** BEGIN LICENSE BLOCK *****
+# 
+# Copyright © 2005-2012, NIF File Format Library and Tools contributors.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+# 
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+# 
+#    * Neither the name of the NIF File Format Library and Tools
+#      project nor the names of its contributors may be used to endorse
+#      or promote products derived from this software without specific
+#      prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# ***** END LICENSE BLOCK *****
 
 import bpy
 from bpy.props import (PointerProperty, 
@@ -32,6 +68,8 @@ def underscore_to_camelcase(s):
     return ''.join(camelcase_words(s.split('_')))
 
 class NiftoolsMaterialProps(bpy.types.PropertyGroup):
+    '''Adds custom properties to material'''
+    
     @classmethod
     def register(cls):
         bpy.types.Material.niftools = PointerProperty(
@@ -74,6 +112,7 @@ class NiftoolsObjectProps(bpy.types.PropertyGroup):
 
 
 class NiftoolsObjectCollisionProps(bpy.types.PropertyGroup):
+    '''Group of Havok related properties, which gets attached to objects through a property pointer.'''
     @classmethod
     def register(cls):
 
