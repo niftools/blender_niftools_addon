659ec0da3b7cb996125c6455463f9955725cf2a9
neomonkeus <neomonkeus@gmail.com>
2012-07-17 19:15:25 +0100

    Sorted problem with install script not installing all the files correctly. Debugger updated to allow script to continue to run without a debug server running.

--- scripts/addons/io_scene_nif/nifdebug.py
+++ scripts/addons/io_scene_nif/nifdebug.py
@@ -1,18 +1,16 @@
 import sys
 
-DEBUGGING = True
 # set the PYDEV_SOURCE_DIR correctly before using the debugger
 PYDEV_SOURCE_DIR = 'C:\Program Files\eclipse\plugins\org.python.pydev.debug_2.5.0.2012040618\pysrc'
 
 def startdebug():
     
-    if DEBUGGING == True:
-        # test if PYDEV_SOURCE_DIR already in sys.path, otherwise append it
-        if sys.path.count(PYDEV_SOURCE_DIR) < 1:
-            sys.path.append(PYDEV_SOURCE_DIR)
+    # test if PYDEV_SOURCE_DIR already in sys.path, otherwise append it
+    if sys.path.count(PYDEV_SOURCE_DIR) < 1:
+        sys.path.append(PYDEV_SOURCE_DIR)
         
-        # import pydevd module
-        import pydevd
+    # import pydevd module
+    import pydevd
         
-        # set debugging enabled
-        pydevd.settrace(None, True, True, 5678, False, False)
+    # set debugging enabled
+    pydevd.settrace(None, True, True, 5678, False, False)
