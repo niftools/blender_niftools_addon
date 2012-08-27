a8d49b3f54a6a1b5e4308472642c5ab8646c6435
Amorilia <amorilia@users.sourceforge.net>
2012-08-27 15:11:45 +0100

    Fix install script for Windows 7: install to APPDATABLENDERADDONS.

--- install.bat
+++ install.bat
@@ -1,21 +1,21 @@
 @rem quick and dirty script to install the blender nif scripts
-@rem to detect BLENDERADDONS, you can use https://github.com/neomonkeus/buildenv
+@rem to detect APPDATABLENDERADDONS, you can use https://github.com/neomonkeus/buildenv
 @echo off
 
-if "%BLENDERADDONS%" == "" goto pleasesetblenderaddons
+if "%APPDATABLENDERADDONS%" == "" goto pleasesetblenderaddons
 
 echo.Installing to:
-echo.%BLENDERADDONS%\io_scene_nif
+echo.%APPDATABLENDERADDONS%\io_scene_nif
 
 rem remove old files
-rmdir /s /q "%BLENDERADDONS%\io_scene_nif"
+if exist "%APPDATABLENDERADDONS%\io_scene_nif" rmdir /s /q "%APPDATABLENDERADDONS%\io_scene_nif"
 
 rem create root folder
-mkdir "%BLENDERADDONS%\io_scene_nif"
+mkdir "%APPDATABLENDERADDONS%\io_scene_nif"
 
 rem add sub-folders
 for %%A in ( 
-collisionsys, armaturesys, texturesys, materialsys, ) do mkdir "%BLENDERADDONS%\io_scene_nif\%%A"
+collisionsys, armaturesys, texturesys, materialsys, ) do mkdir "%APPDATABLENDERADDONS%\io_scene_nif\%%A"
 
 rem copy files from repository to blender addons folder
 for %%A in ( 
@@ -24,15 +24,15 @@ nif_common.py, nif_export.py, nif_import.py, nif_debug,
 ui.py, properties.py, operators.py, 
 collisionsys, armaturesys, texturesys, materialsys, ) do (
   echo.	%%A
-  copy "scripts\addons\io_scene_nif\%%A" "%BLENDERADDONS%\io_scene_nif\%%A"
+  copy "scripts\addons\io_scene_nif\%%A" "%APPDATABLENDERADDONS%\io_scene_nif\%%A"
 )
 
 goto end
 
 :pleasesetblenderaddons
-echo.Please set BLENDERADDONS to the folder where the blender addons reside, such as:
+echo.Please set APPDATABLENDERADDONS to the folder where the blender addons reside, such as:
 echo.
-echo.  set BLENDERADDONS=C:\Program Files\Blender Foundation\Blender\2.62\scripts\addons
+echo.  set APPDATABLENDERADDONS=%APPDATA%\Blender Foundation\Blender\2.63\scripts\addons
 echo.
 pause
 goto end
