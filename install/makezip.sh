a939ad429d57596f5a0313a9eda0d5cd800ed134
Amorilia <amorilia@users.sourceforge.net>
2012-10-27 16:45:20 +0100

    Simplify release script.

--- install/makezip.sh
+++ install/makezip.sh
@@ -1,9 +1,7 @@
+# clean everything up
+pushd ..
 git clean -xfd
-./install.sh
-
-VERSION=`cat ../scripts/addons/io_scene_nif/VERSION`
-NAME="blender_nif_plugin"
-FILES="AUTHORS.rst CHANGELOG.rst LICENSE.rst README.rst install/install.sh install/install.bat scripts/ docs/_build/html/"
+popd
 
 # update documentation
 pushd ../docs
@@ -11,6 +9,7 @@ make clean
 make html
 popd
 
+# create release zip and exe
 pushd ..
 python3 setup.py sdist --format=zip
 python3 setup.py --command-packages bdist_nsi bdist_nsi --blender-addon=2.62
