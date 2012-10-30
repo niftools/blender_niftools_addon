7c3b427cc3e3b588858c32921a16323904106cd2
Amorilia <amorilia@users.sourceforge.net>
2012-10-30 22:36:37 +0000

    Trivial fixes.

--- install/makezip.bat
+++ install/makezip.bat
@@ -1,10 +1,13 @@
+@echo off
+
 set /p VERSION=<../scripts/addons/io_scene_nif/VERSION
 set NAME=blender_nif_plugin
 set EXTRAFILES=AUTHORS.rst CHANGELOG.rst LICENSE.rst README.rst
 
-pushd ..\scripts\addons
 del %NAME%-%VERSION%.*
-"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip ..\..\install\%NAME%-%VERSION%.zip io_scene_nif -xr!__pycache__
+
+pushd ..\scripts\addons
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip ..\..\install\%NAME%-%VERSION%.zip -xr!__pycache__ io_scene_nif 
 popd
 
 pushd ..
