02bba3771510a962ad5a6f35d07f195dc5a4f26e
Amorilia <amorilia@users.sourceforge.net>
2012-10-30 22:08:51 +0000

    Ditch nsis installer, simply produce zip.

--- install/makezip.sh
+++ install/makezip.sh
@@ -14,9 +14,4 @@ popd
 pushd ..
 rm -f "${NAME}-${VERSION}".*
 zip -9r "install/${NAME}-${VERSION}.zip" ${FILES} -x \*/__pycache__/\*
-tar cfvj "install/${NAME}-${VERSION}.tar.bz2" ${FILES} --exclude=*__pycache__*
 popd
-
-# create windows installer
-rm -f "${NAME}-${VERSION}-windows.exe"
-makensis -V3 -DVERSION=${VERSION} ${NAME}.nsi
