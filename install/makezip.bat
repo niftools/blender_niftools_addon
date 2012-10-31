9ce315528f76ac250b72adbdf6c5150959b5b227
Amorilia <amorilia@users.sourceforge.net>
2012-10-31 21:18:53 +0000

    Update zip script.

--- install/makezip.bat
+++ install/makezip.bat
@@ -1,17 +1,12 @@
 @echo off
 
-set /p VERSION=<../scripts/addons/io_scene_nif/VERSION
+set /p VERSION=<../io_scene_nif/VERSION
 set NAME=blender_nif_plugin
-set EXTRAFILES=AUTHORS.rst CHANGELOG.rst LICENSE.rst README.rst
 
 del %NAME%-%VERSION%.*
 
-pushd ..\scripts\addons
-"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip ..\..\install\%NAME%-%VERSION%.zip -xr!__pycache__ io_scene_nif 
-popd
-
 pushd ..
-"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip install\%NAME%-%VERSION%.zip %EXTRAFILES%
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip install\%NAME%-%VERSION%.zip -xr!__pycache__ io_scene_nif 
 popd
 
 pause
