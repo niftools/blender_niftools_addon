3508169258c0b228d03660b2654b97a9684ae097
Amorilia <amorilia@users.sourceforge.net>
2012-11-01 16:23:06 +0000

    Update batch files (untested, probably broken, but the gist should be ok).

--- install/makezip.bat
+++ install/makezip.bat
@@ -6,7 +6,21 @@ set NAME=blender_nif_plugin
 del %NAME%-%VERSION%.*
 
 pushd ..
-"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip install\%NAME%-%VERSION%.zip -xr!__pycache__ io_scene_nif
+git clean -xfd
+xcopy /s pyffi/pyffi io_scene_nif
+xcopy pyffi/*.rst io_scene_nif/pyffi
+del /s /f /q io_scene_nif/pyffi/formats/cgf
+del /s /f /q  io_scene_nif/pyffi/formats/dae
+del /s /f /q  io_scene_nif/pyffi/formats/psk
+del /s /f /q  io_scene_nif/pyffi/formats/rockstar
+del /s /f /q  io_scene_nif/pyffi/formats/tga
+del /s /f /q  io_scene_nif/pyffi/qskope
+copy AUTHORS.rst io_scene_nif
+copy CHANGELOG.rst io_scene_nif
+copy LICENSE.rst io_scene_nif
+copy README.rst io_scene_nif
+"%PROGRAMFILES%\7-Zip\7z.exe" a -tzip install\%NAME%-%VERSION%.zip -xr!__pycache__ -xr!.git -xr!.project -xr!fileformat.dtd io_scene_nif
+del /s /f /q io_scene_nif/pyffi
 popd
 
 pause
