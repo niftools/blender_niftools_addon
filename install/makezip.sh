3052a939cab4a32247b62d3787432126599b82f6
Amorilia <amorilia@users.sourceforge.net>
2012-11-01 16:07:44 +0000

    Include rst files with zip.

--- install/makezip.sh
+++ install/makezip.sh
@@ -13,6 +13,10 @@ rm -r io_scene_nif/pyffi/formats/psk
 rm -r io_scene_nif/pyffi/formats/rockstar
 rm -r io_scene_nif/pyffi/formats/tga
 rm -r io_scene_nif/pyffi/qskope
+cp AUTHORS.rst io_scene_nif
+cp CHANGELOG.rst io_scene_nif
+cp LICENSE.rst io_scene_nif
+cp README.rst io_scene_nif
 zip -9r "${DIR}/${NAME}-${VERSION}.zip" io_scene_nif -x \*/__pycache__/\* -x \*/.git\* -x \*/.project -x \*.dtd
 rm -r io_scene_nif/pyffi
 popd
