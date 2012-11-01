73035f6b8cb2361502507d2ff4714b119ec6b5c0
Amorilia <amorilia@users.sourceforge.net>
2012-11-01 13:01:54 +0000

    Update makezip and install scripts to include pyffi.

--- install/makezip.sh
+++ install/makezip.sh
@@ -1,11 +1,17 @@
-git clean -xfd
-./install.sh
+#!/bin/sh
 
 VERSION=`cat ../io_scene_nif/VERSION`
 NAME="blender_nif_plugin"
 
-rm -f "${NAME}-${VERSION}".*
-
 pushd ..
-zip -9r "install/${NAME}-${VERSION}.zip" io_scene_nif -x \*/__pycache__/\*
+git clean -xfd
+cp -r pyffi/pyffi io_scene_nif
+rm -r io_scene_nif/pyffi/formats/cgf
+rm -r io_scene_nif/pyffi/formats/dae
+rm -r io_scene_nif/pyffi/formats/psk
+rm -r io_scene_nif/pyffi/formats/rockstar
+rm -r io_scene_nif/pyffi/formats/tga
+rm -r io_scene_nif/pyffi/qskope
+zip -9r "install/${NAME}-${VERSION}.zip" io_scene_nif -x \*/__pycache__/\* -x \*/.git\* -x \*/.project -x \*.dtd
+rm -r io_scene_nif/pyffi
 popd
