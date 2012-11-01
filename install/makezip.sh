f6c38c277da07630af878e91aad14ebccae880ee
Amorilia <amorilia@users.sourceforge.net>
2012-11-01 16:56:34 +0000

    Use temp folder to create install package.

--- install/makezip.sh
+++ install/makezip.sh
@@ -3,21 +3,30 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 VERSION=`cat ${DIR}/../io_scene_nif/VERSION`
 NAME="blender_nif_plugin"
+ROOT=${DIR}/..
 
-pushd "${DIR}/.."
+pushd $ROOT
 git clean -xfd
-cp -r pyffi/pyffi io_scene_nif
-cp pyffi/*.rst io_scene_nif/pyffi
+git submodule foreach --recursive git clean -xfd
+popd
+
+mkdir ${DIR}/temp
+
+pushd ${DIR}/temp
+cp -r ${ROOT}/io_scene_nif/ .
+cp -r ${ROOT}/pyffi/pyffi/ io_scene_nif
+cp ${ROOT}/pyffi/*.rst io_scene_nif/pyffi
 rm -r io_scene_nif/pyffi/formats/cgf
 rm -r io_scene_nif/pyffi/formats/dae
 rm -r io_scene_nif/pyffi/formats/psk
 rm -r io_scene_nif/pyffi/formats/rockstar
 rm -r io_scene_nif/pyffi/formats/tga
 rm -r io_scene_nif/pyffi/qskope
-cp AUTHORS.rst io_scene_nif
-cp CHANGELOG.rst io_scene_nif
-cp LICENSE.rst io_scene_nif
-cp README.rst io_scene_nif
+cp ${ROOT}/AUTHORS.rst io_scene_nif
+cp ${ROOT}/CHANGELOG.rst io_scene_nif
+cp ${ROOT}/LICENSE.rst io_scene_nif
+cp ${ROOT}/README.rst io_scene_nif
 zip -9r "${DIR}/${NAME}-${VERSION}.zip" io_scene_nif -x \*/__pycache__/\* -x \*/.git\* -x \*/.project -x \*/fileformat.dtd
-rm -r io_scene_nif/pyffi
 popd
+
+rm -r ${DIR}/temp
