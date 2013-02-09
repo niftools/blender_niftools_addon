09ff7b58ecc064da25a65bbe9b897da069f4d3ba
neomonkeus <neomonkeus@gmail.com>
2013-02-09 21:33:05 +0000

    Minor path change, swaps 'base_' for 'test_'

--- testframework/test/property/material/n_gen_material.py
+++ testframework/test/property/material/n_gen_material.py
@@ -7,18 +7,8 @@ def n_attach_material_prop(n_trishapedata):
     '''Attaches a NiMaterialProperty to a trishapedata block property's array at pos[0]'''
     
     n_nimaterialprop = NifFormat.NiMaterialProperty()
-
-    # add property to top of list
-    n_trishapedata.properties.reverse()
-
-    n_trishapedata.num_properties += 1
-    n_trishapedata.properties.update_size()
-    n_trishapedata.properties[-1] = n_nimaterialprop
-
-    n_trishapedata.properties.reverse()
-
     n_nimaterialprop.name = b'Material'
-    
+   
     with ref(n_nimaterialprop.ambient_color) as n_color3:
         n_color3.r = 1.0
         n_color3.g = 1.0
@@ -41,6 +31,13 @@ def n_attach_material_prop(n_trishapedata):
     
     n_nimaterialprop.glossiness = 12.5 # default nif.xml - 0.0, blender - 12.5
     n_nimaterialprop.alpha = 1.0 # default nif.xml - 0.0
+    
+    # add property to top of list
+    n_trishapedata.properties.reverse()
+    n_trishapedata.num_properties += 1
+    n_trishapedata.properties.update_size()
+    n_trishapedata.properties[-1] = n_nimaterialprop
+    n_trishapedata.properties.reverse()
 
 def n_alter_glossiness(n_nimaterialprop):
     n_nimaterialprop.glossiness = 25.0
