7bba6cc765826c5b55f3151f59527be61a413969
neomonkeus <neomonkeus@gmail.com>
2013-03-06 18:23:45 +0000

    Added blender side tests for texture slots and solution to old path checking problem.

--- testframework/test/textures/b_gen_texture.py
+++ testframework/test/textures/b_gen_texture.py
@@ -1,3 +1,5 @@
+from os import path
+
 import bpy
 import math
 import mathutils
@@ -18,18 +20,24 @@ def b_create_load_texture(b_mat_texslot, texture_path):
 def b_create_diffuse_texture_properties(b_mat_texslot):
     b_mat_texslot.use_map_color_diffuse = True
     
-def b_check_texture_slot(b_mat):
-
-    nose.tools.assert_equal(b_mat.texture_slots[0] != None, True) # check slot exists
-    b_mat_texslot = b_mat.texture_slots[0]
+def b_check_texture_slot(b_mat_texslot):
     nose.tools.assert_equal(b_mat_texslot.use, True) # check slot enabled
-    self.b_check_texture_property(b_mat_texslot)
-
-def b_check_texture_property(self, b_mat_texslot):
-    nose.tools.assert_is_instance(b_mat_texslot.texture, bpy.types.ImageTexture)
-    #nose.tools.assert_equal(b_mat_texslot.texture.image.filepath, self.diffuse_texture_path)
     nose.tools.assert_equal(b_mat_texslot.texture_coords, 'UV')
     nose.tools.assert_equal(b_mat_texslot.uv_layer, 'UVMap')
+    
+def b_check_image_texture_property(b_mat_texslot, texture_path):
+    
+    nose.tools.assert_is_instance(b_mat_texslot.texture, bpy.types.ImageTexture) #check we have a texture
+
+    n_split_path = texture_path.split(path.sep)
+    n_rel_path = n_split_path[len(n_split_path)-2:] #get a path relative to \\textures folder
+    
+    b_split_path = b_mat_texslot.texture.image.filepath.split(path.sep) #see if nif loaded correct path
+    b_rel_path = b_split_path[len(b_split_path)-2:]
+    
+    nose.tools.assert_equal(b_rel_path, n_rel_path) #see if we have the right images
+    
+def b_check_diffuse_texture_settings(b_mat_texslot):
     nose.tools.assert_equal(b_mat_texslot.use_map_color_diffuse, True)
 
    
