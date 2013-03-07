f34d50b8537a35d532c932a211c1e8f94bc07287
neomonkeus <neomonkeus@gmail.com>
2013-03-07 17:19:45 +0000

    Generalised texture creation and refactored specific map implementations to match organisation structure.

--- testframework/test/textures/n_gen_texture.py
+++ testframework/test/textures/n_gen_texture.py
@@ -317,23 +317,7 @@ def n_create_texture_property(n_trishape):
     n_trishape.num_properties += 1
     n_trishape.properties.update_size()
     n_trishape.properties[-1] = n_nitexturingproperty
-    n_trishape.properties.reverse()
-  
-    
-def n_create_diffuse_texture(n_nitexturingproperty):
-    """Adds a diffuse texture to a NiTexturingProperty"""
-    
-    n_nitexturingproperty.has_base_texture = True
-    
-    n_nisourcetexture = NifFormat.NiSourceTexture()  
-    file_path = 'textures' + path.sep + 'diffuse' + path.sep + 'diffuse.dds'
-    n_nisourcetexture.file_name = file_path.encode()
-    n_nisourcetexture.pixel_layout = 6
-    n_nisourcetexture.use_mipmaps = 1
-    
-    with ref(n_nitexturingproperty.base_texture) as n_texdesc:
-        n_texdesc.source = n_nisourcetexture
-     
+    n_trishape.properties.reverse()   
         
 def n_create_store_normal_data(n_nitrishape):
     """Store normal data as BinaryExtraData"""
@@ -358,23 +342,3 @@ def n_check_texdesc(n_tex_desc):
     nose.tools.assert_equal(n_tex_desc.filter_mode, 2)
     nose.tools.assert_equal(n_tex_desc.uv_set, 0)
     nose.tools.assert_equal(n_tex_desc.has_texture_transform, False)
-    
-def n_check_diffuse_property(n_tex_prop):
-    nose.tools.assert_is_instance(n_tex_prop, NifFormat.NiTexturingProperty)
-    nose.tools.assert_equal(n_tex_prop.has_base_texture, True)
-    
-def n_check_diffuse_source_texture(n_nisourcetexture, n_texture_path):
-    nose.tools.assert_is_instance(n_nisourcetexture, NifFormat.NiSourceTexture)
-    
-    n_split_path = n_texture_path.split(path.sep)
-    n_rel_path = n_split_path[len(n_split_path)-3:] #get a path relative to \\textures folder
-    
-    n_src_path = n_nisourcetexture.file_name.decode()
-    n_src_path = n_src_path.split(path.sep)
-
-    nose.tools.assert_equal(n_src_path, n_rel_path)
-    nose.tools.assert_equal(n_nisourcetexture.pixel_layout, NifFormat.PixelLayout.PIX_LAY_DEFAULT) # 6
-    nose.tools.assert_equal(n_nisourcetexture.use_mipmaps, NifFormat.MipMapFormat.MIP_FMT_YES) # 1
-    nose.tools.assert_equal(n_nisourcetexture.use_external, 1)
-    
-    
