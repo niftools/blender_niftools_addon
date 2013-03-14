39d0393bc54fe803919c74ddc4b0d85fb74507ac
neomonkeus <neomonkeus@gmail.com>
2013-03-14 20:10:05 +0000

    Separated out reusable collision components and started work on BhkBoxShape based collisions.

--- testframework/test/collisions/bhkshape/bhkboxshape/test_boxshape.py
+++ testframework/test/collisions/bhkshape/bhkboxshape/test_boxshape.py
@@ -9,8 +9,10 @@ from test import SingleNif
 from test.data import gen_data
 from test.geometry.trishape import b_gen_geometry
 from test.geometry.trishape import n_gen_geometry
-from test.collisions.bhkshape import b_gen_bhkboxshape
-from test.collisions.bhkshape import n_gen_bhkboxshape
+from test.collisions.bhkshape import b_gen_collision
+from test.collisions.bhkshape import n_gen_collision
+from test.collisions.bhkshape.bhkboxshape import b_gen_bhkboxshape
+from test.collisions.bhkshape.bhkboxshape import n_gen_bhkboxshape
 
 class TestCollisionBhkBoxShape(SingleNif):
     """Test material property"""
@@ -24,15 +26,21 @@ class TestCollisionBhkBoxShape(SingleNif):
         
         #Mesh obj
         b_obj = b_gen_geometry.b_create_base_geometry(self.b_name)
-        
+                
         #Col obj
-        b_col = b_gen_geometry.b_create_cube(self.b_col_name)
-
+        b_col_obj = b_gen_geometry.b_create_cube(self.b_col_name)
+        b_gen_geometry.b_scale_object()
+        b_col_obj.matrix_local = b_gen_geometry.b_get_transform_matrix()
+        b_gen_collision.b_create_default_collision_properties(b_col_obj)
+        b_gen_bhkboxshape.b_create_bhkboxshape_properties(b_col_obj)
+        
     def b_check_data(self):
         b_obj = bpy.data.objects[self.b_name]
         b_gen_geometry.b_check_geom_obj(b_obj)
-
-
+        
+        b_col_obj = bpy.data.objects[self.b_col_name]
+        b_gen_collision.b_check_default_collision_properties(b_col_obj)
+        b_gen_bhkboxshape.b_check_bhkboxshape_properties(b_col_obj)
     
     def n_create_data(self):
         gen_data.n_create_header(self.n_data)
@@ -47,28 +55,8 @@ class TestCollisionBhkBoxShape(SingleNif):
         n_gen_geometry.n_check_trishape(n_nitrishape)
         
         nose.tools.assert_equal(n_nitrishape.num_properties, 0)
-        
-class TestBhkCollisionBoxShape(TestBaseGeometry, TestBhkCollision):
-    n_name = "collisions/base_bhkcollision_box" #name of nif
-    b_name = "Cube" #name of blender mesh object
+    
 
-    def b_create_object(self):
-        b_obj = TestBaseGeometry.b_create_object(self)
-        b_coll = TestBaseGeometry.b_create_object(self, "CollisionBox")
-        b_coll.draw_type = 'WIRE'
-
-        #Physics
-        b_coll.game.use_collision_bounds = True
-        b_coll.game.collision_bounds_type = 'BOX'
-
-        b_coll.nifcollision.use_blender_properties = True
-        b_coll.nifcollision.motion_system = "MO_SYS_FIXED"
-        b_coll.nifcollision.oblivion_layer = "OL_STATIC"
-        b_coll.nifcollision.quality_type = "MO_QUAL_FIXED"
-        b_coll.nifcollision.col_filter = 0
-        b_coll.nifcollision.havok_material = "HAV_MAT_WOOD"
-
-        b_coll.select = True
 
 
 
