cce51070321560b764723008e5d7fce28dad50f9
Ghost <Ghostwalker_71@hotmail.com>
2013-04-10 00:34:09 -0500

    Fixed error introduced during parenting decipher

--- io_scene_nif/nif_import.py
+++ io_scene_nif/nif_import.py
@@ -436,7 +436,7 @@ class NifImport(NifCommon):
                 geom_group = []
             elif self.armaturehelper.is_bone(niBlock):
                 # bones have already been imported during import_armature
-                b_obj = b_armature.data.edit_bones[self.names[niBlock]]
+                b_obj = b_armature.data.bones[self.names[niBlock]]
                 # bones cannot group geometries into a single mesh
                 geom_group = []
             else:
@@ -535,7 +535,7 @@ class NifImport(NifCommon):
                 for (n_child, b_child) in object_children:
                     b_child.parent = b_obj
 
-            elif isinstance(b_obj, bpy.types.EditBone):
+            elif isinstance(b_obj, bpy.types.Bone):
                 # bone parentship, is a bit more complicated
                 # go to rest position
                 
