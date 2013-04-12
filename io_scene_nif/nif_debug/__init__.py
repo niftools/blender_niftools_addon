352610050eec63947dd4f45710789e1f7308f9bf
neomonkeus <neomonkeus@gmail.com>
2013-04-12 16:35:35 +0100

    Initial attempt at removing hard coded python debugger location.

--- io_scene_nif/nif_debug/__init__.py
+++ io_scene_nif/nif_debug/__init__.py
@@ -1,19 +1,20 @@
 import sys
+import os
+
+PYDEV_SOURCE_DIR = ""
 
 def startdebug():
+
     try:
-        # set the PYDEV_SOURCE_DIR correctly before using the debugger
-        PYDEV_SOURCE_DIR = 'C:\Program Files\eclipse\plugins\org.python.pydev.debug_2.5.0.2012040618\pysrc'
+        PYDEV_SOURCE_DIR = os.environ['PYDEV_SOURCE_DIR']
         
-        # test if PYDEV_SOURCE_DIR already in sys.path, otherwise append it
-        if sys.path.count(PYDEV_SOURCE_DIR) < 1:
+        if sys.path.count(PYDEV_SOURCE_DIR) > 0:
             sys.path.append(PYDEV_SOURCE_DIR)
-            
-        # import pydevd module
+             
         import pydevd
-            
-        # set debugging enabled
         pydevd.settrace(None, True, True, 5678, False, False)
-    except:
-        print("Unable to connect to Remote Debug Server.")
+     
+    except:    
+        print("Unable to connect to Remote debugging server")
+        print("PYDEV_SOURCE_DIR := " + PYDEV_SOURCE_DIR)
         pass
