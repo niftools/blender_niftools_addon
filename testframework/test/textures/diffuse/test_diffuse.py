ed1d0c073649caaf8f077494255a04b2b644a758
neomonkeus <neomonkeus@gmail.com>
2013-04-15 15:54:58 +0100

    Standardised the SingleNif template methods, b_create_objects() -> b_create_data()

--- testframework/test/textures/diffuse/test_diffuse.py
+++ testframework/test/textures/diffuse/test_diffuse.py
@@ -68,7 +68,7 @@ class TestTexturePropertyDiffuseMap(SingleNif):
     
     diffuse_texture_path = os.path.join(nif_dir, 'textures', 'diffuse', 'diffuse.dds')
 
-    def b_create_objects(self):
+    def b_create_data(self):
         b_obj = b_gen_geometry.b_create_cube(self.b_name)
         b_gen_uv.b_uv_object()
         b_gen_geometry.b_transform_cube(b_obj)
