2677ff5df392ddb45a23287147695663e2b48587
neomonkeus <neomonkeus@gmail.com>
2013-04-16 19:54:38 +0100

    Moved set & get bone_extra_matrix_inv methods

--- io_scene_nif/nif_export.py
+++ io_scene_nif/nif_export.py
@@ -65,17 +65,7 @@ class NifExport(NifCommon):
     FLOAT_MAX = +3.4028234663852886e+38
 
 
-    def set_bone_extra_matrix_inv(self, bonename, mat):
-        """Set bone extra matrix, inverted. The bonename is first converted
-        to blender style (to ensure compatibility with older imports).
-        """
-        self.bones_extra_matrix_inv[self.get_bone_name_for_blender(bonename)] = mat
-
-    def get_bone_extra_matrix_inv(self, bonename):
-        """Get bone extra matrix, inverted. The bonename is first converted
-        to blender style (to ensure compatibility with older imports).
-        """
-        return self.bones_extra_matrix_inv[self.get_bone_name_for_blender(bonename)]
+    
 
     def rebuild_full_names(self):
         """Recovers the full object names from the text buffer and rebuilds
