614e2ba4b5213e10ed6cfba174ec81e157e53660
Ghost <Ghostwalker_71@hotmail.com>
2013-06-13 22:22:45 -0500

    Fix for Sub Issue : # 16: Incorrect encoding of bone names.

--- io_scene_nif/armaturesys/armature_import.py
+++ io_scene_nif/armaturesys/armature_import.py
@@ -854,4 +854,5 @@ class Armature():
 		# write the names to the text buffer
 		for block, shortname in self.nif_common.names.items():
 			if block.name and shortname != block.name:
+				block_name = block.name.decode()
 				namestxt.write('%s;%s\n' % (shortname, block.name))
