fb4e38d16d8981e01a2701057bef53ab63978bdc
neomonkeus <neomonkeus@gmail.com>
2013-06-24 00:06:45 +0100

    Mock of unit test for non class method testing. Uses globals so might just convert to a test class.

--- testframework/unit/utility/test_utility.py
+++ testframework/unit/utility/test_utility.py
@@ -1,9 +1,26 @@
 import nose
+from nose import with_setup
+
 import pyffi
+from pyffi.formats.nif import NifFormat
 
 import io_scene_nif
+from io_scene_nif.utility import utilities
 
-from io_scene_nif import nif_import
+niBlock = NifFormat.NiNode()
+obj = ""
 
-def test():
-    nose.tools.assert_equal(True, True)
+def setup_func():
+    print("\nSetup")
+    global obj 
+    obj = "object setup complete"
+    
+def teardown_func():
+    print("Tear Down")
+
+@with_setup(setup_func, teardown_func)
+def test_import_matrix():
+    print("Running ")
+    print(obj)
+    pass
+    #def import_matrix(self, niBlock, relative_to=None):
