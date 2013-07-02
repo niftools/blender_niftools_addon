efded7e35e5428044e8387114dda7b6c9855840a
neomonkeus <neomonkeus@gmail.com>
2013-07-02 21:01:26 +0100

    Added missing fix for import_matrix utility.

--- testframework/unit/utility/test_utility.py
+++ testframework/unit/utility/test_utility.py
@@ -12,28 +12,8 @@ import mathutils
 import math
 
 '''
-Rotation Matrix(X)
-OpenGL - LHS                  DirectX
-1    0        0               1     0        0  
-0    cos(30)  -sin(30)   ->   0     cos(30)  sin(30)
-0    sin(30)   cos(30)        0    -sin(30)  cos(30)
 
-Memory:
-[  0][  1][  2][  3] [  4][  5][  6][  7] [  8][  9][ 10][ 11] [ 12][ 13][ 14][ 15] 
-
-DirectX
-[m11][m21][m31][m41] [m12][m22][m32][m42] [m13][m23][m33][m43] [m14][m24][m34][m44] 
-
-OpenGL
-[0,0][1,0][2,0][3,0] [0,1][1,1][2,1][3,1] [0,2][1,2][2,2][2,3] [0,3][1,3][2,3][3,3] 
-
-Access                  Repr                      Access              Repr
-[m11][m12][m13][m14]    [  1][   0][   0][   ]    [00][01][02][03]    [  1][  0][   0][ x]
-[m21][m22][m23][m24]    [  0][ cos][ sin][   ]    [10][11][12][13]    [  0][cos][-sin][ y]
-[m31][m32][m33][m34]    [  0][-sin][ cos][   ]    [20][21][22][23]    [  0][sin][ cos][ z]
-[m41][m42][m43][m44]    [ t1][  t2][  t3][ t4]    [30][31][32][33]    [   ][   ][    ][ w]
 '''
-
 class Test_Utilites:
 
     @classmethod
@@ -93,6 +73,7 @@ class Test_Utilites:
     def test_import_translation_matrix(self):
         converted_mat = utilities.import_matrix(self, self.niBlock)
         
+        print("Comparing Matrices:")
         for row in range(0,4):
             for col in range(0,4):
                 nose.tools.assert_true(converted_mat[row][col] - self.b_mat[row][col] 
