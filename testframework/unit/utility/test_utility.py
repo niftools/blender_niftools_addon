9407c5c728cb681c3c9684f8850a4cdcb3aa5445
neomonkeus <neomonkeus@gmail.com>
2013-08-01 01:45:55 +0100

    Reverts old matrix integration test code for geometry tests.

--- testframework/unit/utility/test_utility.py
+++ testframework/unit/utility/test_utility.py
@@ -17,11 +17,11 @@ class Test_Utilites:
     def setUpClass(cls):
         print("Setup" + str(cls))
         
-        cls.nif_matrix = self.build_nif_matrix()
+        cls.nif_matrix = cls.build_nif_matrix()
         cls.niBlock = NifFormat.NiNode()
         cls.niBlock.set_transform(cls.nif_matrix)
         
-        cls.blender_matrix= self.build_blender_matrix()
+        cls.blender_matrix = cls.build_blender_matrix()
         
     @classmethod
     def tearDownClass(cls):
@@ -37,13 +37,16 @@ class Test_Utilites:
         for row in range(0,4):
             for col in range(0,4):
                 print(str(row) + ":" + str(col) + " = " + 
-                      str(converted_mat[row][col]) + " : " + str(self.b_mat[row][col]))
-                nose.tools.assert_true(converted_mat[row][col] - self.b_mat[row][col] 
+                      str(converted_mat[row][col]) + " : " + str(self.blender_matrix[row][col]))
+                nose.tools.assert_true(converted_mat[row][col] - self.blender_matrix[row][col] 
                                          < NifFormat.EPSILON)
                 
                 
     @classmethod
     def build_blender_matrix(cls):
+        translation = (2.0, 3.0, 4.0)
+        scale = 2
+        
         #Blender matrix
         b_loc_vec = mathutils.Vector(translation)
         b_loc_vec = mathutils.Matrix.Translation(b_loc_vec)
@@ -51,19 +54,20 @@ class Test_Utilites:
         b_rot_mat_x = mathutils.Matrix.Rotation(math.radians(30.0), 4, 'X')
         b_rot_mat_y = mathutils.Matrix.Rotation(math.radians(60.0), 4, 'Y')
         b_rot_mat_z = mathutils.Matrix.Rotation(math.radians(90.0), 4, 'Z')
-        b_rot_mat = b_rot_mat_z * b_rot_mat_y * b_rot_mat_x
+        b_rot_mat = b_rot_mat_x * b_rot_mat_y * b_rot_mat_z
         
         b_scale_mat = mathutils.Matrix.Scale(scale, 4)
+        b_matrix = b_scale_mat * b_rot_mat * b_loc_vec 
         
         print("Blender - RHS")
-        print(cls.b_mat)
+        print(b_matrix)
         
-        return b_loc_vec * b_rot_mat * b_scale_mat
-        
-
+        return b_matrix
+    
         
     @classmethod
     def build_nif_matrix(cls):
+        n_mat = NifFormat.Matrix44()
         translation = (2.0, 3.0, 4.0)
         scale = 2
         rhsrotx = (1.0, 0.0, 0.0,
@@ -78,51 +82,31 @@ class Test_Utilites:
                    -1, 0, 0,
                    0, 0, 1)
         
+        rhsrot = (0, -0.8660, 0.5,
+                  0.5,  0.4330, 0.75,
+                  -0.8660, 0.2550, 0.4330)
+        
         n_vec3 = NifFormat.Vector3()
         n_vec3.x = translation[0]
         n_vec3.y = translation[1]
         n_vec3.z = translation[2]
 
-        n_mat33_x = NifFormat.Matrix33()
-        n_mat33_x.m_11 = rhsrotx[0]
-        n_mat33_x.m_12 = rhsrotx[1]
-        n_mat33_x.m_13 = rhsrotx[2]
-        n_mat33_x.m_21 = rhsrotx[3]
-        n_mat33_x.m_22 = rhsrotx[4]
-        n_mat33_x.m_23 = rhsrotx[5]
-        n_mat33_x.m_31 = rhsrotx[6]
-        n_mat33_x.m_32 = rhsrotx[7]
-        n_mat33_x.m_33 = rhsrotx[8]
+        n_mat33 = NifFormat.Matrix33()
+        n_mat33.m_11 = rhsrotx[0]
+        n_mat33.m_12 = rhsrotx[3]
+        n_mat33.m_13 = rhsrotx[6]
+        n_mat33.m_21 = rhsrotx[1]
+        n_mat33.m_22 = rhsrotx[4]
+        n_mat33.m_23 = rhsrotx[7]
+        n_mat33.m_31 = rhsrotx[2]
+        n_mat33.m_32 = rhsrotx[5]
+        n_mat33.m_33 = rhsrotx[8]
         
-        n_mat33_y = NifFormat.Matrix33()
-        n_mat33_y.m_11 = rhsroty[0]
-        n_mat33_y.m_12 = rhsroty[1]
-        n_mat33_y.m_13 = rhsroty[2]
-        n_mat33_y.m_21 = rhsroty[3]
-        n_mat33_y.m_22 = rhsroty[4]
-        n_mat33_y.m_23 = rhsroty[5]
-        n_mat33_y.m_31 = rhsroty[6]
-        n_mat33_y.m_32 = rhsroty[7]
-        n_mat33_y.m_33 = rhsroty[8]
-        
-        n_mat33_z = NifFormat.Matrix33()
-        n_mat33_z.m_11 = rhsrotz[0]
-        n_mat33_z.m_12 = rhsrotz[1]
-        n_mat33_z.m_13 = rhsrotz[2]
-        n_mat33_z.m_21 = rhsrotz[3]
-        n_mat33_z.m_22 = rhsrotz[4]
-        n_mat33_z.m_23 = rhsrotz[5]
-        n_mat33_z.m_31 = rhsrotz[6]
-        n_mat33_z.m_32 = rhsrotz[7]
-        n_mat33_z.m_33 = rhsrotz[8]
-        
-        n_com = n_mat33_x * n_mat33_y * n_mat33_z
-        
-        n_mat = NifFormat.Matrix44()
-        n_mat.set_scale_rotation_translation(scale, n_com, n_vec3)
+
+        n_mat.set_scale_rotation_translation(scale, n_mat33, n_vec3)
         
         print("Building Matrices")
         print("Nif - LHS")
-        print(cls.n_mat)
+        print(n_mat)
         
         return n_mat
