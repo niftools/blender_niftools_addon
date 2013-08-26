c573183e275182bde170a9eb9a7d19a298330aef
Ghost <Ghostwalker_71@hotmail.com>
2013-08-26 14:02:04 -0500

    Reworked envelope check to work in the blender2.68 API

--- io_scene_nif/nif_export.py
+++ io_scene_nif/nif_export.py
@@ -159,15 +159,21 @@ class NifExport(NifCommon):
                     # ensure we get the mesh vertices in animation mode,
                     # and not in rest position!
                     b_obj.data.pose_position = 'POSE'
-                    if (b_obj.data.use_deform_envelopes):
-                        return self.error(
-                            "'%s': Cannot export envelope skinning."
-                            " If you have vertex groups,"
-                            " turn off envelopes. If you don't have vertex"
-                            " groups, select the bones one by one press W"
-                            " to convert their envelopes to vertex weights,"
-                            " and turn off envelopes."
-                            % b_obj.name)
+
+
+                    if b_obj.type == 'MESH':
+                        b_obj_name = b_obj.name
+                        b_obj_parent = b_obj.parent.name
+                        if (bpy.data.objects[b_obj_name].modifiers[b_obj_parent].use_bone_envelopes == True):
+                        
+                            return self.error(
+                                    "'%s': Cannot export envelope skinning."
+                                    " If you have vertex groups,"
+                                    " turn off envelopes. If you don't have vertex"
+                                    " groups, select the bones one by one press W"
+                                    " to convert their envelopes to vertex weights,"
+                                    " and turn off envelopes."
+                                    % b_obj.name)
 
                 # check for non-uniform transforms
                 # (lattices are not exported so ignore them as they often tend
