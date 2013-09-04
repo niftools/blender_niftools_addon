5be65a3c33fc6e668e062b051f1c58578a272700
neomonkeus <neomonkeus@gmail.com>
2013-09-04 23:53:21 +0100

    Updated import and export code to support specular maps. Needs visual testing to see how it renders in the viewport as to what settings should be mapped to. But for now it will allow successful import/export.

--- io_scene_nif/texturesys/texture_export.py
+++ io_scene_nif/texturesys/texture_export.py
@@ -335,7 +335,7 @@ class TextureHelper():
                     self.glowmtex = b_mat_texslot
     
                 #specular
-                elif b_mat_texslot.use_map_specular:
+                elif b_mat_texslot.use_map_color_spec:
                     #multi-check
                     if self.glossmtex:
                         raise nif_utils.NifExportError("Multiple gloss textures in"
