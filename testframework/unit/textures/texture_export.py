a65f489d1247e6bfc4512d86cdb62ce8c8d3d2a1
neomonkeus <neomonkeus@gmail.com>
2013-10-19 04:01:44 +0100

    Improved unit testing for textureslot.use checking for None material. Fixed bug in process.

--- testframework/unit/textures/texture_export.py
+++ testframework/unit/textures/texture_export.py
@@ -1,12 +1,11 @@
 import nose
-from nose.tools import with_setup
 
 import bpy
 
 import io_scene_nif
 from io_scene_nif.texturesys.texture_export import TextureHelper
 
-class Test_TextureHelper_Used_Slots:    
+class Test_TextureHelper:    
         
     def setUp(self):
         self.b_mat = bpy.data.materials.new("test_material")
@@ -16,9 +15,15 @@ class Test_TextureHelper_Used_Slots:
         bpy.data.materials.remove(self.b_mat)
         del self.b_mat
      
+    def test_no_material(self):
+        self.used_textslots = TextureHelper.get_used_textslots(None)
+        print("Used Slots: %s, items = %s" % (self.used_textslots, len(self.used_textslots)))
+        
+        nose.tools.assert_true(len(self.used_textslots) == 0)
+     
      
     def test_empty_textureslots(self):
-        self.used_textslots = TextureHelper.get_used_textslots(self, self.b_mat)
+        self.used_textslots = TextureHelper.get_used_textslots(self.b_mat)
         print("Used Slots: %s, items = %s" % (self.used_textslots, len(self.used_textslots)))
         
         nose.tools.assert_true(len(self.used_textslots) == 0)
@@ -28,7 +33,7 @@ class Test_TextureHelper_Used_Slots:
         print("Adding textureslot")
         self.texture_slot0 = self.b_mat.texture_slots.add()
                    
-        self.used_textslots = TextureHelper.get_used_textslots(self, self.b_mat)
+        self.used_textslots = TextureHelper.get_used_textslots(self.b_mat)
         print("Used Slots: %s, items = %s" % (self.used_textslots, len(self.used_textslots)))
         
         nose.tools.assert_true(len(self.used_textslots) == 1)
@@ -42,7 +47,8 @@ class Test_TextureHelper_Used_Slots:
         print("Not using second: ")
         self.texture_slot1.use = False
         
-        self.used_textslots = TextureHelper.get_used_textslots(self, self.b_mat)
+        self.used_textslots = TextureHelper.get_used_textslots(self.b_mat)
         
         print("Used Slots: %s, items = %s" % (self.used_textslots, len(self.used_textslots)))
         nose.tools.assert_true(len(self.used_textslots) == 1)
+        
