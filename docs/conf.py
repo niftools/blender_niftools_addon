c49c81c87760ad6e5cdfb0a6c1da455ea1eda246
neomonkeus <neomonkeus@gmail.com>
2013-11-04 18:24:26 +0000

    Reorganised document structures.

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Blender NIF Plugin'
-copyright = '2012, Niftools'
+copyright = '2014, Niftools'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
