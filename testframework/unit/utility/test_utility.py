3dd4456275c54ceec1c3ea10133b7521793121ab
neomonkeus <neomonkeus@gmail.com>
2013-12-19 00:40:58 +0000

    Tidied up the matrix test, still needs to have correct matrix building but should make it easier now.

--- testframework/unit/utility/test_utility.py
+++ testframework/unit/utility/test_utility.py
@@ -12,18 +12,30 @@ from io_scene_nif.utility import nif_utils
 import pyffi
 from pyffi.formats.nif import NifFormat
  
-
-
-
-@nottest
+@nottest 
 class Test_Matrix_Operations:
 #Read docs/development/issues.rst for more info on matrix conversions.
  
     @classmethod
+    def setup_class(cls):
+        print("Class setup: " + str(cls))
+         
+        cls.reinitialise()
+        
+    @classmethod
+    def teardown_class(cls):
+        print("Class Teardown: " + str(cls))
+         
+        del cls.niBlock
+        del cls.nif_matrix
+        del cls.blender_matrix
+        
+    @classmethod
     def setup(cls):
-        print("Setup" + str(cls))
+        print("Method setup")
          
         cls.nif_matrix = cls.build_nif_matrix()
+        
         cls.niBlock = NifFormat.NiNode()
         cls.niBlock.set_transform(cls.nif_matrix)
          
@@ -31,11 +43,14 @@ class Test_Matrix_Operations:
          
     @classmethod
     def teardown(cls):
-        print("Teardown" + str(cls))
-         
+        cls.reinitialise()
+    
+    @classmethod
+    def reinitialise(cls):
+        cls.nif_matrix = None
         cls.niBlock = None
-        cls.vec = None
-         
+        cls.blender_matrix = None
+    
     def test_import_matrix(self):
         converted_mat = nif_utils.import_matrix(self.niBlock)
          
@@ -47,7 +62,9 @@ class Test_Matrix_Operations:
                 nose.tools.assert_true(converted_mat[row][col] - self.blender_matrix[row][col] 
                                          < NifFormat.EPSILON)
                  
-                 
+    def test_matrix_decompose_srt(self):
+        pass
+    
     @classmethod
     def build_blender_matrix(cls):
         translation = (2.0, 3.0, 4.0)
@@ -64,9 +81,8 @@ class Test_Matrix_Operations:
          
         b_scale_mat = mathutils.Matrix.Scale(scale, 4)
         b_matrix = b_scale_mat * b_rot_mat * b_loc_vec 
-         
-        print("Blender - RHS")
-        print(b_matrix)
+
+#         print(b_matrix)
          
 #         (-0.0000, -0.2812,  0.4871, 20.0000)
 #         ( 0.4871, -0.2436, -0.1406, 20.0000)
@@ -82,48 +98,50 @@ class Test_Matrix_Operations:
          
         n_mat = NifFormat.Matrix44()
         translation = (2.0, 3.0, 4.0)
-        scale = 2
+        scale = 2.0
+        
         rhsrotx = (1.0, 0.0, 0.0,
                    0.0, 0.866, 0.5,
                    0.0, -0.5, 0.866)
-         
+        
         rhsroty = (0.5, 0.0, -0.866,
                    0.0, 1.0, 0.0,
                    0.866, 0.0, 0.5)
-         
+       
         rhsrotz = (0, 1, 0,
                    -1, 0, 0,
                    0, 0, 1)
+        
+        rhsrotx = cls.create_matrix(rhsrotx)
+        rhsroty = cls.create_matrix(rhsroty)
+        rhsrotz = cls.create_matrix(rhsrotz)
          
-        rhsrot = (0, -0.8660, 0.5,
-                  0.5,  0.4330, 0.75,
-                  -0.8660, 0.2550, 0.4330)
+        n_mat33 = rhsrotz * rhsroty * rhsrotx
          
         n_vec3 = NifFormat.Vector3()
         n_vec3.x = translation[0]
         n_vec3.y = translation[1]
         n_vec3.z = translation[2]
  
-        n_mat33 = NifFormat.Matrix33()
-        n_mat33.m_11 = rhsrotx[0]
-        n_mat33.m_12 = rhsrotx[3]
-        n_mat33.m_13 = rhsrotx[6]
-        n_mat33.m_21 = rhsrotx[1]
-        n_mat33.m_22 = rhsrotx[4]
-        n_mat33.m_23 = rhsrotx[7]
-        n_mat33.m_31 = rhsrotx[2]
-        n_mat33.m_32 = rhsrotx[5]
-        n_mat33.m_33 = rhsrotx[8]
-         
- 
         n_mat.set_scale_rotation_translation(scale, n_mat33, n_vec3)
-         
-        print("Building Matrices")
-        print("Nif - LHS")
-        print(n_mat)
-         
+        
         return n_mat
     
+    @classmethod
+    def create_matrix(cls, tuple):
+        n_mat33 = NifFormat.Matrix33()
+        n_mat33.m_11 = tuple[0]
+        n_mat33.m_12 = tuple[1]
+        n_mat33.m_13 = tuple[2]
+        n_mat33.m_21 = tuple[3]
+        n_mat33.m_22 = tuple[4]
+        n_mat33.m_23 = tuple[5]
+        n_mat33.m_31 = tuple[6]
+        n_mat33.m_32 = tuple[7]
+        n_mat33.m_33 = tuple[8]
+        
+        return n_mat33
+
 class Test_Find_Block_Properties:
     """Tests find_property method"""
     
