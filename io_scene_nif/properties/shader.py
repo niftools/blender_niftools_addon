c8e5aa9da5697f7d81483357a2060c2a4cfe5cd1
neomonkeus <neomonkeus@gmail.com>
2014-09-21 17:44:03 +0100

    Updated file headers

--- io_scene_nif/properties/shader.py
+++ io_scene_nif/properties/shader.py
@@ -1,8 +1,8 @@
-''' Nif User Interface, connect custom properties from properties.py into Blenders UI'''
+''' Nif User Interface, custom nif properties for shaders'''
 
 # ***** BEGIN LICENSE BLOCK *****
 # 
-# Copyright © 2005-2013, NIF File Format Library and Tools contributors.
+# Copyright © 2005-2014, NIF File Format Library and Tools contributors.
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
