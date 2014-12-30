741c3711b47d2f6d6761eb8d2126b5e95c08f385
neomonkeus <neomonkeus@gmail.com>
2014-12-30 01:49:40 +0000

    Updated the blender api inter-link

--- docs/conf.py
+++ docs/conf.py
@@ -247,7 +247,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
     'http://docs.python.org/': None,
-    'http://www.blender.org/documentation/blender_python_api_2_63_release/': None,
+    'http://www.blender.org/api/blender_python_api_2_72_release/': None,
     'http://pyffi.sourceforge.net/': None,
     }
 
