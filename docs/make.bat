fb1e0e74b586ff98beb06ec1d43cf19126563666
neomonkeus <neomonkeus@gmail.com>
2014-12-30 02:27:38 +0000

    Removed test call to sphinx-api-docs and updated output.

--- docs/make.bat
+++ docs/make.bat
@@ -51,9 +51,11 @@ if "%1" == "clean" (
 
 if "%1" == "html" (
 	%SPHINXAPIBUILD% 
+	if errorlevel 1 exit /b 1
+	echo.
+	echo.Generated auto-docs for api
 	%SPHINXBUILD% -b html %ALLSPHINXOPTS% %BUILDDIR%/html
 	if errorlevel 1 exit /b 1
-	sphinx-apidoc -
 	echo.
 	echo.Build finished. The HTML pages are in %BUILDDIR%/html.
 	goto end
