8b725ca3422aa3c50d3d8451ec0cedbb4166441a
neomonkeus <neomonkeus@gmail.com>
2015-04-06 17:53:11 +0100

    Added nix shell script for deps and updated docs.

---
+++ install/install_deps.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#Script to install developer dependancies for the Blender Nif Plugin
+
+python3 -m pip install Sphinx --target="%APPDATABLENDERADDONS%modules"
+python3 -m pip install nose --target="%APPDATABLENDERADDONS%\modules"
