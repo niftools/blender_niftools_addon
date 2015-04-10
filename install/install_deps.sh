46e529f8060fa98fd43b94a4fc639be0e592ef68
neomonkeus <neomonkeus@gmail.com>
2015-04-10 12:40:12 +0100

    Fix path bug in scripts

--- install/install_deps.sh
+++ install/install_deps.sh
@@ -2,5 +2,5 @@
 
 #Script to install developer dependancies for the Blender Nif Plugin
 
-python3 -m pip install Sphinx --target="%APPDATABLENDERADDONS%modules"
-python3 -m pip install nose --target="%APPDATABLENDERADDONS%\modules"
+python3 -m pip install Sphinx --target="%APPDATABLENDERADDONS%/modules"
+python3 -m pip install nose --target="%APPDATABLENDERADDONS%/modules"
