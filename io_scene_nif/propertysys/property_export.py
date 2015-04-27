8d49efa63563bb2770db48981bfc460ab977aebc
Ghost <Ghostwalker71@users.noreply.github.com>
2015-04-27 01:15:03 -0500

    propertysys module edits to comply with pep8 conventions

--- io_scene_nif/propertysys/property_export.py
+++ io_scene_nif/propertysys/property_export.py
@@ -40,24 +40,30 @@
 from pyffi.formats.nif import NifFormat
 from io_scene_nif.materialsys.material_export import Material
 
+
 class PropertyHelper():
-    
+
     def __init__(self, parent):
         self.object_property = ObjectProperty(parent)
         self.material_property = Material(parent)
-        
-        
+
+
 class ObjectProperty():
-    
+
     def __init__(self, parent):
         self.nif_export = parent
         self.properties = parent.properties
-        
-    def export_vertex_color_property(self, block_parent, flags=1,
-                                     vertex_mode=0, lighting_mode=1):
+
+    def export_vertex_color_property(
+                                     self,
+                                     block_parent,
+                                     flags=1,
+                                     vertex_mode=0,
+                                     lighting_mode=1
+                                     ):
         """Create a vertex color property, and attach it to an existing block
         (typically, the root of the nif tree).
-    
+
         @param block_parent: The block to which to attach the new property.
         @param flags: The C{flags} of the new property.
         @param vertex_mode: The C{vertex_mode} of the new property.
@@ -65,20 +71,25 @@ class ObjectProperty():
         @return: The new property block.
         """
         # create new vertex color property block
-        vcolprop = self.nif_export.objecthelper.create_block("NiVertexColorProperty")
-    
+        vcolprop = self.nif_export.objecthelper.create_block(
+                        "NiVertexColorProperty")
+
         # make it a property of the parent
         block_parent.add_property(vcolprop)
-    
+
         # and now export the parameters
         vcolprop.flags = flags
         vcolprop.vertex_mode = vertex_mode
         vcolprop.lighting_mode = lighting_mode
-    
+
         return vcolprop
-    
-    def export_z_buffer_property(self, block_parent,
-                                 flags=15, function=3):
+
+    def export_z_buffer_property(
+                                 self,
+                                 block_parent,
+                                 flags=15,
+                                 function=3
+                                 ):
         """Create a z-buffer property, and attach it to an existing block
         (typically, the root of the nif tree).
 
@@ -98,7 +109,7 @@ class ObjectProperty():
         zbuf.function = function
 
         return zbuf
-    
+
     def export_alpha_property(self, flags=0x00ED, threshold=0):
         """Return existing alpha property with given flags, or create new one
         if an alpha property with required flags is not found."""
@@ -108,12 +119,13 @@ class ObjectProperty():
                and block.flags == flags and block.threshold == threshold:
                 return block
         # no alpha property with given flag found, so create new one
-        alphaprop = self.nif_export.objecthelper.create_block("NiAlphaProperty")
+        alphaprop = self.nif_export.objecthelper.create_block(
+                        "NiAlphaProperty")
         alphaprop.flags = flags
         alphaprop.threshold = threshold
         return alphaprop
 
-    def export_specular_property(self, flags = 0x0001):
+    def export_specular_property(self, flags=0x0001):
         """Return existing specular property with given flags, or create new one
         if a specular property with required flags is not found."""
         # search for duplicate
@@ -122,11 +134,12 @@ class ObjectProperty():
                and block.flags == flags:
                 return block
         # no specular property with given flag found, so create new one
-        specprop = self.nif_export.objecthelper.create_block("NiSpecularProperty")
+        specprop = self.nif_export.objecthelper.create_block(
+                        "NiSpecularProperty")
         specprop.flags = flags
         return specprop
 
-    def export_wireframe_property(self, flags = 0x0001):
+    def export_wireframe_property(self, flags=0x0001):
         """Return existing wire property with given flags, or create new one
         if an wire property with required flags is not found."""
         # search for duplicate
@@ -136,7 +149,8 @@ class ObjectProperty():
                 return block
 
         # no wire property with given flag found, so create new one
-        wireprop = self.nif_export.objecthelper.create_block("NiWireframeProperty")
+        wireprop = self.nif_export.objecthelper.create_block(
+                        "NiWireframeProperty")
         wireprop.flags = flags
         return wireprop
 
@@ -150,9 +164,8 @@ class ObjectProperty():
                 # is needed
                 return block
         # no stencil property found, so create new one
-        stencilprop = self.nif_export.objecthelper.create_block("NiStencilProperty")
+        stencilprop = self.nif_export.objecthelper.create_block(
+                            "NiStencilProperty")
         if self.properties.game == 'FALLOUT_3':
             stencilprop.flags = 19840
         return stencilprop
-    
-    
