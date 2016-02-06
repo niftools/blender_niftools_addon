ac324e92e7587b23e550f96f0dbc7b8d5e1be0ab
Eli2 <FabianRoth64@googlemail.com>
2016-02-06 15:48:26 +0100

    Remove access to invalid EditBone in b_child_bone after switching to OBJECT mode

--- io_scene_nif/armaturesys/armature_import.py
+++ io_scene_nif/armaturesys/armature_import.py
@@ -257,7 +257,6 @@ class Armature():
 			b_child_bone.parent = b_bone
 			bpy.ops.object.mode_set(mode='OBJECT',toggle=False)
 			b_bone = b_armatureData.bones[bone_name]
-			b_child_bone = b_armatureData.bones[b_child_bone.name]
 
 
 		
