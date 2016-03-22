2534cff7240942b1f4f70feb12880e477bb22635
neomonkeus <neomonkeus@gmail.com>
2016-03-22 17:55:50 +0000

    Added Scene UI for version properties
    Removed unnecessary register functions.

--- io_scene_nif/ui/__init__.py
+++ io_scene_nif/ui/__init__.py
@@ -35,20 +35,4 @@
 #
 # ***** END LICENSE BLOCK *****
 
-from . import armature, collision, geometry, material, object, shader
-
-def register():
-    armature.register()
-    collision.register()
-    geometry.register()
-    material.register()
-    object.register()
-    shader.register()
-    
-def unregister():
-    armature.unregister()
-    collision.unregister()
-    geometry.unregister()
-    material.unregister()
-    object.unregister()
-    shader.unregister()
+from . import armature, collision, geometry, material, object, shader, scene
