f904d0a9b1c8f350e59203ceb4657c989e060561
neomonkeus <neomonkeus@gmail.com>
2016-03-23 01:12:12 +0000

    Updated testframework data generation for version information

--- testframework/integration/data/b_gen_header.py
+++ testframework/integration/data/b_gen_header.py
@@ -1,27 +1,27 @@
-"""Exports and imports mesh data"""
+"""Header Helper functions"""
 
 # ***** BEGIN LICENSE BLOCK *****
-# 
+#
 # Copyright © 2005-2015, NIF File Format Library and Tools contributors.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
-# 
+#
 #    * Redistributions of source code must retain the above copyright
 #      notice, this list of conditions and the following disclaimer.
-# 
+#
 #    * Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials provided
 #      with the distribution.
-# 
+#
 #    * Neither the name of the NIF File Format Library and Tools
 #      project nor the names of its contributors may be used to endorse
 #      or promote products derived from this software without specific
 #      prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
@@ -38,37 +38,21 @@
 # ***** END LICENSE BLOCK *****
 
 import bpy
-import nose.tools
+   
+def set_version_info(nif_ver=0, user_ver=0, user_ver_2=0):
+    scene = bpy.context.scene.niftools_scene
+    scene.nif_version = nif_ver
+    scene.user_version = user_ver
+    scene.user_version_2 = user_ver_2
 
-from integration import SingleNif
-from integration.data import n_gen_header 
-from integration.object import b_gen_object, n_gen_object
-
-class TestNiNode(SingleNif):
-    """Test base geometry, single blender object."""
-
-    n_name = 'object/test_object' # (documented in base class)
-    b_name = 'NifObject'
-
-    def b_create_header(self):
-        self.n_game = 'OBLIVION'
-
-    def b_create_data(self):
-        b_gen_object.b_create_transformed_object(self.b_name)
-        
-    def b_check_data(self):
-        b_obj = bpy.data.objects[self.b_name]
-        b_gen_object.b_check_transform(b_obj)
-
-    def n_create_header(self):
-        n_gen_header.n_create_header_oblivion(self.n_data)
-
-    def n_create_data(self):
-        n_gen_object.n_create_blocks(self.n_data)
-        return self.n_data
-
-    def n_check_data(self):
-        n_ninode = self.n_data.roots[0].children[0]
-        n_gen_object.n_check_ninode(n_ninode)
-        n_gen_object.n_check_transform(n_ninode)
-
+def b_set_morrowind_info():
+    set_version_info(nif_ver=0x14000005)
+    
+def b_set_oblivion_info():
+    set_version_info(nif_ver=0x14000005, user_ver=11, user_ver_2=11)
+    
+def b_set_fo3_info():
+    set_version_info(nif_ver=0x14000005, user_ver=11, user_ver_2=34)
+    
+def b_set_skyrim_info():
+    raise NotImplementedError
