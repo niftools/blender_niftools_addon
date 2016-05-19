74fa31772d32713c20416f118466885d25e5d45f
neomonkeus <neomonkeus@gmail.com>
2016-05-20 00:50:49 +0100

    Compiling egm test. EGM isn't supported atm so main code fails.

--- testframework/unit/io/egm/test_egm.py
+++ testframework/unit/io/egm/test_egm.py
@@ -3,7 +3,7 @@ import nose
 import bpy
 import os
 
-from io_scene_nif.io.nif import EGMFile
+from io_scene_nif.io.egm import EGMFile
 from io_scene_nif.utility.nif_logging import NifLog
 
 class Test_EGM_IO:
@@ -13,13 +13,13 @@ class Test_EGM_IO:
         cls.working_dir = os.path.dirname(__file__)
         
     def test_load_supported_version(self):
-        data = EGMFile.load_nif(self.working_dir + os.sep + "readable.nif")
+        data = EGMFile.load_egm(self.working_dir + os.sep + "readable.egm")
         nose.tools.assert_equal(data.version, 335544325)
 
     @nose.tools.raises(Exception)
     def test_load_unsupported_version(self):
-        EGMFile.load_egm(self.working_dir + os.sep + "unreadable.nif")
+        EGMFile.load_egm(self.working_dir + os.sep + "unreadable.egm")
         
     @nose.tools.raises(Exception)
     def test_load_unsupported_file(self):
-        EGMFile.load_egm(self.working_dir + os.sep + "notnif.txt")
+        EGMFile.load_egm(self.working_dir + os.sep + "notegm.txt")
