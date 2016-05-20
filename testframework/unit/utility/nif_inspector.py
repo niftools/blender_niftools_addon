9a6dff12ebcea412b1b33a83dbaf61c9ef2fbce5
neomonkeus <neomonkeus@gmail.com>
2016-05-20 22:49:46 +0100

    Updated nif_inspector with descriptions about usage.

--- testframework/unit/utility/nif_inspector.py
+++ testframework/unit/utility/nif_inspector.py
@@ -24,20 +24,24 @@ class NifInspector:
         
     @istest
     def Runner(self):
+        'Runner test to allow developer to quickly test in-memory, reading and writing of files'
         print("Test running")
-        self.read_file()
+        # self.read_file()
+        # self.check_data()
+        # self.create_data()
         # self.write_file()
         pass
      
      
-    def write_file(self):       
-        self.create_data()
+    def write_file(self):    
+        'Helper method to write a nif file'   
         path = self.output_dir + os.sep + self.output_filename + self.ext
         print("Writing to: " + path)
         with open(path, 'wb') as stream:
             self.nif_file.write(stream)
          
     def read_file(self):
+        'Helper method to write a nif file'
         path = self.input_dir + os.sep + self.input_filename + self.ext
         print("Reading : " + path)
         stream = open(path, 'rb')
@@ -45,10 +49,13 @@ class NifInspector:
         self.check_data()
          
     def create_data(self):
+        'Stub method to populate self.nif_file'
         print("creating nif_file")
+        
 
             
     def check_data(self):
+        'Stub method to testing self.nif_file'
         print("Checking nif_file")
         
     
