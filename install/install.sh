a0221c8177cfc94f9b4d3c06e4bb7eba656bf4a3
neomonkeus <neomonkeus@gmail.com>
2018-07-12 00:11:25 +0100

    Update build scripts tp remove dependencies

--- install/install.sh
+++ install/install.sh
@@ -4,15 +4,16 @@ DIR=`pwd`/`dirname $0`
 VERSION=`cat ${DIR}/../io_scene_nif/VERSION`
 NAME="blender_nif_plugin"
 
-for BLENDERVERSION in 2.66 2.65 2.64 2.63 2.62
+for BLENDER_VERSION in 2.79 2.78 2.77 2.76 2.75
 do
-  BLENDERADDONS=~/.blender/$BLENDERVERSION/scripts/addons
-  if [ -e ~/.blender/$BLENDERVERSION/ ]
+  BLENDER_ADDONS=~/.blender/${BLENDER_VERSION}/scripts/addons
+  if [ -e ~/.blender/${BLENDER_VERSION}/ ]
   then
+    echo "Blender addons directory : ${BLENDER_ADDONS}"
     break
   fi
 done
-if [ ! -e ~/.blender/$BLENDERVERSION/ ]
+if [ ! -e ~/.blender/${BLENDER_VERSION}/ ]
 then
   echo Blender addons folder not found.
   echo Start blender at least once, save user preferences, and try again.
@@ -20,14 +21,14 @@ then
 fi
 
 echo Installing to:
-echo $BLENDERADDONS/io_scene_nif
-mkdir -p $BLENDERADDONS
+echo ${BLENDER_ADDONS}/io_scene_nif
+mkdir -p ${BLENDER_ADDONS}
 
 # remove old files
-rm -rf $BLENDERADDONS/io_scene_nif/
+rm -rf ${BLENDER_ADDONS}/io_scene_nif/
 
 # create zip
 sh ${DIR}/makezip.sh
 
 # copy files from repository to blender addons folder
-unzip -q "$DIR/$NAME-$VERSION.zip" -d $BLENDERADDONS
+unzip -q "${DIR}/${NAME}-${VERSION}.zip" -d ${BLENDER_ADDONS}
