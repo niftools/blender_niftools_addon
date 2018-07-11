66c29dd6f5b99056e74545ae53356edea70e8fd7
neomonkeus <neomonkeus@gmail.com>
2018-07-12 00:11:25 +0100

    Update build scripts tp remove dependencies

---
+++ install/zip.ps1
@@ -0,0 +1,19 @@
+param (
+    [string]$source,
+    [string]$destination
+)
+
+write ("Source : {0}" -f $source)
+write ("Destination : {0}" -f $destination)
+
+If(Test-path $destination) {
+write ("Ensuring existing archive is removed : {0}" -f $destination)
+Remove-item $destination
+write ("Existing archive removed successfully")
+}
+
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::CreateFromDirectory($source, $destination)
+If(Test-path $destination) {
+    write("File successully written")
+}
