f177ca44f0c759e52ecfb6e572814de3984a102c
neomonkeus <neomonkeus@gmail.com>
2018-09-25 22:52:25 +0100

    fix issue with uv unwrap operator

--- testframework/integration/modules/geometry/vertex/uv/b_gen_uv.py
+++ testframework/integration/modules/geometry/vertex/uv/b_gen_uv.py
@@ -42,5 +42,5 @@ import bpy
 
 def b_uv_object():
     bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.uv.unwrap(method='ANGLE_BASED', fill_holes=True, correct_aspect=True, use_subsurf_data=False, uv_subsurf_level=1)
+    bpy.ops.uv.unwrap(method='ANGLE_BASED', fill_holes=True, correct_aspect=True, use_subsurf_data=False)
     bpy.ops.object.mode_set(mode='OBJECT')
