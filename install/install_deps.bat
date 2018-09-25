3038f35ff37f1eff2f61fa70ba21bc54a5c5e73c
neomonkeus <neomonkeus@gmail.com>
2018-09-25 12:29:14 +0100

    Updated dependencies required for integration tests

--- install/install_deps.bat
+++ install/install_deps.bat
@@ -1,3 +1,21 @@
-:: Script to install developer dependancies for the Blender Nif plugin
-python -m pip install Sphinx --target="%BLENDER_ADDONS_DIR"%\dependencies
-python -m pip install nose --target="%BLENDER_ADDONS_DIR"%\dependencies
+:: Install developer dependencies for integration tests
+@echo off
+
+if "%BLENDER_ADDONS_DIR%" == "" if not exist "%BLENDER_ADDONS_DIR%" (
+echo."Update BLENDER_ADDONS_DIR to the folder where the blender addons reside, such as:"
+echo."set BLENDER_ADDONS_DIR=%APPDATA%\Blender Foundation\Blender\2.79\scripts\addons"
+echo.
+pause
+goto end
+)
+
+::TODO replace with quivilant setup.py call via pip to remove
+
+echo."Installing Sphinx to %BLENDER_ADDONS_DIR%"
+python -m pip install Sphinx --target="%BLENDER_ADDONS_DIR%"
+
+echo."Installing nose to %BLENDER_ADDONS_DIR%\dependencies"
+python -m pip install nose --target="%BLENDER_ADDONS_DIR%"
+
+echo."Installing nose to %BLENDER_ADDONS_DIR%\dependencies"
+python -m pip install pyffi --target="%BLENDER_ADDONS_DIR%"
--- install/install_deps.sh
+++ install/install_deps.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+# Install developer dependencies for integration tests
 
-#Script to install developer dependancies for the Blender Nif Plugin
-python -m pip install Sphinx --target="${BLENDER_ADDONS_DIR}\dependencies"
-python -m pip install nose --target="${BLENDER_ADDONS_DIR}\dependencies"
+if [[ "${BLENDER_ADDONS_DIR}" == "" || ! -e "${BLENDER_ADDONS_DIR}"  ]]; then
+    echo."Update BLENDER_ADDONS_DIR to the folder where the blender addons reside, such as:"
+    echo."set BLENDER_ADDONS_DIR=~/Blender Foundation/Blender/2.79/scripts/addons"
+    echo.
+    exit 1
+fi
+
+echo "Installing Sphinx to ${BLENDER_ADDONS_DIR}"
+python -m pip install Sphinx --target="${BLENDER_ADDONS_DIR}"
+
+echo "Installing nose to ${BLENDER_ADDONS_DIR}"
+python -m pip install nose --target="${BLENDER_ADDONS_DIR}"
+
+echo "Installing nose to ${BLENDER_ADDONS_DIR}"
+python -m pip install pyffi --target="${BLENDER_ADDONS_DIR}"
