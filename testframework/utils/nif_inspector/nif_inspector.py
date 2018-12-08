95590c2784748a37d77681c3ddb2ec765a087dbe
neomonkeus <neomonkeus@gmail.com>
2018-12-08 13:59:46 +0000

    Moved out the nif inspector utility to its own module
    It wasn't going to be run as part of the test framework and wasn't been picked by the nose framework runner anyways.

--- testframework/utils/nif_inspector/nif_inspector.py
+++ testframework/utils/nif_inspector/nif_inspector.py
@@ -2,53 +2,53 @@ from pyffi.formats.nif import NifFormat
 
 import os.path
 
-from nose.tools import istest
+import unittest
 
 
-@istest
-class NifInspector:
+class TestNifInspector(unittest.TestCase):
     """Handy utility class for debugging and testing reading / writing nifs"""
-    
-    @classmethod
-    def setupclass(cls):
-        # Uses script directory to read from/write from, override for different location
-        cls.input_dir = os.path.dirname(__file__)
-        cls.input_filename = 'test'
-        cls.output_dir = cls.input_dir
-        cls.output_filename = cls.input_filename + "_out"
-        cls.ext = ".nif"
-        cls.nif_file = NifFormat.Data()
-        print("setup class")
-        
-    @istest
-    def runner(self):
+
+    def test_runner(self):
         """Runner test to allow developer to quickly test in-memory, reading and writing of files"""
         print("Test running")
-        # self.read_file()
+        self._read_file()
         # self.check_data()
         # self.create_data()
         # self.write_file()
         pass
 
-    def write_file(self):
+    @classmethod
+    def setUpClass(cls):
+        # Uses script directory to read from/write from, override for different location
+        cls.root = os.path.dirname(__file__)
+        cls.input_dir = os.path.join(cls.root, "input")
+        cls.output_dir = os.path.join(cls.root, "output")
+        cls.input_filename = ''  # Replace with name of file to be loaded
+        cls.ext = ".nif"
+        cls.output_filename = cls.input_filename + "_out"
+        cls.nif_file = NifFormat.Data()
+        print("setup class")
+
+    def _write_file(self):
         """Helper method to write a nif file"""
         path = self.output_dir + os.sep + self.output_filename + self.ext
         print("Writing to: " + path)
         with open(path, 'wb') as stream:
             self.nif_file.write(stream)
          
-    def read_file(self):
+    def _read_file(self):
         """Helper method to write a nif file"""
         path = self.input_dir + os.sep + self.input_filename + self.ext
         print("Reading : " + path)
-        stream = open(path, 'rb')
-        self.nif_file.read(stream)
-        self.check_data()
-         
-    def create_data(self):
+        with open(path, 'rb') as stream:
+            self.nif_file.read(stream)
+        print(self.nif_file)
+
+    def _create_data(self):
         """Stub method to populate self.nif_file"""
         print("creating nif_file")
 
-    def check_data(self):
+    def _check_data(self):
         """Stub method to testing self.nif_file"""
         print("Checking nif_file")
+
