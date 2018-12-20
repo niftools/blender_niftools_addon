5e469a9a99271d693de3a40deb5903863f550a5b
neomonkeus <neomonkeus@gmail.com>
2018-12-20 17:44:54 +0000

    Move out dead type checking for slot for now code to here for now.
    - Probably won't use these, as the b_mat check will fail to process so would need to be replaced with an actual material cache and ensure
    self.b_mat is not set until all types checks have been performed.
    - The duplicate code checks would need to be removed for the function above it.

--- io_scene_nif/modules/property/texture/texture_export.py
+++ io_scene_nif/modules/property/texture/texture_export.py
@@ -40,6 +40,8 @@
 from pyffi.formats.nif import NifFormat
 
 from io_scene_nif.modules import armature
+from io_scene_nif.modules.animation.animation_export import AnimationHelper
+from io_scene_nif.modules.obj.object_export import ObjectHelper
 from io_scene_nif.modules.property import texture
 from io_scene_nif.utility import nif_utils
 from io_scene_nif.utility.nif_global import NifOp
@@ -49,13 +51,13 @@ from io_scene_nif.modules.property.texture.texture_writer import TextureWriter
 
 
 class TextureHelper:
+
     # Default ordering of Extra data blocks for different games
     USED_EXTRA_SHADER_TEXTURES = {'SID_MEIER_S_RAILROADS': (3, 0, 4, 1, 5, 2),
                                   'CIVILIZATION_IV': (3, 0, 1, 2)}
 
-    def __init__(self, parent):
-        self.nif_export = parent
-        self.texture_writer = TextureWriter(parent=self)
+    def __init__(self):
+        self.texture_writer = TextureWriter()
         # TODO [texture] Not a dictionary lookup
         self.MESH_UVLAYERS_LIST = list()
 
@@ -77,131 +79,6 @@ class TextureHelper:
         for slot in texture_slots:
             used_uvlayers.add(slot.uv_layer)
         return used_uvlayers
-
-    # TODO [property][shader]
-    def export_bs_shader_property(self, b_obj=None, b_mat=None):
-        """Export a Bethesda shader property block."""
-        self.determine_texture_types(b_obj, b_mat)
-
-        # create new block
-        if b_obj.niftools_shader.bs_shadertype == 'BSShaderPPLightingProperty':
-            bsshader = NifFormat.BSShaderPPLightingProperty()
-            # set shader options
-            # TODO [property][shader]
-            b_s_type = NifFormat.BSShaderType._enumkeys.index(b_obj.niftools_shader.bsspplp_shaderobjtype)
-            bsshader.shader_type = NifFormat.BSShaderType._enumvalues[b_s_type]
-
-            # Shader Flags
-            if hasattr(bsshader, 'shader_flags'):
-                self.export_shader_flags(b_obj, bsshader)
-
-        if b_obj.niftools_shader.bs_shadertype == 'BSLightingShaderProperty':
-            bsshader = NifFormat.BSLightingShaderProperty()
-            b_s_type = NifFormat.BSLightingShaderPropertyShaderType._enumkeys.index(b_obj.niftools_shader.bslsp_shaderobjtype)
-            bsshader.shader_type = NifFormat.BSLightingShaderPropertyShaderType._enumvalues[b_s_type]
-
-            # TODO [property][material]
-            # UV Offset
-            if hasattr(bsshader, 'uv_offset'):
-                self.export_uv_offset(bsshader)
-
-            # UV Scale
-            if hasattr(bsshader, 'uv_scale'):
-                self.export_uv_scale(bsshader)
-
-            # Texture Clamping mode
-            if not self.base_mtex.texture.image.use_clamp_x:
-                wrap_s = 2
-            else:
-                wrap_s = 0
-            if not self.base_mtex.texture.image.use_clamp_y:
-                wrap_t = 1
-            else:
-                wrap_t = 0
-            bsshader.texture_clamp_mode = (wrap_s + wrap_t)
-
-            # Diffuse color
-            bsshader.skin_tint_color.r = b_mat.diffuse_color.r
-            bsshader.skin_tint_color.g = b_mat.diffuse_color.g
-            bsshader.skin_tint_color.b = b_mat.diffuse_color.b
-            # b_mat.diffuse_intensity = 1.0
-
-            bsshader.lighting_effect_1 = b_mat.niftools.lightingeffect1
-            bsshader.lighting_effect_2 = b_mat.niftools.lightingeffect2
-
-            # Emissive
-            bsshader.emissive_color.r = b_mat.niftools.emissive_color.r
-            bsshader.emissive_color.g = b_mat.niftools.emissive_color.g
-            bsshader.emissive_color.b = b_mat.niftools.emissive_color.b
-            bsshader.emissive_multiple = b_mat.emit
-
-            # gloss
-            bsshader.glossiness = b_mat.specular_hardness
-
-            # Specular color
-            bsshader.specular_color.r = b_mat.specular_color.r
-            bsshader.specular_color.g = b_mat.specular_color.g
-            bsshader.specular_color.b = b_mat.specular_color.b
-            bsshader.specular_strength = b_mat.specular_intensity
-
-            # Alpha
-            if b_mat.use_transparency:
-                bsshader.alpha = (1 - b_mat.alpha)
-
-            # Shader Flags
-            if hasattr(bsshader, 'shader_flags_1'):
-                self.export_shader_flags(b_obj, bsshader)
-
-        if b_obj.niftools_shader.bs_shadertype == 'BSEffectShaderProperty':
-            bsshader = NifFormat.BSEffectShaderProperty()
-
-            # Alpha
-            if b_mat.use_transparency:
-                bsshader.alpha = (1 - b_mat.alpha)
-
-            # clamp Mode
-            bsshader.texture_clamp_mode = 65283
-
-            # Emissive
-            bsshader.emissive_color.r = b_mat.niftools.emissive_color.r
-            bsshader.emissive_color.g = b_mat.niftools.emissive_color.g
-            bsshader.emissive_color.b = b_mat.niftools.emissive_color.b
-            bsshader.emissive_color.a = b_mat.niftools.emissive_alpha
-            bsshader.emissive_multiple = b_mat.emit
-
-            # Shader Flags
-            if hasattr(bsshader, 'shader_flags_1'):
-                self.export_shader_flags(b_obj, bsshader)
-
-        if b_obj.niftools_shader.bs_shadertype == 'None':
-            raise nif_utils.NifError("Export version expected shader. "
-                                     "No shader applied to mesh '%s', these cannot be exported to NIF."
-                                     " Set shader before exporting." % b_obj)
-        # set textures
-        texset = NifFormat.BSShaderTextureSet()
-        bsshader.texture_set = texset
-        if self.base_mtex:
-            texset.textures[0] = self.texture_writer.export_texture_filename(self.base_mtex.texture)
-        if self.normal_mtex:
-            texset.textures[1] = self.texture_writer.export_texture_filename(self.normal_mtex.texture)
-        if self.glow_mtex:
-            texset.textures[2] = self.texture_writer.export_texture_filename(self.glow_mtex.texture)
-        if self.detail_mtex:
-            texset.textures[3] = self.texture_writer.export_texture_filename(self.detail_mtex.texture)
-
-        if b_obj.niftools_shader.bs_shadertype == 'BSLightingShaderProperty':
-            texset.num_textures = 9
-            texset.textures.update_size()
-            if self.detail_mtex:
-                texset.textures[6] = self.texture_writer.export_texture_filename(self.detail_mtex.texture)
-            if self.gloss_mtex:
-                texset.textures[7] = self.texture_writer.export_texture_filename(self.gloss_mtex.texture)
-
-        if b_obj.niftools_shader.bs_shadertype == 'BSEffectShaderProperty':
-            bsshader.source_texture = self.texture_writer.export_texture_filename(self.base_mtex.texture)
-            bsshader.greyscale_texture = self.texture_writer.export_texture_filename(self.glow_mtex.texture)
-
-        return bsshader
 
     def export_texturing_property(self, flags=0x0001, applymode=None, b_mat=None, b_obj=None):
         """Export texturing property."""
@@ -240,7 +117,7 @@ class TextureHelper:
                 pass
             else:
                 # texture slot 0 = base
-                self.animationhelper.texture_animation.export_flip_controller(fliptxt, self.base_mtex.texture, texprop, 0)
+                AnimationHelper(parent=self).texture_animation.export_flip_controller(fliptxt, self.base_mtex.texture, texprop, 0)
 
         if self.glow_mtex:
             texprop.has_glow_texture = True
@@ -349,7 +226,7 @@ class TextureHelper:
                 texeff.num_affected_node_list_pointers += 1
                 texeff.affected_node_list_pointers.update_size()
         texeff.unknown_vector.x = 1.0
-        return self.register_block(texeff)
+        return ObjectHelper.register_block(texeff)
 
     def export_uv_offset(self, shader):
         shader.uv_offset.u = self.base_mtex.offset.x
@@ -360,35 +237,6 @@ class TextureHelper:
     def export_uv_scale(self, shader):
         shader.uv_scale.u = self.base_mtex.scale.x
         shader.uv_scale.v = self.base_mtex.scale.y
-
-        return shader
-
-    def export_shader_flags(self, b_obj, shader):
-
-        b_flag_list = b_obj.niftools_shader.bl_rna.properties.keys()
-        if hasattr(shader, 'shader_flags'):
-            for sf_flag in shader.shader_flags._names:
-                if sf_flag in b_flag_list:
-                    b_flag = b_obj.niftools_shader.get(sf_flag)
-                    if b_flag:
-                        sf_flag_index = shader.shader_flags._names.index(sf_flag)
-                        shader.shader_flags._items[sf_flag_index]._value = 1
-
-        if hasattr(shader, 'shader_flags_1'):
-            for sf_flag in shader.shader_flags_1._names:
-                if sf_flag in b_flag_list:
-                    b_flag = b_obj.niftools_shader.get(sf_flag)
-                    if b_flag:
-                        sf_flag_index = shader.shader_flags_1._names.index(sf_flag)
-                        shader.shader_flags_1._items[sf_flag_index]._value = 1
-
-        if hasattr(shader, 'shader_flags_2'):
-            for sf_flag in shader.shader_flags_2._names:
-                if sf_flag in b_flag_list:
-                    b_flag = b_obj.niftools_shader.get(sf_flag)
-                    if b_flag:
-                        sf_flag_index = shader.shader_flags_2._names.index(sf_flag)
-                        shader.shader_flags_2._items[sf_flag_index]._value = 1
 
         return shader
 
@@ -460,7 +308,7 @@ class TextureHelper:
 
                 # bump map
                 elif b_mat_texslot.use_map_normal and \
-                        b_mat_texslot.texture.use_normal_map == False:
+                        b_mat_texslot.texture.use_normal_map is False:
                     # multi-check
                     if self.bump_mtex:
                         raise nif_utils.NifError("Multiple bump/normal texture in mesh '%s', material '%s'.\n"
@@ -572,3 +420,53 @@ class TextureHelper:
             else:
                 NifLog.warn("Non-UV texture in mesh '{0}', material '{1}'.\nEither delete all non-UV textures or "
                             "create a UV map for every texture associated with selected object and run the script again.".format(b_obj.name, b_mat.name))
+
+    # TODO [texture] Decide whether or not to use this caching, implementation is borked as no b_mat hash store.
+    def has_diffuse_textures(self, b_mat):
+        if self.b_mat == b_mat:
+            return self.diffusetextures
+
+        for b_mat_texslot in texture.get_used_textslots(b_mat):
+            if b_mat_texslot.use and b_mat_texslot.use_map_color_diffuse:
+                self.diffusetextures.append(b_mat_texslot)
+        return self.diffusetextures
+
+    def has_glow_textures(self, b_mat):
+        if self.b_mat == b_mat:
+            return self.glowtextures
+
+        for b_mat_texslot in texture.get_used_textslots(b_mat):
+            if b_mat_texslot.use and b_mat_texslot.use_map_emit:
+                self.glowtextures.append(b_mat_texslot)
+        return self.glowtextures
+
+    def has_bumpmap_textures(self, b_mat):
+        if self.b_mat == b_mat:
+            return self.bumpmaptextures
+
+        for b_mat_texslot in texture.get_used_textslots(b_mat):
+            if b_mat_texslot.use:
+                if b_mat_texslot.texture.use_normal_map is False and \
+                        b_mat_texslot.use_map_color_diffuse is False:
+                    self.bumpmaptextures.append(b_mat_texslot)
+        return self.bumpmaptextures
+
+    def has_gloss_textures(self, b_mat):
+        if self.b_mat == b_mat:
+            return self.glosstextures
+
+        for b_mat_texslot in texture.get_used_textslots(b_mat):
+            if b_mat_texslot.use and b_mat_texslot.use_map_color_spec:
+                self.glosstextures.append(b_mat_texslot)
+        return self.glosstextures
+
+    def has_normalmap_textures(self, b_mat):
+        if self.b_mat == b_mat:
+            return self.normalmaptextures
+
+        for b_mat_texslot in texture.get_used_textslots(b_mat):
+            if b_mat_texslot.use:
+                if b_mat_texslot.use_map_color_diffuse is False and b_mat_texslot.texture.use_normal_map and b_mat_texslot.use_map_normal:
+                    self.normalmaptextures.append(b_mat_texslot)
+        return self.normalmaptextures
+
