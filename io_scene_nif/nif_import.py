7bb2269b1fbb6210533b92f56334468882d4e870
HENDRIX-ZT2 <arnfinn@spholderer.de>
2019-09-12 17:35:07 +0200

    Import: Extract Skinning
    NiSkinInstance processing moved to armature module.

--- io_scene_nif/nif_import.py
+++ io_scene_nif/nif_import.py
@@ -945,76 +945,8 @@ class NifImport(NifCommon):
                         tface.image = imgobj
 
         # import skinning info, for meshes affected by bones
-        skininst = niBlock.skin_instance
-        if skininst:
-            skindata = skininst.data
-            bones = skininst.bones
-            # the usual case
-            if skindata.has_vertex_weights:
-                boneWeights = skindata.bone_list
-                for idx, n_bone in enumerate(bones):
-                    # skip empty bones (see pyffi issue #3114079)
-                    if not n_bone:
-                        continue
-                    vertex_weights = boneWeights[idx].vertex_weights
-                    # if we are in import geom only mode we have not stored the bones in the dict
-                    # groupname = self.dict_names[n_bone]
-                    groupname = self.import_name(n_bone)
-                    if not groupname in b_obj.vertex_groups.items():
-                        v_group = b_obj.vertex_groups.new(groupname)
-                    for skinWeight in vertex_weights:
-                        vert = skinWeight.index
-                        weight = skinWeight.weight
-                        v_group.add([v_map[vert]], weight, 'REPLACE')
-            # WLP2 - hides the weights in the partition
-            else:
-                skinpartition = skininst.skin_partition
-                for block in skinpartition.skin_partition_blocks:
-                    # create all vgroups for this block's bones
-                    block_bone_names = [self.import_name(bones[i]) for i in block.bones]
-                    for groupname in block_bone_names:
-                        b_obj.vertex_groups.new(groupname)
+        self.armaturehelper.import_skin(niBlock, b_obj, v_map)
 
-                    # go over each vert in this block
-                    for vert, vertex_weights, bone_indices in zip(block.vertex_map,
-                                                                  block.vertex_weights,
-                                                                  block.bone_indices):
-                        # assign this vert's 4 weights to its 4 vgroups (at max)
-                        for w, b_i in zip(vertex_weights, bone_indices):
-                            if w > 0:
-                                groupname = block_bone_names[b_i]
-                                v_group = b_obj.vertex_groups[groupname]
-                                v_group.add([v_map[vert]], w, 'REPLACE')
-                    
-
-        # import body parts as vertex groups
-        if isinstance(skininst, NifFormat.BSDismemberSkinInstance):
-            skinpart_list = []
-            bodypart_flag = []
-            skinpart = niBlock.get_skin_partition()
-            for bodypart, skinpartblock in zip(
-                skininst.partitions, skinpart.skin_partition_blocks):
-                bodypart_wrap = NifFormat.BSDismemberBodyPartType()
-                bodypart_wrap.set_value(bodypart.body_part)
-                groupname = bodypart_wrap.get_detail_display()
-                # create vertex group if it did not exist yet
-                if not(groupname in b_obj.vertex_groups.items()):
-                    v_group = b_obj.vertex_groups.new(groupname)
-                    skinpart_index = len(skinpart_list)
-                    skinpart_list.append((skinpart_index, groupname))
-                    bodypart_flag.append(bodypart.part_flag)
-                # find vertex indices of this group
-                groupverts = [v_map[v_index]
-                              for v_index in skinpartblock.vertex_map]
-                # create the group
-                v_group.add(groupverts, 1, 'ADD')
-            b_obj.niftools_part_flags_panel.pf_partcount = len(skinpart_list)
-            for i, pl_name in skinpart_list:
-                b_obj_partflag = b_obj.niftools_part_flags.add()
-                # b_obj.niftools_part_flags.pf_partint = (i)
-                b_obj_partflag.name = (pl_name)
-                b_obj_partflag.pf_editorflag = (bodypart_flag[i].pf_editor_visible)
-                b_obj_partflag.pf_startflag = (bodypart_flag[i].pf_start_net_boneset)
         # import morph controller
         if NifOp.props.animation:
             self.animationhelper.object_animation.import_morph_controller(niBlock, b_obj, v_map)
