e2ddd3919fa560a625958d02a79f569b4d6e3cb5
neomonkeus <neomonkeus@gmail.com>
2019-09-21 19:06:57 +0100

    Apply pep8 formatting to the bhk sphere test.

--- testframework/integration/modules/collision/bhkshape/bhksphereshape/test_sphereshape.py
+++ testframework/integration/modules/collision/bhkshape/bhksphereshape/test_sphereshape.py
@@ -83,38 +83,38 @@ class TestCollisionBhkSphereShape(SingleNif):
 
     def n_create_data(self):
         n_gen_geometry.n_create_blocks(self.n_data)
-        
+
         n_ninode = self.n_data.roots[0]
         n_gen_collision.n_attach_bsx_flag(n_ninode)
-        
+
         # generate common collision tree
         n_bhkcolobj = n_gen_collision.n_attach_bhkcollisionobject(n_ninode)
         n_bhkrigidbody = n_gen_collision.n_attach_bhkrigidbody(n_bhkcolobj)
-        
+
         # generate bhkboxshape specific data
         n_gen_bhkboxshape.n_update_bhkrigidbody(n_bhkrigidbody)
         n_bhktransform = n_gen_bhkboxshape.n_attach_bhkconvextransform(n_bhkrigidbody)
         n_gen_bhkboxshape.n_attach_bhkboxshape(n_bhktransform)
-        
+
         return self.n_data
 
     def n_check_data(self):
         n_ninode = self.n_data.roots[0]
-        
+
         # bsx flag
         # nose.tools.assert_equal(n_ninode.num_extra_data_list, 2) # TODO [object] UPB
         n_bsxflag = n_ninode.extra_data_list[0]
         n_gen_collision.n_check_bsx_flag(n_bsxflag)
-        
+
         # check common collision
         n_bhkcollisionobject = n_gen_collision.n_check_bhkcollisionobject_data(n_ninode)
-        n_bhkrigidbody =  n_gen_collision.n_check_bhkrigidbody_data(n_bhkcollisionobject)
-        
+        n_bhkrigidbody = n_gen_collision.n_check_bhkrigidbody_data(n_bhkcollisionobject)
+
         # check bhkboxshape specific data
         n_gen_bhkboxshape.n_check_bhkrigidbody_data(n_bhkrigidbody)
         n_bhktransform = n_gen_bhkboxshape.n_check_bhkconvextransform_data(n_bhkrigidbody)
         n_gen_bhkboxshape.n_check_bhkboxshape_data(n_bhktransform)
-        
+
         # geometry
         n_trishape = n_ninode.children[0]
         n_gen_geometry.n_check_trishape(n_trishape)
