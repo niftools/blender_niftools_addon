751486164a6fc61f0499d9891e0829d1cd58d99a
neomonkeus <neomonkeus@gmail.com>
2019-11-22 17:30:31 +0000

    Add in pyffi as an external dependency.

--- install/install_deps.bat
+++ install/install_deps.bat
@@ -1,3 +1,4 @@
-:: Script to install developer dependancies for the Blender Nif plugin
+:: Script to install developer dependencies for the Blender Nif plugin
 python -m pip install Sphinx --target="%BLENDER_ADDONS_DIR"%\dependencies
 python -m pip install nose --target="%BLENDER_ADDONS_DIR"%\dependencies
+python -m pip install PyFFI=2.2.4.dev0 --target="%BLENDER_ADDONS_DIR"%\dependencies
--- install/install_deps.sh
+++ install/install_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-#Script to install developer dependancies for the Blender Nif Plugin
+#Script to install developer dependencies for the Blender Nif Plugin
 python -m pip install Sphinx --target="${BLENDER_ADDONS_DIR}\dependencies"
 python -m pip install nose --target="${BLENDER_ADDONS_DIR}\dependencies"
+python -m pip install PyFFI=2.2.4.dev0 --target="${BLENDER_ADDONS_DIR}\dependencies"
