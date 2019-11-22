03254eaf9a419ff78ec46f02d80e5361b8065dd2
neomonkeus <neomonkeus@gmail.com>
2019-11-22 21:41:06 +0000

    Externalise pyffi dependency setup.

--- install/install_deps.sh
+++ install/install_deps.sh
@@ -5,4 +5,3 @@ SCRIPT_DIR="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 #Script to install developer dependencies for the Blender Nif Plugin
 python -m pip install Sphinx --target="${BLENDER_ADDONS_DIR:-${SCRIPT_DIR}}/dependencies"
 python -m pip install nose --target="${BLENDER_ADDONS_DIR:-${SCRIPT_DIR}}/dependencies"
-python -m pip install 'PyFFI==2.2.4.dev0' --target="${BLENDER_ADDONS_DIR:-${SCRIPT_DIR}}/dependencies"
