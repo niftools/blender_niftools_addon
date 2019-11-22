bb760b209d7e35fb41e62cb3994e9d24e2cef21b
neomonkeus <neomonkeus@gmail.com>
2019-11-22 21:27:49 +0000

    Add in a default for dependency cache.

--- install/install_deps.bat
+++ install/install_deps.bat
@@ -1,4 +1,8 @@
 :: Script to install developer dependencies for the Blender Nif plugin
+
+:: TODO Add default localion in case variable is not set
+:: Eg. SCRIPT_DIR="$( cd "$(dirname "$0")" || exit ; pwd -P )"
+
 python -m pip install Sphinx --target="%BLENDER_ADDONS_DIR"%\dependencies
 python -m pip install nose --target="%BLENDER_ADDONS_DIR"%\dependencies
 python -m pip install PyFFI=2.2.4.dev0 --target="%BLENDER_ADDONS_DIR"%\dependencies
