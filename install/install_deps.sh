e8019743ca773cae6f2b108a29b49ff767c3fde8
neomonkeus <neomonkeus@gmail.com>
2019-11-22 21:27:49 +0000

    Add in a default for dependency cache.

--- install/install_deps.sh
+++ install/install_deps.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+SCRIPT_DIR="$( cd "$(dirname "$0")" || exit ; pwd -P )"
+
 #Script to install developer dependencies for the Blender Nif Plugin
-python -m pip install Sphinx --target="${BLENDER_ADDONS_DIR}\dependencies"
-python -m pip install nose --target="${BLENDER_ADDONS_DIR}\dependencies"
-python -m pip install PyFFI=2.2.4.dev0 --target="${BLENDER_ADDONS_DIR}\dependencies"
+python -m pip install Sphinx --target="${BLENDER_ADDONS_DIR:-${SCRIPT_DIR}}/dependencies"
+python -m pip install nose --target="${BLENDER_ADDONS_DIR:-${SCRIPT_DIR}}/dependencies"
+python -m pip install 'PyFFI==2.2.4.dev0' --target="${BLENDER_ADDONS_DIR:-${SCRIPT_DIR}}/dependencies"
