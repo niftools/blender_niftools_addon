5402127aa0895e9d5ec5a6d634e2c8ba845e9e4b
neomonkeus <neomonkeus@gmail.com>
2019-11-23 00:29:18 +0000

    Resolve pep8 issues for nif utils module.

--- io_scene_nif/utility/nif_utils.py
+++ io_scene_nif/utility/nif_utils.py
@@ -1,6 +1,4 @@
 """ Nif Utilities, stores common code that is used across the code base"""
-
-
 # ***** BEGIN LICENSE BLOCK *****
 # 
 # Copyright © 2005-2015, NIF File Format Library and Tools contributors.
@@ -38,11 +36,11 @@
 #
 # ***** END LICENSE BLOCK *****
 
-import mathutils
 import math
 
+import mathutils
+
 from io_scene_nif.utility.nif_logging import NifLog
-from io_scene_nif.utility.nif_global import NifOp
 
 
 class NifError(Exception):
@@ -51,15 +49,15 @@ class NifError(Exception):
 
 
 def vec_roll_to_mat3(vec, roll):
-    #port of the updated C function from armature.c
-    #https://developer.blender.org/T39470
-    #note that C accesses columns first, so all matrix indices are swapped compared to the C version
-    
+    # port of the updated C function from armature.c
+    # https://developer.blender.org/T39470
+    # note that C accesses columns first, so all matrix indices are swapped compared to the C version
+
     nor = vec.normalized()
     THETA_THRESHOLD_NEGY = 1.0e-9
     THETA_THRESHOLD_NEGY_CLOSE = 1.0e-5
-    
-    #create a 3x3 matrix
+
+    # create a 3x3 matrix
     bMatrix = mathutils.Matrix().to_3x3()
 
     theta = 1.0 + nor[1]
@@ -72,32 +70,33 @@ def vec_roll_to_mat3(vec, roll):
         bMatrix[2][1] = nor[2]
         bMatrix[1][2] = -nor[2]
         if theta > THETA_THRESHOLD_NEGY_CLOSE:
-            #If nor is far enough from -Y, apply the general case.
+            # If nor is far enough from -Y, apply the general case.
             bMatrix[0][0] = 1 - nor[0] * nor[0] / theta
             bMatrix[2][2] = 1 - nor[2] * nor[2] / theta
             bMatrix[0][2] = bMatrix[2][0] = -nor[0] * nor[2] / theta
-        
+
         else:
-            #If nor is too close to -Y, apply the special case.
+            # If nor is too close to -Y, apply the special case.
             theta = nor[0] * nor[0] + nor[2] * nor[2]
             bMatrix[0][0] = (nor[0] + nor[2]) * (nor[0] - nor[2]) / -theta
             bMatrix[2][2] = -bMatrix[0][0]
             bMatrix[0][2] = bMatrix[2][0] = 2.0 * nor[0] * nor[2] / theta
 
     else:
-        #If nor is -Y, simple symmetry by Z axis.
+        # If nor is -Y, simple symmetry by Z axis.
         bMatrix = mathutils.Matrix().to_3x3()
         bMatrix[0][0] = bMatrix[1][1] = -1.0
 
-    #Make Roll matrix
+    # Make Roll matrix
     rMatrix = mathutils.Matrix.Rotation(roll, 3, nor)
-    
-    #Combine and output result
+
+    # Combine and output result
     mat = rMatrix * bMatrix
     return mat
 
+
 def mat3_to_vec_roll(mat):
-    #this hasn't changed
+    # this hasn't changed
     vec = mat.col[1]
     vecmat = vec_roll_to_mat3(mat.col[1], 0)
     vecmatinv = vecmat.inverted()
@@ -105,9 +104,11 @@ def mat3_to_vec_roll(mat):
     roll = math.atan2(rollmat[0][2], rollmat[2][2])
     return vec, roll
 
-def import_matrix(niBlock, relative_to=None):
-    """Retrieves a niBlock's transform matrix as a Mathutil.Matrix."""
-    return mathutils.Matrix( niBlock.get_transform(relative_to).as_list() ).transposed()
+
+def import_matrix(n_block, relative_to=None):
+    """Retrieves a n_block's transform matrix as a Mathutil.Matrix."""
+    return mathutils.Matrix(n_block.get_transform(relative_to).as_list()).transposed()
+
 
 def decompose_srt(matrix):
     """Decompose Blender transform matrix as a scale, rotation matrix, and
@@ -116,37 +117,37 @@ def decompose_srt(matrix):
     # get matrix components
     trans_vec, rot_quat, scale_vec = matrix.decompose()
 
-    #obtain a combined scale and rotation matrix to test determinate
+    # obtain a combined scale and rotation matrix to test determinate
     rotmat = rot_quat.to_matrix()
-    scalemat = mathutils.Matrix(   ((scale_vec[0], 0.0, 0.0),
-                                    (0.0, scale_vec[1], 0.0),
-                                    (0.0, 0.0, scale_vec[2])) )
+    scalemat = mathutils.Matrix(((scale_vec[0], 0.0, 0.0),
+                                 (0.0, scale_vec[1], 0.0),
+                                 (0.0, 0.0, scale_vec[2])))
     scale_rot = scalemat * rotmat
 
     # and fix their sign
-    if (scale_rot.determinant() < 0): scale_vec.negate()
-    # only uniform scaling
-    # allow rather large error to accomodate some nifs
-    if abs(scale_vec[0]-scale_vec[1]) + abs(scale_vec[1]-scale_vec[2]) > 0.02:
-        NifLog.warn("Non-uniform scaling not supported." +
-            " Workaround: apply size and rotation (CTRL-A).")
+    if scale_rot.determinant() < 0:
+        scale_vec.negate()
+
+    # only uniform scaling allow rather large error to accommodate some nifs
+    if abs(scale_vec[0] - scale_vec[1]) + abs(scale_vec[1] - scale_vec[2]) > 0.02:
+        NifLog.warn("Non-uniform scaling not supported. Workaround: apply size and rotation (CTRL-A).")
     return [scale_vec[0], rotmat, trans_vec]
 
 
-def find_property(niBlock, property_type):
+def find_property(n_block, property_type):
     """Find a property."""
-    for prop in niBlock.properties:
+    for prop in n_block.properties:
         if isinstance(prop, property_type):
             return prop
-    for prop in niBlock.bs_properties:
+    for prop in n_block.bs_properties:
         if isinstance(prop, property_type):
             return prop
     return None
 
 
-def find_controller(niBlock, controller_type):
+def find_controller(n_block, controller_type):
     """Find a controller."""
-    ctrl = niBlock.controller
+    ctrl = n_block.controller
     while ctrl:
         if isinstance(ctrl, controller_type):
             break
@@ -154,12 +155,12 @@ def find_controller(niBlock, controller_type):
     return ctrl
 
 
-def find_extra(niBlock, extratype):
+def find_extra(n_block, extratype):
     # TODO: 3.0 - Optimise
-    
+
     """Find extra data."""
     # pre-10.x.x.x system: extra data chain
-    extra = niBlock.extra_data
+    extra = n_block.extra_data
     while extra:
         if isinstance(extra, extratype):
             break
@@ -168,7 +169,7 @@ def find_extra(niBlock, extratype):
         return extra
 
     # post-10.x.x.x system: extra data list
-    for extra in niBlock.extra_data_list:
+    for extra in n_block.extra_data_list:
         if isinstance(extra, extratype):
             return extra
     return None
