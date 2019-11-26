c0d53d6ff725ec71e2b7e2d8739a8b3f09883021
neomonkeus <neomonkeus@gmail.com>
2019-11-26 10:47:37 +0000

    Wrap the morph export in a class.

--- io_scene_nif/modules/geometry/morph/morph_export.py
+++ io_scene_nif/modules/geometry/morph/morph_export.py
@@ -36,8 +36,10 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 # ***** END LICENSE BLOCK *****
+import os
 
 import bpy
+from pyffi.formats.egm import EgmFormat
 from pyffi.formats.nif import NifFormat
 
 from io_scene_nif.modules.obj.block_registry import block_store
@@ -45,129 +47,151 @@ from io_scene_nif.utility.nif_global import NifOp
 from io_scene_nif.utility.nif_logging import NifLog
 
 
-def export_morph(self, b_mesh, b_obj, tridata, trishape, vertlist, vertmap):
-    # shape key morphing
-    key = b_mesh.shape_keys
-    if key:
-        if len(key.key_blocks) > 1:
-            # yes, there is a key object attached
-            # export as egm, or as morph_data?
-            if key.key_blocks[1].name.startswith("EGM"):
-                # egm export!
-                self.exportEgm(key.key_blocks)
-            # TODO [animation] pull out to
-            elif key.ipo:
-                # regular morph_data export
-                # (there must be a shape ipo)
-                keyipo = key.ipo
-                # check that they are relative shape keys
-                if not key.relative:
-                    # XXX if we do "key.relative = True"
-                    # XXX would this automatically fix the keys?
-                    raise ValueError("Can only export relative shape keys.")
+class MorphHelper:
 
-                # create geometry morph controller
-                morph_ctrl = block_store.create_block("NiGeomMorpherController", keyipo)
-                morph_ctrl.target = trishape
-                morph_ctrl.frequency = 1.0
-                morph_ctrl.phase = 0.0
-                trishape.add_controller(morph_ctrl)
-                ctrl_start = 1000000.0
-                ctrl_stop = -1000000.0
-                ctrl_flags = 0x000c
+    def __init__(self):
+        self.egm_data = None
 
-                # create geometry morph data
-                morph_data = block_store.create_block("NiMorphData", keyipo)
-                morph_ctrl.data = morph_data
-                morph_data.num_morphs = len(key.key_blocks)
-                morph_data.num_vertices = len(vertlist)
-                morph_data.morphs.update_size()
+    def export_morph(self, b_mesh, b_obj, tridata, trishape, vertlist, vertmap):
+        # shape key morphing
+        key = b_mesh.shape_keys
+        if key:
+            if len(key.key_blocks) > 1:
+                # yes, there is a key object attached
+                # export as egm, or as morph_data?
+                if key.key_blocks[1].name.startswith("EGM"):
+                    # egm export!
+                    self.export_egm(key.key_blocks)
+                # TODO [animation] pull out to
+                elif key.ipo:
+                    # regular morph_data export
+                    # (there must be a shape ipo)
+                    keyipo = key.ipo
+                    # check that they are relative shape keys
+                    if not key.relative:
+                        # XXX if we do "key.relative = True"
+                        # XXX would this automatically fix the keys?
+                        raise ValueError("Can only export relative shape keys.")
 
-                # create interpolators (for newer nif versions)
-                morph_ctrl.num_interpolators = len(key.key_blocks)
-                morph_ctrl.interpolators.update_size()
+                    # create geometry morph controller
+                    morph_ctrl = block_store.create_block("NiGeomMorpherController", keyipo)
+                    morph_ctrl.target = trishape
+                    morph_ctrl.frequency = 1.0
+                    morph_ctrl.phase = 0.0
+                    trishape.add_controller(morph_ctrl)
+                    ctrl_start = 1000000.0
+                    ctrl_stop = -1000000.0
+                    ctrl_flags = 0x000c
 
-                # interpolator weights (for Fallout 3)
-                morph_ctrl.interpolator_weights.update_size()
+                    # create geometry morph data
+                    morph_data = block_store.create_block("NiMorphData", keyipo)
+                    morph_ctrl.data = morph_data
+                    morph_data.num_morphs = len(key.key_blocks)
+                    morph_data.num_vertices = len(vertlist)
+                    morph_data.morphs.update_size()
 
-                # TODO [morph] some unknowns, bethesda only
-                # TODO [morph] just guessing here, data seems to be zero always
-                morph_ctrl.num_unknown_ints = len(key.key_blocks)
-                morph_ctrl.unknown_ints.update_size()
+                    # create interpolators (for newer nif versions)
+                    morph_ctrl.num_interpolators = len(key.key_blocks)
+                    morph_ctrl.interpolators.update_size()
 
-                for keyblocknum, keyblock in enumerate(key.key_blocks):
-                    # export morphed vertices
-                    morph = morph_data.morphs[keyblocknum]
-                    morph.frame_name = keyblock.name
-                    NifLog.info("Exporting morph {0}: vertices".format(keyblock.name))
-                    morph.arg = morph_data.num_vertices
-                    morph.vectors.update_size()
-                    for b_v_index, (vert_indices, vert) in enumerate(list(zip(vertmap, keyblock.data))):
-                        # vertmap check
-                        if not vert_indices:
+                    # interpolator weights (for Fallout 3)
+                    morph_ctrl.interpolator_weights.update_size()
+
+                    # TODO [morph] some unknowns, bethesda only
+                    # TODO [morph] just guessing here, data seems to be zero always
+                    morph_ctrl.num_unknown_ints = len(key.key_blocks)
+                    morph_ctrl.unknown_ints.update_size()
+
+                    for keyblocknum, keyblock in enumerate(key.key_blocks):
+                        # export morphed vertices
+                        morph = morph_data.morphs[keyblocknum]
+                        morph.frame_name = keyblock.name
+                        NifLog.info("Exporting morph {0}: vertices".format(keyblock.name))
+                        morph.arg = morph_data.num_vertices
+                        morph.vectors.update_size()
+                        for b_v_index, (vert_indices, vert) in enumerate(list(zip(vertmap, keyblock.data))):
+                            # vertmap check
+                            if not vert_indices:
+                                continue
+                            # copy vertex and assign morph vertex
+                            mv = vert.copy()
+                            if keyblocknum > 0:
+                                mv.x -= b_mesh.vertices[b_v_index].co.x
+                                mv.y -= b_mesh.vertices[b_v_index].co.y
+                                mv.z -= b_mesh.vertices[b_v_index].co.z
+                            for vert_index in vert_indices:
+                                morph.vectors[vert_index].x = mv.x
+                                morph.vectors[vert_index].y = mv.y
+                                morph.vectors[vert_index].z = mv.z
+
+                        # export ipo shape key curve
+                        curve = keyipo[keyblock.name]
+
+                        # create interpolator for shape key (needs to be there even if there is no curve)
+                        interpol = block_store.create_block("NiFloatInterpolator")
+                        interpol.value = 0
+                        morph_ctrl.interpolators[keyblocknum] = interpol
+                        # fallout 3 stores interpolators inside the interpolator_weights block
+                        morph_ctrl.interpolator_weights[keyblocknum].interpolator = interpol
+
+                        # geometry only export has no float data also skip keys that have no curve (such as base key)
+                        if NifOp.props.animation == 'GEOM_NIF' or not curve:
                             continue
-                        # copy vertex and assign morph vertex
-                        mv = vert.copy()
-                        if keyblocknum > 0:
-                            mv.x -= b_mesh.vertices[b_v_index].co.x
-                            mv.y -= b_mesh.vertices[b_v_index].co.y
-                            mv.z -= b_mesh.vertices[b_v_index].co.z
-                        for vert_index in vert_indices:
-                            morph.vectors[vert_index].x = mv.x
-                            morph.vectors[vert_index].y = mv.y
-                            morph.vectors[vert_index].z = mv.z
 
-                    # export ipo shape key curve
-                    curve = keyipo[keyblock.name]
+                        # note: we set data on morph for older nifs and on floatdata for newer nifs
+                        # of course only one of these will be actually written to the file
+                        NifLog.info("Exporting morph {0}: curve".format(keyblock.name))
+                        interpol.data = block_store.create_block("NiFloatData", curve)
+                        floatdata = interpol.data.data
+                        if curve.getExtrapolation() == "Constant":
+                            ctrl_flags = 0x000c
+                        elif curve.getExtrapolation() == "Cyclic":
+                            ctrl_flags = 0x0008
 
-                    # create interpolator for shape key (needs to be there even if there is no curve)
-                    interpol = block_store.create_block("NiFloatInterpolator")
-                    interpol.value = 0
-                    morph_ctrl.interpolators[keyblocknum] = interpol
-                    # fallout 3 stores interpolators inside the interpolator_weights block
-                    morph_ctrl.interpolator_weights[keyblocknum].interpolator = interpol
+                        morph.interpolation = NifFormat.KeyType.LINEAR_KEY
+                        morph.num_keys = len(curve.getPoints())
+                        morph.keys.update_size()
 
-                    # geometry only export has no float data also skip keys that have no curve (such as base key)
-                    if NifOp.props.animation == 'GEOM_NIF' or not curve:
-                        continue
+                        floatdata.interpolation = NifFormat.KeyType.LINEAR_KEY
+                        floatdata.num_keys = len(curve.getPoints())
+                        floatdata.keys.update_size()
 
-                    # note: we set data on morph for older nifs and on floatdata for newer nifs
-                    # of course only one of these will be actually written to the file
-                    NifLog.info("Exporting morph {0}: curve".format(keyblock.name))
-                    interpol.data = block_store.create_block("NiFloatData", curve)
-                    floatdata = interpol.data.data
-                    if curve.getExtrapolation() == "Constant":
-                        ctrl_flags = 0x000c
-                    elif curve.getExtrapolation() == "Cyclic":
-                        ctrl_flags = 0x0008
+                        start = bpy.context.scene.frame_start
+                        fps = NifOp.context.scene.render.fps
+                        for i, btriple in enumerate(curve.getPoints()):
+                            knot = btriple.getPoints()
+                            morph.keys[i].arg = morph.interpolation
+                            morph.keys[i].time = (knot[0] - start) * fps
+                            morph.keys[i].value = curve.evaluate(knot[0])
+                            # morph.keys[i].forwardTangent = 0.0 # ?
+                            # morph.keys[i].backwardTangent = 0.0 # ?
+                            floatdata.keys[i].arg = floatdata.interpolation
+                            floatdata.keys[i].time = (knot[0] - start) * fps
+                            floatdata.keys[i].value = curve.evaluate(knot[0])
+                            # floatdata.keys[i].forwardTangent = 0.0 # ?
+                            # floatdata.keys[i].backwardTangent = 0.0 # ?
+                            ctrl_start = min(ctrl_start, morph.keys[i].time)
+                            ctrl_stop = max(ctrl_stop, morph.keys[i].time)
+                    morph_ctrl.flags = ctrl_flags
+                    morph_ctrl.start_time = ctrl_start
+                    morph_ctrl.stop_time = ctrl_stop
 
-                    morph.interpolation = NifFormat.KeyType.LINEAR_KEY
-                    morph.num_keys = len(curve.getPoints())
-                    morph.keys.update_size()
+                    # fix data consistency type
+                    tridata.consistency_flags = b_obj.niftools.consistency_flags
 
-                    floatdata.interpolation = NifFormat.KeyType.LINEAR_KEY
-                    floatdata.num_keys = len(curve.getPoints())
-                    floatdata.keys.update_size()
+    def export_egm(self, key_blocks):
+        self.egm_data = EgmFormat.Data(num_vertices=len(key_blocks[0].data))
+        for key_block in key_blocks:
+            if key_block.name.startswith("EGM SYM"):
+                morph = self.egm_data.add_sym_morph()
+            elif key_block.name.startswith("EGM ASYM"):
+                morph = self.egm_data.add_asym_morph()
+            else:
+                continue
+            NifLog.info("Exporting morph %s to egm" % key_block.name)
+            relative_vertices = []
 
-                    start = bpy.context.scene.frame_start
-                    fps = NifOp.context.scene.render.fps
-                    for i, btriple in enumerate(curve.getPoints()):
-                        knot = btriple.getPoints()
-                        morph.keys[i].arg = morph.interpolation
-                        morph.keys[i].time = (knot[0] - start) * fps
-                        morph.keys[i].value = curve.evaluate(knot[0])
-                        # morph.keys[i].forwardTangent = 0.0 # ?
-                        # morph.keys[i].backwardTangent = 0.0 # ?
-                        floatdata.keys[i].arg = floatdata.interpolation
-                        floatdata.keys[i].time = (knot[0] - start) * fps
-                        floatdata.keys[i].value = curve.evaluate(knot[0])
-                        # floatdata.keys[i].forwardTangent = 0.0 # ?
-                        # floatdata.keys[i].backwardTangent = 0.0 # ?
-                        ctrl_start = min(ctrl_start, morph.keys[i].time)
-                        ctrl_stop = max(ctrl_stop, morph.keys[i].time)
-                morph_ctrl.flags = ctrl_flags
-                morph_ctrl.start_time = ctrl_start
-                morph_ctrl.stop_time = ctrl_stop
-
-                # fix data consistency type
-                tridata.consistency_flags = b_obj.niftools.consistency_flags
+            # note: key_blocks[0] is base key
+            for vert, key_vert in zip(key_blocks[0].data, key_block.data):
+                relative_vertices.append(key_vert - vert)
+            morph.set_relative_vertices(relative_vertices)
