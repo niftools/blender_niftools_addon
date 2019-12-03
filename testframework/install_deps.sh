e548ebfc2eb71d8a3eb9286ae2473f0a40d70447
neomonkeus <neomonkeus@gmail.com>
2019-12-03 20:22:36 +0000

    Add pyffi dependency for tests, commented for now as points to dev ver.

--- testframework/install_deps.sh
+++ testframework/install_deps.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 # Install developer dependencies for integration tests
 
 if [[ "${BLENDER_ADDONS_DIR}" == "" || ! -e "${BLENDER_ADDONS_DIR}"  ]]; then
     echo "Update BLENDER_ADDONS_DIR to the folder where the blender addons reside, such as:"
     echo "set BLENDER_ADDONS_DIR=~/Blender Foundation/Blender/2.79/scripts/addons"
-    echo "\n"
+    echo
     exit 1
 fi
 
 echo "Installing Sphinx to ${BLENDER_ADDONS_DIR}"
-python -m pip install Sphinx nose pyffi --target="${BLENDER_ADDONS_DIR}"
+python -m pip install Sphinx nose --target="${BLENDER_ADDONS_DIR}"
 
 # Currently installing pyffi from pypi fails for runtime classes
-# python -m pip install pyffi --target="${BLENDER_ADDONS_DIR}"
+# python -m pip install PyFFI==2.2.4.dev2 --target="${BLENDER_ADDONS_DIR}"
