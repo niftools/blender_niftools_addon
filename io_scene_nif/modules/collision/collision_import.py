4dfdb4d1c9c82f695fd2cb99e04a46e466fbb34a
HENDRIX-ZT2 <arnfinn@spholderer.de>
2019-12-04 18:06:32 +0100

    Several fixes
    Resolve some efactoring hierarchy changes + typos
    
    Anim export: add another check for presence of anim_data

--- io_scene_nif/modules/collision/collision_import.py
+++ io_scene_nif/modules/collision/collision_import.py
@@ -88,7 +88,9 @@ class Collision:
             for mat_type in ("material", "oblivion_havok_material", "fallout_3_havok_material", "skyrim_havok_material"):
                 havok_material = getattr(n_obj, mat_type, None)
                 if havok_material:
-                    mat_name = str(havok_material.material)
+                    # todo [collision] fix this
+                    # mat_name = str(havok_material.material)
+                    mat_name = str(havok_material)
                     b_mat = get_material(mat_name)
                     b_me.materials.append(b_mat)
 
