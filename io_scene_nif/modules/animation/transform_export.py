e1c0d8a0496d2328ce18556f4e87ad2a6f69283e
HENDRIX-ZT2 <arnfinn@spholderer.de>
2019-12-06 18:05:00 +0100

    Refactor export_transforms()
    Update call signature; can no longer be used without b_obj

--- io_scene_nif/modules/animation/transform_export.py
+++ io_scene_nif/modules/animation/transform_export.py
@@ -68,27 +68,25 @@ class TransformAnimation:
             key = [k.co[1] for k in point]
             yield frame, MathutilsClass(key)
 
-    def export_transforms(self, parent_block, b_obj=None, bone=None):
+    def export_transforms(self, parent_block, b_obj, bone=None):
         """
-        If called on b_obj=None and bone=None it should save an empty controller.
-        If called on an b_obj = type(armature), it expects a bone too.
-        If called on an object, with bone=None, it exports object level animation.
+        If bone == None, object level animation is exported.
+        If a bone is given, skeletal animation is exported.
         """
-        
-        target_name  = ""
-        # sometimes we need to export an empty keyframe... 
-        scale_curve = []
-        quat_curve = []
-        euler_curve = []
-        trans_curve = []
 
-        exp_fcurves = []
+        # todo [anim] maybe refactor calling behavior to specify desired blender action
+        #             will allow for simple batch export of multiple kf files later on
+
+        # blender object must exist
+        assert(b_obj)
+        # if a bone, b_obj must be an armature
+        if bone:
+            assert( type(b_obj.data) == bpy.types.Armature )
 
         # just for more detailed error reporting later on
         bonestr = ""
-
-        # we have either skeletal or object animation
-        if b_obj and b_obj.animation_data and b_obj.animation_data.action:
+        # check if the blender object has an action assigned to it
+        if b_obj.animation_data and b_obj.animation_data.action:
             action = b_obj.animation_data.action
 
             # skeletal animation - with bone correction & coordinate corrections
@@ -114,21 +112,19 @@ class TransformAnimation:
                 bind_matrix = b_obj.matrix_parent_inverse
                 exp_fcurves = [fcu for fcu in action.fcurves if
                                fcu.data_path in ("rotation_quaternion", "rotation_euler", "location", "scale")]
-            # decompose the bind matrix
-            if exp_fcurves:
-                bind_scale, bind_rot, bind_trans = nif_utils.decompose_srt(bind_matrix)
-                bind_rot = bind_rot.to_4x4()
-            start_frame, stop_frame = action.frame_range
-
-        # we are supposed to export an empty controller
+            else:
+                # bone isn't keyframed in this action, nothing to do here
+                return
         else:
-            # only set frame range
-            start_frame = bpy.context.scene.frame_start
-            stop_frame = bpy.context.scene.frame_end
-
+            # no action is attached to b_obj, nothing to do here
+            return
+        # decompose the bind matrix
+        bind_scale, bind_rot, bind_trans = nif_utils.decompose_srt(bind_matrix)
+        bind_rot = bind_rot.to_4x4()
         n_kfc, n_kfi = self.nif_export.animationhelper.create_controller(parent_block, target_name)
 
         # fill in the non-trivial values
+        start_frame, stop_frame = action.frame_range
         animation_export.set_flags_and_timing(n_kfc, exp_fcurves, start_frame, stop_frame)
 
         # get the desired fcurves for each data type from exp_fcurves
@@ -137,79 +133,73 @@ class TransformAnimation:
         eulers = [fcu for fcu in exp_fcurves if fcu.data_path.endswith("euler")]
         scales = [fcu for fcu in exp_fcurves if fcu.data_path.endswith("scale")]
 
-        # go over all fcurves collected above and transform and store all keys
-        if scales:
+        # ensure that those groups that are present have all their fcurves
+        for fcus, num_fcus in ( (quaternions, 4),
+                                (eulers, 3),
+                                (translations, 3),
+                                (scales, 3) ):
+            if fcus and len(fcus) != num_fcus:
+                raise nif_utils.NifError("Incomplete key set {} for action {}. Ensure that if a bone is keyframed for a property, all channels are keyframed.".format(bonestr, action.name))
+
+        # go over all fcurves collected above and transform and store all their keys
+        quat_curve = []
+        euler_curve = []
+        trans_curve = []
+        scale_curve = []
+        for frame, quat in self.iter_frame_key(quaternions, mathutils.Quaternion):
+            quat = armature.export_keymat(bind_rot, quat.to_matrix().to_4x4(), bone).to_quaternion()
+            quat_curve.append((frame, quat))
+
+        for frame, euler in self.iter_frame_key(eulers, mathutils.Euler):
+            keymat = armature.export_keymat(bind_rot, euler.to_matrix().to_4x4(), bone)
+            euler = keymat.to_euler("XYZ", euler)
+            euler_curve.append((frame, euler))
+
+        for frame, trans in self.iter_frame_key(translations, mathutils.Vector):
+            keymat = armature.export_keymat(bind_rot, mathutils.Matrix.Translation(trans), bone)
+            trans = keymat.to_translation() + bind_trans
+            trans_curve.append((frame, trans))
+
+        for frame, scale in self.iter_frame_key(scales, mathutils.Vector):
             # just use the first scale curve and assume even scale over all curves
-            for frame, scale in self.iter_frame_key(scales, mathutils.Vector):
-                scale_curve.append((frame, scale[0]))
-
-        if quaternions:
-            if len(quaternions) != 4:
-                raise nif_utils.NifError("Incomplete ROT key set {} for action {}".format(bonestr, action.name))
+            scale_curve.append((frame, scale[0]))
+        
+        if n_kfi:
+            if max(len(c) for c in (quat_curve, euler_curve, trans_curve, scale_curve)) > 1:
+                # number of frames is > 1, so add transform data
+                n_kfd = block_store.create_block("NiTransformData", exp_fcurves)
+                n_kfi.data = n_kfd
             else:
-                for frame, quat in self.iter_frame_key(quaternions, mathutils.Quaternion):
-                    quat = armature.export_keymat(bind_rot, quat.to_matrix().to_4x4(), bone).to_quaternion()
-                    quat_curve.append((frame, quat))
+                # only add data if number of keys is > 1
+                # (see importer comments with import_kf_root: a single frame
+                # keyframe denotes an interpolator without further data)
+                # insufficient keys, so set the data and we're done!
+                if trans_curve:
+                    trans = trans_curve[0][1]
+                    n_kfi.translation.x, n_kfi.translation.y, n_kfi.translation.z = trans
+                if quat_curve:
+                    quat = quat_curve[0][1]
+                elif euler_curve:
+                    quat = euler_curve[0][1].to_quaternion()
+                if quat_curve or euler_curve:
+                    n_kfi.rotation.x = quat.x
+                    n_kfi.rotation.y = quat.y
+                    n_kfi.rotation.z = quat.z
+                    n_kfi.rotation.w = quat.w
+                # ignore scale for now...
+                n_kfi.scale = 1.0
+                # no need to add any keys, done
+                return
 
-        if eulers:
-            if len(eulers) != 3:
-                raise nif_utils.NifError("Incomplete Euler key set {} for action {}".format(bonestr, action.name))
-            else:
-                for frame, euler in self.iter_frame_key(eulers, mathutils.Euler):
-                    keymat = armature.export_keymat(bind_rot, euler.to_matrix().to_4x4(), bone)
-                    euler = keymat.to_euler("XYZ", euler)
-                    euler_curve.append((frame, euler))
-
-        if translations:
-            if len(translations) != 3:
-                raise nif_utils.NifError("Incomplete LOC key set{} for action {}".format(bonestr, action.name))
-            else:
-                for frame, trans in self.iter_frame_key(translations, mathutils.Vector):
-                    keymat = armature.export_keymat(bind_rot, mathutils.Matrix.Translation(trans), bone)
-                    trans = keymat.to_translation() + bind_trans
-                    trans_curve.append((frame, trans))
-
-        # finally we can export the data calculated above
-        if n_kfi and max(len(quat_curve), len(euler_curve), len(trans_curve), len(scale_curve)) <= 1:
-            # only add data if number of keys is > 1
-            # (see importer comments with import_kf_root: a single frame
-            # keyframe denotes an interpolator without further data)
-            # insufficient keys, so set the data and we're done!
-            if trans_curve:
-                trans = trans_curve[0][1]
-                n_kfi.translation.x = trans[0]
-                n_kfi.translation.y = trans[1]
-                n_kfi.translation.z = trans[2]
-            if quat_curve:
-                quat = quat_curve[0][1]
-                n_kfi.rotation.x = quat.x
-                n_kfi.rotation.y = quat.y
-                n_kfi.rotation.z = quat.z
-                n_kfi.rotation.w = quat.w
-            elif euler_curve:
-                quat = euler_curve[0][1].to_quaternion()
-                n_kfi.rotation.x = quat.x
-                n_kfi.rotation.y = quat.y
-                n_kfi.rotation.z = quat.z
-                n_kfi.rotation.w = quat.w
-            # ignore scale for now...
-            n_kfi.scale = 1.0
-            # done!
-            return
-
-        # add the keyframe data
-        if not n_kfi:
+        else:
+            # add the keyframe data
             n_kfd = block_store.create_block("NiKeyframeData", exp_fcurves)
             n_kfc.data = n_kfd
-        else:
-            # number of frames is > 1, so add transform data
-            n_kfd = block_store.create_block("NiTransformData", exp_fcurves)
-            n_kfi.data = n_kfd
 
         # TODO [animation] support other interpolation modes, get interpolation from blender?
         #                  probably requires additional data like tangents and stuff
 
-        # save all nif keys
+        # finally we can export the data calculated above
         if euler_curve:
             n_kfd.rotation_type = NifFormat.KeyType.XYZ_ROTATION_KEY
             n_kfd.num_rotation_keys = 1  # *NOT* len(frames) this crashes the engine!
