eb6c4483457ef5de27eb85a29bca2288d9f8a700
neomonkeus <neomonkeus@gmail.com>
2020-01-08 21:30:46 +0000

    Some semblance of readability on loop code.

--- io_scene_nif/modules/geometry/mesh/mesh_import.py
+++ io_scene_nif/modules/geometry/mesh/mesh_import.py
@@ -44,46 +44,51 @@ class Mesh:
 
     @staticmethod
     def add_triangles_to_bmesh(b_mesh, n_triangles, v_map):
-        # Indexs for later
+        # Indices for later
         b_poly_index = len(b_mesh.polygons)  # TODO [general] Replace with add to end
-        bf2_index = len(b_mesh.polygons)
+        b_poly_offset = len(b_mesh.polygons)
         b_loop_index = len(b_mesh.loops)
+
         # add polys to mesh
         num_trianges = len(n_triangles)
         poly_count = num_trianges
         b_mesh.polygons.add(poly_count)
         b_mesh.loops.add(poly_count * 3)
+
         f_map = [None] * num_trianges
-        num_new_faces = 0  # counter for debugging
         unique_faces = list()  # to avoid duplicate polygons
         tri_point_list = list()
-        for i, f in enumerate(n_triangles):
+        for n_tri_index, n_triangle in enumerate(n_triangles):
             # get face index
-            f_verts = [v_map[vert_index] for vert_index in f]
+            f_verts = [v_map[n_vert] for n_vert in n_triangle]
             if tuple(f_verts) in unique_faces:
                 continue
 
             unique_faces.append(tuple(f_verts))
-            f_map[i] = b_poly_index
-            tri_point_list.append(len(n_triangles[i]))
-            ls_list = list()
+            f_map[n_tri_index] = b_poly_index
+            tri_point_list.append(len(n_triangles[n_tri_index]))
             b_poly_index += 1
-            num_new_faces += 1
 
-        for ls1 in range(0, num_new_faces * (tri_point_list[len(ls_list)]), (tri_point_list[len(ls_list)])):
+        ls_list = list()
+        num_unique_faces = len(unique_faces)
+        for ls1 in range(0, num_unique_faces * (tri_point_list[len(ls_list)]), (tri_point_list[len(ls_list)])):
             ls_list.append((ls1 + b_loop_index))
-        for i in range(len(unique_faces)):
-            face = f_map[i]
-            if face is None:
+
+        for n_tri_index in range(num_unique_faces):
+            face_index = f_map[n_tri_index]
+            if face_index is None:
                 continue
-            b_mesh.polygons[face].loop_start = ls_list[(face - bf2_index)]
-            b_mesh.polygons[face].loop_total = len(unique_faces[(face - bf2_index)])
-            l = 0
-            lp_points = [v_map[loop_point] for loop_point in n_triangles[(face - bf2_index)]]
-            while l < (len(n_triangles[(face - bf2_index)])):
-                b_mesh.loops[(l + (b_loop_index))].vertex_index = lp_points[l]
-                l += 1
-            b_loop_index += (len(n_triangles[(face - bf2_index)]))
+            b_mesh.polygons[face_index].loop_start = ls_list[(face_index - b_poly_offset)]
+            b_mesh.polygons[face_index].loop_total = len(unique_faces[(face_index - b_poly_offset)])
+
+            loop = 0
+            lp_points = [v_map[loop_point] for loop_point in n_triangles[(face_index - b_poly_offset)]]
+            num_loops = len(n_triangles[(face_index - b_poly_offset)])
+            while loop < num_loops:
+                b_mesh.loops[(loop + b_loop_index)].vertex_index = lp_points[loop]
+                loop += 1
+
+            b_loop_index += num_loops
         # at this point, deleted polygons (degenerate or duplicate) satisfy f_map[i] = None
-        NifLog.debug("{0} unique polygons".format(num_new_faces))
-        return bf2_index, f_map
+        NifLog.debug("{0} unique polygons".format(num_unique_faces))
+        return b_poly_offset, f_map
