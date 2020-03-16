4ab97e623c3c61b9fd5befbb40034db39d6b5cf1
neomonkeus <neomonkeus@gmail.com>
2020-03-16 17:58:35 +0000

    Remove old lightingShaderproperty methods.

--- io_scene_nif/modules/nif_import/property/shader/bsshaderproperty.py
+++ io_scene_nif/modules/nif_import/property/shader/bsshaderproperty.py
@@ -44,7 +44,6 @@ from io_scene_nif.modules.nif_import.object.block_registry import block_store
 from io_scene_nif.modules.nif_import.property.material import Material
 from io_scene_nif.modules.nif_import.property.shader import BSShader
 from io_scene_nif.modules.nif_import.property.texture.types.bsshadertexture import BSShaderTexture
-from io_scene_nif.utils.util_logging import NifLog
 
 
 class BSShaderPropertyProcessor(BSShader):
@@ -83,13 +82,6 @@ class BSShaderPropertyProcessor(BSShader):
     def n_block(self, value):
         self._n_block = value
 
-    @staticmethod
-    def get():
-        """ Static access method. """
-        if not BSShaderPropertyProcessor.__instance:
-            BSShaderPropertyProcessor()
-        return BSShaderPropertyProcessor.__instance
-
     def __init__(self):
         """ Virtually private constructor. """
         if BSShaderPropertyProcessor.__instance is not None:
@@ -99,17 +91,27 @@ class BSShaderPropertyProcessor(BSShader):
             BSShaderPropertyProcessor.__instance = self
             self.texturehelper = BSShaderTexture().get()
 
+    @staticmethod
+    def get():
+        """ Static access method. """
+        if not BSShaderPropertyProcessor.__instance:
+            BSShaderPropertyProcessor()
+        return BSShaderPropertyProcessor.__instance
+
     def register_bsproperty(self, processor):
         processor.register(NifFormat.BSLightingShaderProperty, self.import_bs_lighting_shader_property)
         processor.register(NifFormat.BSEffectShaderProperty, self.import_bs_effect_shader_property)
+        processor.register(NifFormat.BSShaderPPLightingProperty, self.import_bs_shader_pp_lighting_proprerty)
 
     def import_bs_lighting_shader_property(self, bs_shader_property):
+        b_mat = self.create_material_name(bs_shader_property)
 
-        name = block_store.import_name(bs_shader_property)
-        if name is None:
-            name = (self._n_block.name.decode() + "_nt_mat")
-        b_mat = bpy.data.materials.new(name)
-        self.b_mesh.materials.append(b_mat)
+        b_shader = b_mat.niftools_shader
+        b_shader.bs_shadertype = 'BSLightingShaderProperty'
+
+        sf_type = NifFormat.BSLightingShaderPropertyShaderType._enumkeys[bs_shader_property.sf_type]
+        b_shader.bslsp_shaderobjtype = NifFormat.BSLightingShaderPropertyShaderType._enumkeys[sf_type]
+        self.import_shader_flags(b_mat, bs_shader_property)
 
         # Textures
         self.texturehelper.import_bsshaderproperty_textureset(b_mat, bs_shader_property)
@@ -129,7 +131,7 @@ class BSShaderPropertyProcessor(BSShader):
         if (b_mat.diffuse_color.r + b_mat.diffuse_color.g + b_mat.diffuse_color.g) == 0:
             Material.import_material_diffuse(b_mat, bs_shader_property.hair_tint_color)
 
-        # TODO [material][shader][property] Handle nialphaproperty node lookup
+        # TODO [material][b_shader][property] Handle nialphaproperty node lookup
         # # Alpha
         # if n_alpha_prop:
         #     b_mat = self.set_alpha_bsshader(b_mat, bs_shader_property)
@@ -145,14 +147,22 @@ class BSShaderPropertyProcessor(BSShader):
         b_mat.niftools.lightingeffect1 = bs_shader_property.lighting_effect_1
         b_mat.niftools.lightingeffect2 = bs_shader_property.lighting_effect_2
 
-    def import_bs_effect_shader_property(self, bs_effect_shader_property):
-
-        # update material material name
-        name = block_store.import_name(bs_effect_shader_property)
+    def create_material_name(self, bs_shader_property):
+        name = block_store.import_name(bs_shader_property)
         if name is None:
             name = (self._n_block.name.decode() + "_nt_mat")
         b_mat = bpy.data.materials.new(name)
         self.b_mesh.materials.append(b_mat)
+        return b_mat
+
+    def import_bs_effect_shader_property(self, bs_effect_shader_property):
+        # update material material name
+        b_mat = self.create_material_name(bs_effect_shader_property)
+
+        shader = b_mat.niftools_shader
+        shader.bs_shadertype = 'BSEffectShaderProperty'
+        shader.bslsp_shaderobjtype = 'Default'
+        self.import_shader_flags(b_mat, bs_effect_shader_property)
 
         self.texturehelper.import_bseffectshaderproperty_textures(b_mat, bs_effect_shader_property)
 
@@ -177,38 +187,3 @@ class BSShaderPropertyProcessor(BSShader):
             b_mat.emit = bs_effect_shader_property.emissive_multiple
 
         b_mat.niftools_alpha.textureflag = bs_effect_shader_property.controller.flags
-
-        # @staticmethod
-        # def import_bseffectshaderproperty(b_obj, b_prop):
-        #     b_obj.niftools_shader.bs_shadertype = 'BSEffectShaderProperty'
-        #     b_obj.niftools_shader.bslsp_shaderobjtype = 'Default'
-        #     BSShaderProperties.import_shader_flags(b_obj, b_prop)
-        #
-        # @staticmethod
-        # def import_bslightingshaderproperty(b_obj, b_prop):
-        #     b_obj.niftools_shader.bs_shadertype = 'BSLightingShaderProperty'
-        #     sf_type = NifFormat.BSLightingShaderPropertyShaderType._enumvalues.index(b_prop.skyrim_shader_type)
-        #     b_obj.niftools_shader.bslsp_shaderobjtype = NifFormat.BSLightingShaderPropertyShaderType._enumkeys[sf_type]
-        #     BSShaderProperties.import_shader_flags(b_obj, b_prop)
-        #
-        # @staticmethod
-        # def import_bsshaderpplightingproprerty(b_obj, b_prop):
-        #     b_obj.niftools_shader.bs_shadertype = 'BSShaderPPLightingProperty'
-        #     sf_type = NifFormat.BSShaderType._enumvalues.index(b_prop.shader_type)
-        #     b_obj.niftools_shader.bsspplp_shaderobjtype = NifFormat.BSShaderType._enumkeys[sf_type]
-        #     for b_flag_name in b_prop.shader_flags._names:
-        #         sf_index = b_prop.shader_flags._names.index(b_flag_name)
-        #         if b_prop.shader_flags._items[sf_index]._value == 1:
-        #             b_obj.niftools_shader[b_flag_name] = True
-        #
-        # @staticmethod
-        # def import_shader_flags(b_obj, b_prop):
-        #     for b_flag_name_1 in b_prop.shader_flags_1._names:
-        #         sf_index = b_prop.shader_flags_1._names.index(b_flag_name_1)
-        #         if b_prop.shader_flags_1._items[sf_index]._value == 1:
-        #             b_obj.niftools_shader[b_flag_name_1] = True
-        #
-        #     for b_flag_name_2 in b_prop.shader_flags_2._names:
-        #         sf_index = b_prop.shader_flags_2._names.index(b_flag_name_2)
-        #         if b_prop.shader_flags_2._items[sf_index]._value == 1:
-        #             b_obj.niftools_shader[b_flag_name_2] = True
