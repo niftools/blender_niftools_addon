e7a67e8f5fe65e25a3ada2acf3e9cb7089c29f2c
neomonkeus <neomonkeus@gmail.com>
2020-03-17 22:43:48 +0000

    Create a texture instance and stop passing in b_obj, only b_mat.

--- io_scene_nif/modules/nif_export/property/shader/__init__.py
+++ io_scene_nif/modules/nif_export/property/shader/__init__.py
@@ -47,25 +47,26 @@ class BSShader:
     def __init__(self):
         self.texturehelper = Texture()
 
-    def export_bs_shader_property(self, b_obj=None, b_mat=None):
+    def export_bs_shader_property(self, b_mat=None):
         """Export a Bethesda shader property block."""
-        self.texturehelper.determine_texture_types(b_obj, b_mat)
+        self.texturehelper.determine_texture_types(b_mat)
 
         # create new block
-        if b_obj.niftools_shader.bs_shadertype == 'BSShaderPPLightingProperty':
+        if b_mat.niftools_shader.bs_shadertype == 'BSShaderPPLightingProperty':
             bsshader = NifFormat.BSShaderPPLightingProperty()
+
             # set shader options
             # TODO: FIXME:
-            b_s_type = NifFormat.BSShaderType._enumkeys.index(b_obj.niftools_shader.bsspplp_shaderobjtype)
+            b_s_type = NifFormat.BSShaderType._enumkeys.index(b_mat.niftools_shader.bsspplp_shaderobjtype)
             bsshader.shader_type = NifFormat.BSShaderType._enumvalues[b_s_type]
 
             # Shader Flags
             if hasattr(bsshader, 'shader_flags'):
-                BSShader.export_shader_flags(b_obj, bsshader)
+                BSShader.export_shader_flags(b_mat, bsshader)
 
-        if b_obj.niftools_shader.bs_shadertype == 'BSLightingShaderProperty':
+        if b_mat.niftools_shader.bs_shadertype == 'BSLightingShaderProperty':
             bsshader = NifFormat.BSLightingShaderProperty()
-            b_s_type = NifFormat.BSLightingShaderPropertyShaderType._enumkeys.index(b_obj.niftools_shader.bslsp_shaderobjtype)
+            b_s_type = NifFormat.BSLightingShaderPropertyShaderType._enumkeys.index(b_mat.niftools_shader.bslsp_shaderobjtype)
             bsshader.shader_type = NifFormat.BSLightingShaderPropertyShaderType._enumvalues[b_s_type]
 
             # UV Offset
@@ -117,9 +118,9 @@ class BSShader:
 
             # Shader Flags
             if hasattr(bsshader, 'shader_flags_1'):
-                self.export_shader_flags(b_obj, bsshader)
+                self.export_shader_flags(b_mat, bsshader)
 
-        if b_obj.niftools_shader.bs_shadertype == 'BSEffectShaderProperty':
+        if b_mat.niftools_shader.bs_shadertype == 'BSEffectShaderProperty':
             bsshader = NifFormat.BSEffectShaderProperty()
 
             # Alpha
@@ -138,11 +139,11 @@ class BSShader:
 
             # Shader Flags
             if hasattr(bsshader, 'shader_flags_1'):
-                self.export_shader_flags(b_obj, bsshader)
+                self.export_shader_flags(b_mat, bsshader)
 
-        if b_obj.niftools_shader.bs_shadertype == 'None':
+        if b_mat.niftools_shader.bs_shadertype == 'None':
             raise util_math.NifError("Export version expected shader. No shader applied to mesh '{0}', these cannot be exported to NIF."
-                                     "Set shader before exporting.".format(b_obj))
+                                     "Set shader before exporting.".format(b_mat))
         # set textures
         texset = NifFormat.BSShaderTextureSet()
         bsshader.texture_set = texset
@@ -155,7 +156,7 @@ class BSShader:
         if self.texturehelper.detail_mtex:
             texset.textures[3] = TextureWriter.export_texture_filename(self.texturehelper.detail_mtex.texture)
 
-        if b_obj.niftools_shader.bs_shadertype == 'BSLightingShaderProperty':
+        if b_mat.niftools_shader.bs_shadertype == 'BSLightingShaderProperty':
             texset.num_textures = 9
             texset.textures.update_size()
             if self.texturehelper.detail_mtex:
@@ -163,19 +164,19 @@ class BSShader:
             if self.texturehelper.gloss_mtex:
                 texset.textures[7] = TextureWriter.export_texture_filename(self.texturehelper.gloss_mtex.texture)
 
-        if b_obj.niftools_shader.bs_shadertype == 'BSEffectShaderProperty':
+        if b_mat.niftools_shader.bs_shadertype == 'BSEffectShaderProperty':
             bsshader.source_texture = TextureWriter.export_texture_filename(self.texturehelper.base_mtex.texture)
             bsshader.greyscale_texture = TextureWriter.export_texture_filename(self.texturehelper.glow_mtex.texture)
 
         return bsshader
 
     @staticmethod
-    def export_shader_flags(b_obj, shader):
-        b_flag_list = b_obj.niftools_shader.bl_rna.properties.keys()
+    def export_shader_flags(b_mat, shader):
+        b_flag_list = b_mat.niftools_shader.bl_rna.properties.keys()
         if hasattr(shader, 'shader_flags'):
             for sf_flag in shader.shader_flags._names:
                 if sf_flag in b_flag_list:
-                    b_flag = b_obj.niftools_shader.get(sf_flag)
+                    b_flag = b_mat.niftools_shader.get(sf_flag)
                     if b_flag:
                         sf_flag_index = shader.shader_flags._names.index(sf_flag)
                         shader.shader_flags._items[sf_flag_index]._value = 1
@@ -183,7 +184,7 @@ class BSShader:
         if hasattr(shader, 'shader_flags_1'):
             for sf_flag in shader.shader_flags_1._names:
                 if sf_flag in b_flag_list:
-                    b_flag = b_obj.niftools_shader.get(sf_flag)
+                    b_flag = b_mat.niftools_shader.get(sf_flag)
                     if b_flag:
                         sf_flag_index = shader.shader_flags_1._names.index(sf_flag)
                         shader.shader_flags_1._items[sf_flag_index]._value = 1
@@ -191,7 +192,7 @@ class BSShader:
         if hasattr(shader, 'shader_flags_2'):
             for sf_flag in shader.shader_flags_2._names:
                 if sf_flag in b_flag_list:
-                    b_flag = b_obj.niftools_shader.get(sf_flag)
+                    b_flag = b_mat.niftools_shader.get(sf_flag)
                     if b_flag:
                         sf_flag_index = shader.shader_flags_2._names.index(sf_flag)
                         shader.shader_flags_2._items[sf_flag_index]._value = 1
