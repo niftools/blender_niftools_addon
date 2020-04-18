8e6613720c9cd22edf0a653f89200e75827f6fcb
neomonkeus <neomonkeus@gmail.com>
2020-04-18 17:37:35 +0100

    Dynamically generate date when creating the zip file.

--- install/install.sh
+++ install/install.sh
@@ -5,6 +5,9 @@ TEMP="${BUILD_DIR}"/temp
 ROOT="${BUILD_DIR}"/..
 VERSION=$(cat "${ROOT}/io_scene_nif/VERSION.txt")
 NAME="blender_nif_plugin"
+HASH=$(git rev-parse --short HEAD)
+DATE=$(date +%F)
+ZIP_NAME="${NAME}-${VERSION}-${HASH}-${DATE}.zip"
 
 find_blender() {
     for BLENDER_VERSION in 2.79
@@ -42,6 +45,4 @@ sh "${BUILD_DIR}"/makezip.sh
 
 # copy files from repository to blender addons folder
 echo "Unzipping to ${PLUGIN_DIR}"
-HASH=$(git rev-parse --short HEAD)
-ZIP_NAME="${NAME}-${VERSION}-${HASH}.zip"
 unzip -q "${TEMP}/${ZIP_NAME}" -d "${BLENDER_ADDONS_DIR}"
