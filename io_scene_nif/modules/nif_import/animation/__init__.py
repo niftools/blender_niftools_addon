b377e35fdfc53521871d5488aaf7c98b8007224f
Reddraconi <reddraconi@redsavstudios.com>
2020-10-09 19:52:11 -0500

    `TODO`s added where changes may be applicable. Not sure.

--- io_scene_nif/modules/nif_import/animation/__init__.py
+++ io_scene_nif/modules/nif_import/animation/__init__.py
@@ -89,11 +89,13 @@ class Animation:
     def create_fcurves(self, action, dtype, drange, flags=None, bonename=None, keyname=None):
         """ Create fcurves in action for desired conditions. """
         # armature pose bone animation
+        # TODO: This may be a candidate for conversion to f-strings. Not sure.
         if bonename:
             fcurves = [
                 action.fcurves.new(data_path='pose.bones["' + bonename + '"].' + dtype, index=i, action_group=bonename)
                 for i in drange]
         # shapekey pose bone animation
+        # TODO: This may be a candidate for conversion to f-strings. Not sure.
         elif keyname:
             fcurves = [action.fcurves.new(data_path='key_blocks["' + keyname + '"].' + dtype, index=0), ]
         else:
