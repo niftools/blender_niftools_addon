8d883f945b2d1aa9e3a97d0bae1d671cfbd0a4f3
HENDRIX-ZT2 <arnfinn@spholderer.de>
2020-12-01 09:08:34 +0100

    Export: Retrieve scale correction from scene

--- io_scene_niftools/nif_export.py
+++ io_scene_niftools/nif_export.py
@@ -282,16 +282,18 @@ class NifExport(NifCommon):
             """
 
             # apply scale
-            if abs(NifOp.props.scale_correction) > NifOp.props.epsilon:
-                NifLog.info(f"Applying scale correction {NifOp.props.scale_correction}")
+            # moved from NifOp.props.scale_correction
+            scale_correction = bpy.context.scene.niftools_scene.scale_correction
+            if abs(scale_correction) > NifOp.props.epsilon:
+                NifLog.info(f"Applying scale correction {scale_correction}")
                 data.roots = [root_block]
                 toaster = pyffi.spells.nif.NifToaster()
-                toaster.scale = 1 / NifOp.props.scale_correction
+                toaster.scale = 1 / scale_correction
                 pyffi.spells.nif.fix.SpellScale(data=data, toaster=toaster).recurse()
 
                 # also scale egm
                 if EGMData.data:
-                    EGMData.data.apply_scale(1 / NifOp.props.scale_correction)
+                    EGMData.data.apply_scale(1 / scale_correction)
 
             # generate mopps (must be done after applying scale!)
             if bpy.context.scene.niftools_scene.game in ('OBLIVION', 'FALLOUT_3', 'SKYRIM'):
