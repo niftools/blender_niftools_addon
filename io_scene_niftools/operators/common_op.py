5d8bef8c4cdfd31b0a11826e8419a8bcd6f0c075
neomonkeus <neomonkeus@gmail.com>
2020-12-05 00:19:52 +0000

    Fix extensions to write correctly on export.

--- io_scene_niftools/operators/common_op.py
+++ io_scene_niftools/operators/common_op.py
@@ -124,7 +124,7 @@ class CommonEGM:
 
 class CommonKF:
     # Default file name extension.
-    filename_ext = ".egm"
+    filename_ext = ".kf"
 
     # File name filter for file select dialog.
     filter_glob: bpy.props.StringProperty(
