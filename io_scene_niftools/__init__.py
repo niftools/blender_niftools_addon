55f8d5dfe07ff3610577dfecdca09b329e339f4a
neomonkeus <neomonkeus@gmail.com>
2021-03-11 11:29:37 +0000

    Bump min version to 0.0.4 due to texture processor bug in 0.0.3

--- io_scene_niftools/__init__.py
+++ io_scene_niftools/__init__.py
@@ -120,4 +120,4 @@ def configure_autoupdater():
     addon_updater_ops.updater.user = "niftools"
     addon_updater_ops.updater.repo = "blender_niftools_addon"
     addon_updater_ops.updater.website = "https://github.com/niftools/blender-niftools-addon/"
-    addon_updater_ops.updater.version_min_update = (0, 0, 2)
+    addon_updater_ops.updater.version_min_update = (0, 0, 4)
