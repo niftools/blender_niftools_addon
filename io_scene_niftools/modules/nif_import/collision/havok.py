9111a3454faa1397af158da0eaef9d555f48adb5
Candoran2 <45334438+Candoran2@users.noreply.github.com>
2022-04-29 22:39:18 +0200

    Temporarily moved penetration depth from collision modifier permeability to niftools collision property to fix error caused by changes in Blender 3.0 and later.

--- io_scene_niftools/modules/nif_import/collision/havok.py
+++ io_scene_niftools/modules/nif_import/collision/havok.py
@@ -197,7 +197,7 @@ class BhkCollision(Collision):
             b_r_body.deactivate_angular_velocity = mathutils.Vector([ang_vel.w, ang_vel.x, ang_vel.y, ang_vel.z]).magnitude
 
             # Custom Niftools properties
-            b_col_obj.collision.permeability = bhkshape.penetration_depth
+            b_col_obj.nifcollision.penetration_depth = bhkshape.penetration_depth
             b_col_obj.nifcollision.deactivator_type = NifFormat.DeactivatorType._enumkeys[bhkshape.deactivator_type]
             b_col_obj.nifcollision.solver_deactivation = NifFormat.SolverDeactivation._enumkeys[bhkshape.solver_deactivation]
             b_col_obj.nifcollision.max_linear_velocity = bhkshape.max_linear_velocity
