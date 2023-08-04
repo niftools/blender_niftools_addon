9de0ff9cc2ef622bc6deb5f57e182c02405cab39
Candoran2 <45334438+Candoran2@users.noreply.github.com>
2023-08-04 21:19:54 +0200

    Fix for #584.

--- install/zip.ps1
+++ install/zip.ps1
@@ -13,7 +13,20 @@ write ("Existing archive removed successfully")
 }
 
 Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::CreateFromDirectory($source, $destination, 1, 1)
+
+$EncoderClass=@"
+  public class FixedEncoder : System.Text.UTF8Encoding {
+    public FixedEncoder() : base(true) { }
+    public override byte[] GetBytes(string s) {
+      s = s.Replace("\\", "/");
+      return base.GetBytes(s);
+    }
+  }
+"@
+Add-Type -TypeDefinition $EncoderClass
+
+$Encoder = New-Object FixedEncoder
+[io.compression.zipfile]::CreateFromDirectory($source, $destination, 1, 1, $Encoder)
 If(Test-path $destination) {
     write("File successully written")
 }
